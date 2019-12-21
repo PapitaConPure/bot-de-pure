68d4706a79d33dccb6979e6d2c1b473d1ae24163
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-21 06:11:39 -0300

    Update F1

---
+++ commands/papa-guardarestado.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js'); //Integrar discord.js
+var global = require('../config.json'); //Variables globales
+var func = require('../func.js'); //Funciones globales
+const fs = require('fs'); //Integrar operaciones sistema de archivos de consola
+
+module.exports = {
+	name: 'papa-guardarestado',
+	execute(message, args) {
+        if(message.author.id === '423129757954211880' || !func.notModerator(message.member)) {
+            fs.writeFile("../config.json", JSON.stringify(global, null, 4), err => {
+                if(err) {
+                    message.channel.send(':bangbang: Error en la escritura de datos. Se recomienda guardar cualquier dato ingresado al bot en un bloc de notas y reiniciar.');
+                    console.error(err);
+                }
+            });
+        }
+    },
+};
