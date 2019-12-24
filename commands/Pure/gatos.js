de6e352970273c5af09c96d01ccec2eaac0031a5
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-24 18:14:17 -0300

    Update F1

--- commands/Pure/gatos.js
+++ commands/Pure/gatos.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js'); //Integrar discord.js
 var global = require('../../config.json'); //Variables globales
 
+const tmpfunc = async function() {
+	const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+	message.channel.send(file);
+}
+
 module.exports = {
 	name: 'gatos',
 	execute(message, args){
-		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-
-		message.channel.send(file);
+		tmpfunc();
     },
 };
