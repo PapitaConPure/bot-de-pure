6a06810d7fefc9bc38927445f4c9688c3c815d5a
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-24 18:16:44 -0300

    Update F1

--- commands/Pure/gatos.js
+++ commands/Pure/gatos.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js'); //Integrar discord.js
 var global = require('../../config.json'); //Variables globales
+const fetch = require('node-fetch');
 
 const tmpfunc = async function() {
 	const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
