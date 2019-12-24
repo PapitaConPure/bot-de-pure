26bed898c87023a618ac7afd0fd9fdd1386af632
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-24 17:51:03 -0300

    Update F1

--- commands/Pure/gatos.js
+++ commands/Pure/gatos.js
@@ -5,7 +5,11 @@ var global = require('../../config.json'); //Variables globales
 module.exports = {
 	name: 'gatos',
 	execute(message, args){
-		const file = await fetch.fetch('https://aws.random.cat/meow').then(response => response.json());
-		message.channel.send(file);
+		try {
+			const { file } = await fetch.fetch('https://aws.random.cat/meow').then(response => response.json());
+			await message.channel.send(file);
+		} catch (error) {
+			
+		}
     },
 };
