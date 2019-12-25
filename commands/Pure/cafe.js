dd47953501556d1e8e6c146e38854427a30785b2
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-25 04:55:42 -0300

    Update F1

--- commands/Pure/cafe.js
+++ commands/Pure/cafe.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js'); //Integrar discord.js
 var global = require('../../config.json'); //Variables globales
 const fetch = require('node-fetch');
 
-const getRandomInt = function(_max) {
+/*const getRandomInt = function(_max) {
   _max = Math.floor(_max);
   return Math.floor(Math.random() * _max);
-}
+}*/
 
 const tmpfunc = async function(tmpch) {
 	const { data } = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=Qu29veK701szqoFK6tXgOiybuc1q3PaX&q=coffee&limit=25`).then(response => response.json());
-	tmpch.send(data[getRandomInt(24)]);
+	tmpch.send(data/*[getRandomInt(24)]*/);
 }
 
 module.exports = {
