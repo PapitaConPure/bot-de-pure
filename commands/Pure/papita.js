88437fa8eeaa4cbc2631297e2fe58add8c6299a3
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-26 05:22:52 -0300

    Update F1

--- commands/Pure/papita.js
+++ commands/Pure/papita.js
@@ -7,7 +7,6 @@ module.exports = {
         'papa', 'leche'
     ],
 	execute(message, args){
-		const emot = getRandomInt(16);
 		if(message.channel.nsfw) message.channel.send('https://www.youtube.com/watch?v=pwEvEY-7p9o');
 		else message.channel.send('**Lechita:tm: uwu** :milk:');
     },
