18e13ec3220851e51ae31b390f1e7641eee05129
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-26 00:13:12 -0300

    Update F1

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -14,11 +14,11 @@ const tmpfunc = async function(tmpch, arglist) {
 	else srchtags += 'safe';
 	for(let i = 0; i < arglist.length; i++)
 		srchtags += ' ' + arglist[i];
-	srchtags = querystring.stringify({ term: srchtags });
+	srchtags = querystring.stringify({ tags: srchtags });
 	const srchpg = getRandomInt(3);
 	const srchlimit = 10;
 	const { post } = await fetch(
-		`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${srchtags}&pid=${srchpg}&limit=${srchlimit}&api_key=ace81bbbcbf972d37ce0b8b07afccb00261f34ed39e06cd3a8d6936d6a16521b&user_id=497526&json=1`
+		`https://gelbooru.com/index.php?page=dapi&s=post&q=index&${srchtags}&pid=${srchpg}&limit=${srchlimit}&api_key=ace81bbbcbf972d37ce0b8b07afccb00261f34ed39e06cd3a8d6936d6a16521b&user_id=497526&json=1`
 	).then(response => response.json());
 
 	//Crear y usar embed
