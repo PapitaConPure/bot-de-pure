b8084c6ef3fa57d9fdbd9e2b60e2d4ca2702fa1a
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-25 23:40:56 -0300

    Update F1

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -2,9 +2,30 @@ const Discord = require('discord.js'); //Integrar discord.js
 var global = require('../../config.json'); //Variables globales
 const fetch = require('node-fetch');
 
-const tmpfunc = async function(tmpch) {
-	const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-	tmpch.send(file);
+const getRandomInt = function(_max) {
+  _max = Math.floor(_max);
+  return Math.floor(Math.random() * _max);
+}
+
+const tmpfunc = async function(tmpch, arglist) {
+	let srchtags = 'touhou';
+	for(let i = 0; i < arglist.length; i++)
+		srchtags += ' ' + arglist[i];
+	const srchpg = getRandomInt(3);
+	const srchlimit = 10;
+	const { posts } = await fetch(
+		`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${srchtags}&pid=${srchpg}&limit=${srchlimit}&api_key=ace81bbbcbf972d37ce0b8b07afccb00261f34ed39e06cd3a8d6936d6a16521b&user_id=497526`
+	).then(response => response.json());
+
+	//Crear y usar embed
+	const selected = posts[getRandomInt(srchlimit - 1)];
+	const salsa = (selected.source.startsWith('http'))?selected.source:'Desconocida.';
+	const Embed = new Discord.RichEmbed()
+		.setColor('#fa7b62')
+		.setTitle('Tohas uwu')
+		.addField('Salsa', `${salsa}`)
+		.setImage(selected.sample_url);
+	tmpch.send(Embed);
 }
 
 module.exports = {
@@ -15,6 +36,6 @@ module.exports = {
 		'2hu'
     ],
 	execute(message, args){
-		tmpfunc(message.channel);
+		tmpfunc(message.channel, args);
     },
 };
