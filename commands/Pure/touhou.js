eedc180718065a54c20de13d9aba1c19344f6cc5
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-26 00:47:33 -0300

    Update F1

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -14,21 +14,20 @@ const tmpfunc = async function(tmpch, arglist) {
 	else srchtags += 'safe';
 	for(let i = 0; i < arglist.length; i++)
 		srchtags += ' ' + arglist[i];
-	srchtags = querystring.stringify({srchtags});
 	const srchpg = getRandomInt(3);
 	const srchlimit = 10;
-	const { Posts } = await fetch(
+	const { source, image } = await fetch(
 		`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${srchtags}&pid=${srchpg}&limit=${srchlimit}&api_key=ace81bbbcbf972d37ce0b8b07afccb00261f34ed39e06cd3a8d6936d6a16521b&user_id=497526&json=1`
 	).then(json => console.log(json)).then(response => response.json());
 
 	//Crear y usar embed
-	const selected = Posts[getRandomInt(srchlimit - 1)];
-	const salsa = (selected.source.startsWith('http'))?selected.source:'Desconocida.';
+	const selected = getRandomInt(srchlimit - 1);
+	const salsa = (source[selected].startsWith('http'))?source[selected]:'Desconocida.';
 	const Embed = new Discord.RichEmbed()
 		.setColor('#fa7b62')
 		.setTitle('Tohas uwu')
 		.addField('Salsa', `${salsa}`)
-		.setImage(selected.sample_url);
+		.setImage(image[selected]);
 	tmpch.send(Embed);
 }
 
