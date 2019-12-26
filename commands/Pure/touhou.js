ee86a22969448b1378beac1684fa045ef16cfbe0
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-26 01:07:11 -0300

    Update F1

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -18,7 +18,7 @@ const tmpfunc = async function(tmpch, arglist) {
 	const srchlimit = 10;
 	const { source, file_url } = await fetch(
 		`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${srchtags}&pid=${srchpg}&limit=${srchlimit}&api_key=ace81bbbcbf972d37ce0b8b07afccb00261f34ed39e06cd3a8d6936d6a16521b&user_id=497526&json=1`
-	).then(response => { response.json(); console.log(response); });
+	).then(response => { response.json(); console.log(response.json()); });
 
 	//Crear y usar embed
 	const selected = getRandomInt(srchlimit - 1);
