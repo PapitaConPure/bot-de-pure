0a1bc412741e0f25625ff37ad024bdf9df89ccdc
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-27 04:02:37 -0300

    Update F1

--- commands/Drawmaku/ayuda.js
+++ commands/Drawmaku/ayuda.js
@@ -15,8 +15,12 @@ module.exports = {
             '*Drawmaku:*\n' +
             `\t\`${global.p_drmk}entrar <apodo>\` para entrar al evento, solo si se está empezando.\n` +
             `\t\`${global.p_drmk}salir\` para salir del evento, solo si se está dentro.\n` +
-            `\t\`${global.p_drmk}danmaku ||<danmaku*>||\` para ingresar el nombre de tu danmaku ***(¡no olvides las \\|\\|barras verticales\\|\\|!)***.\n` +
-            `\t\`${global.p_drmk}danmaku <imagen adjunta del danmaku*>\` para ingresar el dibujo de tu danmaku ***(se tiene que adjuntar el dibujo junto al comando)***.\n` +
+            `\t\`${global.p_drmk}danmaku ||<danmaku*>||\` para ingresar el nombre de tu danmaku.\n` +
+            `\t\t***¡No olvides las \\|\\|barras verticales\\|\\|!***\n` +
+            `\t\t***Para adjuntar tu dibujo, debes usar un comando aparte.***\n` +
+            `\t\`${global.p_drmk}danmaku <imagen adjunta del danmaku*>\` para ingresar el dibujo de tu danmaku.\n` +
+            `\t\t***Se tiene que adjuntar el dibujo junto al comando.***\n` +
+            `\t\t***Para establecer el nombre de tu danmaku, debes usar un comando aparte.***\n` +
             `\t\`${global.p_drmk}recomp <@jugador*>\` para darle un punto a un jugador por adivinar y mostrar el nombre de la spell.\n` +
             `\t\`${global.p_drmk}lista\` para mostrar la lista de jugadores y sus puntajes.\n` +
             `\t\`${global.p_drmk}turno\` para ver a quién le toca dibujar.\n` +
--- commands/Pure/ayuda.js
+++ commands/Pure/ayuda.js
@@ -14,8 +14,9 @@ module.exports = {
             '**A continuación la lista de comandos.**\n' +
             '*Puré:*\n' +
             `\t\`${global.p_pure}gatos \` para mostrar imágenes de gatitos.\n` +
-            `\t\`${global.p_pure}café \` para mostrar imágenes de café.\n` +
-            `\t\`${global.p_pure}touhou <página> <tags>\` para mostrar imágenes de tohas.\n` +
+            `\t\`${global.p_pure}café <tags [Giphy]>\` para mostrar imágenes de café.\n` +
+            `\t\`${global.p_pure}touhou <página> <tags [Gelbooru]>\` para mostrar imágenes de tohas.\n` +
+            `\t\t***En canales marcados como "NSFW", los resultados serán lewds.***\n` +
             `\t\`${global.p_pure}uwu \` uwu.\n` +
             `*__Nota: los <parámetros> con el símbolo "\\*" son obligatorios.__*\n` +
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬'
