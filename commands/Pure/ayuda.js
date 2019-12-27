983a5895f372c9b01f3903e107d1e8b70b27782b
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-27 04:25:06 -0300

    Update F1

--- commands/Pure/ayuda.js
+++ commands/Pure/ayuda.js
@@ -13,11 +13,11 @@ module.exports = {
             '***LISTA DE COMANDOS***\n' +
             '**A continuación la lista de comandos.**\n' +
             '*Puré:*\n' +
-            `\t├ \`${global.p_pure}gatos \` para mostrar imágenes de gatitos.\n` +
-            `\t├ \`${global.p_pure}café <tags [Giphy]>\` para mostrar imágenes de café.\n` +
-            `\t├ \`${global.p_pure}touhou <página> <tags [Gelbooru]>\` para mostrar imágenes de tohas.\n` +
-            `\t│ \t└ ***En canales marcados como "NSFW", los resultados serán lewds.***\n` +
-            `\t└ \`${global.p_pure}uwu \` uwu.\n` +
+            `\t╠ \`${global.p_pure}gatos \` para mostrar imágenes de gatitos.\n` +
+            `\t╠ \`${global.p_pure}café <tags [Giphy]>\` para mostrar imágenes de café.\n` +
+            `\t╠ \`${global.p_pure}touhou <página> <tags [Gelbooru]>\` para mostrar imágenes de tohas.\n` +
+            `\t║ \t╚ ***En canales marcados como "NSFW", los resultados serán lewds.***\n` +
+            `\t╚ \`${global.p_pure}uwu \` uwu.\n` +
             `*__Nota: los <parámetros> con el símbolo "\\*" son obligatorios.__*\n` +
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬'
         );
