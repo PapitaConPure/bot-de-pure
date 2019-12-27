57b181a1f3a1f9082c65ae907a5613776a1b8e9a
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-27 11:17:44 -0300

    Update F1

--- commands/Pure/papa-guardarestado.js
+++ commands/Pure/papa-guardarestado.js
@@ -3,13 +3,14 @@ var global = require('../../config.json'); //Variables globales
 var func = require('../../func.js'); //Funciones globales
 const fs = require('fs');
 let path = require('path');
+var test = require('../../save.json'); //Variables globales
 
 module.exports = {
 	name: 'papa-guardarestado',
 	execute(message, args) {
         if(message.author.id === '423129757954211880') {
             message.channel.send(`\`\`\`json\n${JSON.stringify(global, null, 4)}\`\`\``);
-            fs.writeFile(path.join(__dirname, '../../save.json'), JSON.stringify(global, null, 4), err => {
+            fs.writeFileSync(path.join(__dirname, '../../save.json'), JSON.stringify(global, null, 4), err => {
                 if(err) {
                     message.channel.send(':bangbang: error en la escritura de datos. Se recomienda guardar cualquier dato ingresado al bot en un bloc de notas y reiniciar.');
                     console.error(err);
@@ -17,6 +18,7 @@ module.exports = {
                 }
             });
             message.channel.send(`:white_check_mark: datos guardados con éxito.`);
+            message.channel.send(`\`\`\`json\n${JSON.stringify(test, null, 4)}\`\`\``);
         } else {
             message.channel.send(':closed_lock_with_key: Solo Papita con Puré puede usar este comando.');
             return;
