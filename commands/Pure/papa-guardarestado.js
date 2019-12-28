70e8bb36e5727ad0efb5b013cecfcce32cb0b369
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-28 05:43:48 -0300

    Update F1

--- commands/Pure/papa-guardarestado.js
+++ commands/Pure/papa-guardarestado.js
@@ -6,10 +6,15 @@ module.exports = {
 	name: 'papa-guardarestado',
 	execute(message, args) {
         if(message.author.id === '423129757954211880') {
+            //let str = JSON.stringify(global, null, 4).split(/\n+/);
             message.channel.send(
-                ':floppy_disk: antes de reiniciar el bot, copiá y pegá esto en el archivo \`config.json\` de la carpeta del bot:\n' +
-                `\`\`\`json\n${JSON.stringify(global, null, 4)}\n\`\`\``
+                ':floppy_disk: antes de reiniciar el bot, copiá y pegá esto en el archivo \`config.json\` de la carpeta del bot:\n'/* +
+                `\`\`\`json\n${str[i]}\n\`\`\``*/
             );
+
+            message.channel.send({
+                files: ['../../config.json']
+            });
         } else {
             message.channel.send(':closed_lock_with_key: Solo Papita con Puré puede usar este comando.');
             return;
