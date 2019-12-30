dca7f7dec5c8562c00178894a48f7ca936874214
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-30 20:31:42 -0300

    Update F1

--- commands/Pure/mari.js
+++ commands/Pure/mari.js
@@ -3,7 +3,9 @@ var global = require('../../config.json'); //Variables globales
 
 const getRandomInt = function(_max) {
   _max = Math.floor(_max);
-  return Math.floor(Math.random() * _max);
+  let _randnum = Math.floor(Math.random() * _max);
+  if(_randnum === _max && _max > 0) _randnum--;
+  return _randnum;
 }
 
 module.exports = {
@@ -12,32 +14,12 @@ module.exports = {
         'mari'
     ],
 	execute(message, args){
-		const emot = getRandomInt(19);
-		let str = '**';
-		switch(emot) {
-			case 0: str += '\'o\''; break;
-			case 1: str += '\'O\''; break;
-			case 2: str += '\'u\''; break;
-			case 3: str += '\'U\''; break;
-			case 4: str += '^O^'; break;
-			case 5: str += 'o.o'; break;
-			case 6: str += 'O.O'; break;
-			case 7: str += 'QUQ'; break;
-			case 8: str += '\'.\''; break;
-			case 9: str += '¬u¬'; break;
-			case 10: str += '¬U¬'; break;
-			case 11: str += 'o\'o'; break;
-			case 12: str += 'o.o\'\''; break;
-			case 13: str += 'o-o'; break;
-			case 14: str += 'O.o'; break;
-			case 15: str += 'x.x'; break;
-			case 16: str += 'ouo'; break;
-			case 17: str += 'OUO'; break;
-			case 18: str += 'OuO'; break;
-			case 19: str += '>:C'; break;
-			case 20: str += '>:c'; break;
-		}
-		str += '**';
-		message.channel.send(str);
+		const emot = [
+			'\'o\'', '\'O\'', '\'u\'', '\'U\'', '^O^', 'o.o', 'O.O', 'QUQ', '\'.\'', '¬u¬', '¬U¬', 'o\'o', 'o.o\'\'', 'o-o', 'O.o', 'x.x', 'ouo', 'OUO', 'OuO', 'Ouo', '>:C',
+			'>:c', 'OwO', 'UwU', '`o´', '`O´', '`U´', '`u´', ':3c', '^O^/', ':oc', 'x\'d', 'x\'D', 'X\'D', ';u;', 'ºoº', 'ºOº', 'ºuº', 'ºUº', 'OnO', '>U<', '>u<', '>O<', '>o<',
+			'\\^O^/', '\\`u´/', '\\´u`/', '\'n\'', '^w^', '^W^', 'ouO', '>:3c', '\'o\'/', '\'u\'/', '\'O\'/', '\'U\'/', '>w<', '>W<'
+		];
+		const selection = getRandomInt(emot.length);
+		message.channel.send(`**${emot[selection]}**`);
     },
 };
