dd1650033cee5532addd363a6dbb477f7059864c
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-30 18:23:09 -0300

    Update F1

--- commands/Pure/m-info.js
+++ commands/Pure/m-info.js
@@ -9,12 +9,13 @@ module.exports = {
     ],
 	execute(message, args) {
 		if(message.member.hasPermission('MANAGE_ROLES', false, true, true)) {
+			const servidor = message.channel.guild;
 			let textcnt = 0;//csm
 			let voicecnt = 0;
 			let categorycnt = 0;
-			let peoplecnt = guild.members.filter(member => !member.user.bot).size;
-			let botcnt = guild.memberCount - peoplecnt;
-			message.guild.channels.forEach(channel => {
+			let peoplecnt = servidor.members.filter(member => !member.user.bot).size;
+			let botcnt = servidor.memberCount - peoplecnt;
+			servidor.channels.forEach(channel => {
 				if(channel.type === 'text') textnct++;
 				else if(channel.type === 'voice') voicecnt++;
 				else if(channel.type === 'category') categorycnt++;
@@ -24,16 +25,16 @@ module.exports = {
 			const Embed = new Discord.RichEmbed()
 				.setColor('#ffd500')
 				.setTitle('Información del servidor OwO')
-				.addField('Nombre', message.guild.name)
+				.addField('Nombre', servidor.name)
 				.addField('Usuarios', `:wrestlers: x ${peoplecnt}\n:robot: x ${botcnt}`, true)
 				.addField('Canales', `:hash: x ${textcnt}\n:loud_sound: x ${voicecnt}\n:label: x ${categorycnt}`, true)
 
-				.addField('Región', message.guild.region, false)
-				.addField('Creador', `${message.guild.owner.username}\n[${message.guild.owner.id}]`, true)
-				.addField('Nivel de verificación', message.guild.verificationLevel, true)
+				.addField('Región', servidor.region, false)
+				.addField('Creador', `${servidor.owner.username}\n[${servidor.owner.id}]`, true)
+				.addField('Nivel de verificación', servidor.verificationLevel, true)
 
-				.addField('Fecha de creación', message.guild.createdAt, false)
-				.addField('ID', message.guild.id, true)
+				.addField('Fecha de creación', servidor.createdAt, false)
+				.addField('ID', servidor.id, true)
 
 				.addImage(message.guild.iconURL);
 			tmpch.send(Embed);
