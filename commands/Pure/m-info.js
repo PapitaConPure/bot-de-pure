da9e7ae0c7fce955e38efc5702c090363839ab1c
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-30 18:32:17 -0300

    Update F1

--- commands/Pure/m-info.js
+++ commands/Pure/m-info.js
@@ -10,7 +10,7 @@ module.exports = {
 	execute(message, args) {
 		if(message.member.hasPermission('MANAGE_ROLES', false, true, true)) {
 			const servidor = message.channel.guild;
-			let textcnt = 0;//csm
+			let textcnt = 0;
 			let voicecnt = 0;
 			let categorycnt = 0;
 			let peoplecnt = servidor.members.filter(member => !member.user.bot).size;
