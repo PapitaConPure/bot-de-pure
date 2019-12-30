52132632a1d123f8ace50e047cf5ac92e363c4a1
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-30 04:10:06 -0300

    Update F1

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -17,13 +17,18 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 	//Nombres de tohas a tags
 	for(let i = 0; i < arglist.length; i++) {
 		switch(arglist[i].toLowerCase()) {
+			//#region Protagonistas
 			case 'reimu': arglist[i] = 'hakurei_reimu'; break;
 			case 'marisa': arglist[i] = 'kirisame_marisa'; break;
+			//#endregion
+			//#region EoSD (6)
 			case 'meiling': arglist[i] = 'hong_meiling'; break;
 			case 'patchouli': arglist[i] = 'patchouli_knowledge'; break;
 			case 'sakuya': arglist[i] = 'izayoi_sakuya'; break;
 			case 'remilia': arglist[i] = 'remilia_scarlet'; break;
 			case 'flandre': arglist[i] = 'flandre_scarlet'; break;
+			//#endregion
+			//#region PCB (7)
 			case 'letty': arglist[i] = 'letty_whiterock'; break;
 			case 'alice': arglist[i] = 'alice_margatroid'; break;
 			case 'shanghai': arglist[i] = 'shanghai_doll'; break;
@@ -36,7 +41,8 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'yuyuko': arglist[i] = 'saigyouji_yuyuko'; break;
 			case 'ran': arglist[i] = 'yakumo_ran'; break;
 			case 'yukari': arglist[i] = 'yakumo_yukari'; break;
-			case 'suika': arglist[i] = 'ibuki_suika'; break;
+			//#endregion
+			//#region IN (8)
 			case 'wriggle': arglist[i] = 'wriggle_nightbug'; break;
 			case 'mystia': arglist[i] = 'mystia_lorelei'; break;
 			case 'keine': arglist[i] = 'kamishirasawa_keine'; break;
@@ -45,11 +51,15 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'eirin': arglist[i] = 'yagokoro_eirin'; break;
 			case 'kaguya': arglist[i] = 'houraisan_kaguya'; break;
 			case 'mokou': arglist[i] = 'fujiwara_no_mokou'; break;
+			//#endregion
+			//#region PoFV (9)
 			case 'aya': arglist[i] = 'shameimaru_aya'; break;
 			case 'medicine': arglist[i] = 'medicine_melancholy'; break;
 			case 'yuuka': arglist[i] = 'kazami_yuuka'; break;
 			case 'komachi': arglist[i] = 'onozuka_komachi'; break;
 			case 'eiki': arglist[i] = 'shiki_eiki'; break;
+			//#endregion
+			//#region MoF (10)
 			case 'shizuha': arglist[i] = 'aki_shizuha'; break;
 			case 'minoriko': arglist[i] = 'aki_minoriko'; break;
 			case 'hina': arglist[i] = 'kagiyama_hina'; break;
@@ -58,10 +68,8 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'sanae': arglist[i] = 'kochiya_sanae'; break;
 			case 'kanako': arglist[i] = 'yasaka_kanako'; break;
 			case 'suwako': arglist[i] = 'moriya_suwako'; break;
-			case 'iku': arglist[i] = 'nagae_iku'; break;
-			case 'tenshi': arglist[i] = 'hinanawi_tenshi'; break;
-			case 'hatate': arglist[i] = 'himekaidou_hatate'; break;
-			case 'kokoro': arglist[i] = 'hata_no_kokoro'; break;
+			//#endregion
+			//#region SA (11)
 			case 'yamame': arglist[i] = 'kurodani_yamame'; break;
 			case 'parsee': arglist[i] = 'mizuhashi_parsee'; break;
 			case 'yuugi': arglist[i] = 'hoshiguma_yuugi'; break;
@@ -71,6 +79,17 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'utsuho': arglist[i] = 'reiuji_utsuho'; break;
 			case 'okuu': arglist[i] = 'reiuji_utsuho'; break;
 			case 'koishi': arglist[i] = 'komeiji_koishi'; break;
+			//#endregion
+			//#region UFO (12)
+			case 'kogasa': arglist[i] = 'tatara_kogasa'; break;
+			case 'ichirin': arglist[i] = 'kumoi_ichirin'; break;
+			case 'murasa': arglist[i] = 'murasa_minamitsu'; break;
+			case 'shou': arglist[i] = 'toramaru_shou'; break;
+			case 'byakuren': arglist[i] = 'hijiri_byakuren'; break;
+			case 'nue': arglist[i] = 'houjuu_nue'; break;
+			case 'nuee': arglist[i] = 'houjuu_nue'; break;
+			//#endregion
+			//#region TD (13)
 			case 'kyouko': arglist[i] = 'kasodani_kyouko'; break;
 			case 'yoshika': arglist[i] = 'miyako_yoshika'; break;
 			case 'seiga': arglist[i] = 'kaku_seiga'; break;
@@ -78,6 +97,8 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'futo': arglist[i] = 'mononobe_no_futo'; break;
 			case 'miko': arglist[i] = 'toyosatomimi_no_miko'; break;
 			case 'mamizou': arglist[i] = 'futatsuiwa_mamizou'; break;
+			//#endregion
+			//#region DDC (14)
 			case 'kagerou': arglist[i] = 'imaizumi_kagerou'; break;
 			case 'benben': arglist[i] = 'tsukumo_benben'; break;
 			case 'yatsuhashi': arglist[i] = 'tsukumo_yatsuhashi'; break;
@@ -85,14 +106,14 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'sukuna': arglist[i] = 'sukuna_shinmyoumaru'; break;
 			case 'shinmyoumaru': arglist[i] = 'sukuna_shinmyoumaru'; break;
 			case 'raiko': arglist[i] = 'horikawa_raiko'; break;
-			case 'usami': arglist[i] = 'usami_sumireko'; break;
-			case 'sumireko': arglist[i] = 'usami_sumireko'; break;
-			case 'jo\'on': arglist[i] = 'yorigami_jo\'on'; break;
-			case 'shion': arglist[i] = 'yorigami_shion'; break;
+			//#endregion
+			//#region LoLK (15)
 			case 'doremy': arglist[i] = 'doremy_sweet'; break;
 			case 'sagume': arglist[i] = 'kishin_sagume'; break;
 			case 'junko': arglist[i] = 'junko*'; break;
 			case 'hecatia': arglist[i] = 'hecatia_lapislazuli'; break;
+			//#endregion
+			//#region HSiFS (16)
 			case 'nemuno': arglist[i] = 'sakata_nemuno'; break;
 			case 'aunn': arglist[i] = 'komano_aun'; break;
 			case 'komano': arglist[i] = 'komano_aun'; break;
@@ -100,6 +121,8 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'satono': arglist[i] = 'nishida_satono'; break;
 			case 'mai': arglist[i] = 'teireida_mai'; break;
 			case 'okina': arglist[i] = 'matara_okina'; break;
+			//#endregion
+			//#region WBaWC (17)
 			case 'eika': arglist[i] = 'ebisu_eika'; break;
 			case 'urumi': arglist[i] = 'ushizaki_urumi'; break;
 			case 'kutaka': arglist[i] = 'niwatari_kutaka'; break;
@@ -107,17 +130,31 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			case 'mayumi': arglist[i] = 'joutouguu_mayumi'; break;
 			case 'keiki': arglist[i] = 'haniyasushin_keiki'; break;
 			case 'saki': arglist[i] = 'kurokoma_saki'; break;
+			//#endregion
+			//#region Fighters
+			case 'suika': arglist[i] = 'ibuki_suika'; break;
+			case 'iku': arglist[i] = 'nagae_iku'; break;
+			case 'tenshi': arglist[i] = 'hinanawi_tenshi'; break;
+			case 'kokoro': arglist[i] = 'hata_no_kokoro'; break;
+			case 'usami': arglist[i] = 'usami_sumireko'; break;
+			case 'sumireko': arglist[i] = 'usami_sumireko'; break;
+			case 'jo\'on': arglist[i] = 'yorigami_jo\'on'; break;
+			case 'shion': arglist[i] = 'yorigami_shion'; break;
+			//#endregion
+			//#region Novelas, Spin-off y CDs
 			case 'rinnosuke': arglist[i] = 'morichika_rinnosuke'; break;
+			case 'toyohime': arglist[i] = 'watatsuki_no_toyohime'; break;
+			case 'yorihime': arglist[i] = 'watatsuki_no_yorihime'; break;
+			case 'hatate': arglist[i] = 'himekaidou_hatate'; break;
 			case 'luna': arglist[i] = 'luna_child'; break;
 			case 'star': arglist[i] = 'star_sapphire'; break;
 			case 'sunny': arglist[i] = 'sunny_milk'; break;
-			case 'toyohime': arglist[i] = 'watatsuki_no_toyohime'; break;
-			case 'yorihime': arglist[i] = 'watatsuki_no_yorihime'; break;
-			case 'kasen': arglist[i] = 'ibaraki_kasen'; break;
 			case 'kosuzu': arglist[i] = 'motoori_kosuzu'; break;
 			case 'maribel': arglist[i] = 'maribel_hearn'; break;
 			case 'renko': arglist[i] = 'usami_renko'; break;
 			case 'akyuu': arglist[i] = 'hieda_no_akyuu'; break;
+			case 'kasen': arglist[i] = 'ibaraki_kasen'; break;
+			//#endregion
 		}
 	}
 
@@ -131,7 +168,7 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 		embedcolor = '#fa7b62';
 		embedtitle = 'Tohas uwu';
 	}
-	let srchpg = 0;
+	let srchpg = 1;
 	if(arglist.length) {
 		if(isNaN(arglist[0])) srchtags += ` ${arglist[0]}`;
 		else {
@@ -179,7 +216,7 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 				global.imgcollector.on('collect', m => {
 					console.log(`Collected ${m.content}`);
 					console.log(BotMessage);
-					if(m.content.startsWith('d')) tmpch.fetchMessage(BotMessage).then(msg => msg.delete());
+					if(m.content.toLowerCase() === 'd') tmpch.fetchMessage(BotMessage).then(msg => msg.delete());
 					else global.imgcollector.stop();
 				});
 				global.imgcollector.on('end', collected => {
