5508b093c7d599bacb7402867fb1380e3dc7f54d
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-31 02:22:04 -0300

    Update F1

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -159,8 +159,8 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 	}
 
 	if(tmpch.nsfw) {
-		srchtags += 'explicit -lolicon -loli -shotacon -bestiality -rumia -cirno -remilia_scarlet -flandre_scarlet -chen -ibuki_suika -inaba_tewi';
-		srchtags += ' -kisume -sukuna_shinmyoumaru -clownpiece -komano_aun -ebisu_eika -luna_child -star_sapphire -sunny_milk -motoori_kosuzu -hieda_no_akyuu';
+		srchtags += 'explicit -lolicon -loli -shotacon -bestiality -rumia -cirno -remilia_scarlet -flandre_scarlet -chen -inaba_tewi';
+		srchtags += ' -kisume -sukuna_shinmyoumaru -clownpiece -ebisu_eika -luna_child -star_sapphire -sunny_milk -motoori_kosuzu -hieda_no_akyuu';
 		embedcolor = '#38214e';
 		embedtitle = 'Tohitas O//w//O';
 	} else {
