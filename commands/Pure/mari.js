f2140dc0e7a4546d4a76a1c24247474e76de5024
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-30 22:33:58 -0300

    Update F1

--- commands/Pure/mari.js
+++ commands/Pure/mari.js
@@ -17,7 +17,7 @@ module.exports = {
 		const emot = [
 			'\'o\'', '\'O\'', '\'u\'', '\'U\'', '^O^', 'o.o', 'O.O', 'QUQ', '\'.\'', '¬u¬', '¬U¬', 'o\'o', 'o.o\'\'', 'o-o', 'O.o', 'x.x', 'ouo', 'OUO', 'OuO', 'Ouo', '>:C',
 			'>:c', 'OwO', 'UwU', '`o´', '`O´', '`U´', '`u´', ':3c', '^O^/', ':oc', 'x\'d', 'x\'D', 'X\'D', ';u;', '°o°', '°O°', '°u°', '°U°', 'OnO', '>U<', '>u<', '>O<', '>o<',
-			'\\^O^/', '\\`u´/', '\\´u`/', '\'n\'', '^w^', '^W^', 'ouO', '>:3c', '\'o\'/', '\'u\'/', '\'O\'/', '\'U\'/', '>w<', '>W<'
+			'\\^O^/', '\\`u´/', '\\´u`/', '\'n\'', '^w^', '^W^', 'ouO', '>:3c', '\'o\'/', '\'u\'/', '\'O\'/', '\'U\'/', '>w<', '>W<', '°w°', '°W°'
 		];
 		const selection = getRandomInt(emot.length);
 		message.channel.send(`**${emot[selection]}**`);
