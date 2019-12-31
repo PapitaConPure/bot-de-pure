bf31fdec592006775b30a70cca1d7acd35b20062
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2019-12-30 21:09:40 -0300

    Update F1

--- commands/Pure/mari.js
+++ commands/Pure/mari.js
@@ -16,7 +16,7 @@ module.exports = {
 	execute(message, args){
 		const emot = [
 			'\'o\'', '\'O\'', '\'u\'', '\'U\'', '^O^', 'o.o', 'O.O', 'QUQ', '\'.\'', '¬u¬', '¬U¬', 'o\'o', 'o.o\'\'', 'o-o', 'O.o', 'x.x', 'ouo', 'OUO', 'OuO', 'Ouo', '>:C',
-			'>:c', 'OwO', 'UwU', '`o´', '`O´', '`U´', '`u´', ':3c', '^O^/', ':oc', 'x\'d', 'x\'D', 'X\'D', ';u;', 'ºoº', 'ºOº', 'ºuº', 'ºUº', 'OnO', '>U<', '>u<', '>O<', '>o<',
+			'>:c', 'OwO', 'UwU', '`o´', '`O´', '`U´', '`u´', ':3c', '^O^/', ':oc', 'x\'d', 'x\'D', 'X\'D', ';u;', '°o°', '°O°', '°u°', '°U°', 'OnO', '>U<', '>u<', '>O<', '>o<',
 			'\\^O^/', '\\`u´/', '\\´u`/', '\'n\'', '^w^', '^W^', 'ouO', '>:3c', '\'o\'/', '\'u\'/', '\'O\'/', '\'U\'/', '>w<', '>W<'
 		];
 		const selection = getRandomInt(emot.length);
