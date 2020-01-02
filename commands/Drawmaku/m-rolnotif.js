2922dc42a387ca6851a774cda922aaca4ef832c6
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-01-02 02:54:48 -0300

    Update F1

--- commands/Drawmaku/m-rolnotif.js
+++ commands/Drawmaku/m-rolnotif.js
@@ -10,19 +10,25 @@ module.exports = {
     ],
 	execute(message, args) {
         if(!func.notModerator(message.member)) {
-            if(!message.member.channel.guild.roles.some(role => role.id === global.notroles)) {
-                message.channel.send(':warning: el parámetro especificado no es un rol o una ID de rol.');
+            const rol = message.channel.guild.roles.find(args[0])
+            if(rol === undefined && !message.mentions.roles.size) {
+                message.channel.send('El argumento especificado no parece ser un rol o una ID de rol.');
                 return;
             }
             if(args.length === 1) {
-                if(args[1].startsWith('<@') && args[1].endsWith('>')) {
-                    args[1] = args[1].slice(2, -1);
-                    if(args[1].startsWith('&')) args[1] = args[1].slice(1);
-                }
+                if(rol === undefined) {
+                    if(args[0].startsWith('<@') && args[0].endsWith('>')) {
+                        args[0] = args[0].slice(2, -1);
+                        if(args[0].startsWith('&')) args[0] = args[0].slice(1);
+                    }
 
+                    if(global.notroles === 'na') message.channel.send(`:white_check_mark: rol ${args[0]} establecido como rol de notificación de Drawmaku.`);
+                    else message.channel.send(`:white_check_mark: rol ${args[0]} sobreescrito en los roles de notificación de Drawmaku.`);
+                } else {
+                    if(global.notroles === 'na') message.channel.send(`:white_check_mark: rol ${rol.name} establecido como rol de notificación de Drawmaku.`);
+                    else message.channel.send(`:white_check_mark: rol ${rol.name} sobreescrito en los roles de notificación de Drawmaku.`);
+                }
                 global.notroles = args[0];
-                if(global.notroles === 'na') message.channel.send(`:white_check_mark: rol <@&${args[0]}> establecido como rol de notificación de Drawmaku.`);
-                else message.channel.send(`:white_check_mark: rol <@&${args[0]}> sobreescrito en los roles de notificación de Drawmaku.`);
             } else if(args.length === 0) {
                 if(global.notroles === 'na')
                     message.channel.send(
