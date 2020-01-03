ff1e69bc0c84b3d0e134c1f99c8be6e4dcee9c1e
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-01-02 22:37:44 -0300

    Update F1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,8 +22,8 @@ module.exports = {
       user : 'node',
       host : '212.83.163.1',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
+      repo : 'git@github.com:PapitaConPure/bot-de-pure.git',
+      path : '/var/www/bot-de-pure',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
