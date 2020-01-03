6542f57b5b5fe1d2aab57d2a1e3a2f82f74b348b
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-01-03 06:03:40 -0300

    Update F1

--- commands/Drawmaku/m-empezar.js
+++ commands/Drawmaku/m-empezar.js
@@ -36,8 +36,8 @@ module.exports = {
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n' + 
             '***INSCRIPCIONES ABIERTAS***\n' +
             `_¡Ya pueden comenzar a inscribirse a la ${global.edi}ª edición de Drawmaku!_\n\n` +
-            `Si quieren empezar sus dibujos ahora: la temática de esta edición será **${global.tem}**` +
-            `> ${global.desctem}` +
+            `Si quieren empezar sus dibujos ahora: la temática de esta edición será **${global.tem}**\n` +
+            `> ${global.desctem}\n` +
             `**Tienen ${tiempoinsc} para inscribirse.**\n` +
             `Para entrar al evento escribe \`${global.p_drmk}entrar\` ahora.\n` +
             `Para salir del evento escribe \`${global.p_drmk}salir\` en cualquier momento.\n` +
