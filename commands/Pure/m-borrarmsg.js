9c27d90cefce378e77d0a7874cf3eb85fb7abd35
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-01-03 04:34:48 -0300

    Update F1

--- commands/Pure/m-borrarmsg.js
+++ commands/Pure/m-borrarmsg.js
@@ -10,14 +10,22 @@ module.exports = {
 	execute(message, args) {
 		if(message.member.hasPermission('MANAGE_ROLES', false, true, true)) {
 			message.delete(message.author.lastMessageID);
+			if(!args.length) {
+				message.channel.send(':warning: debes especificar al menos una ID de un mensaje enviado por mí.');
+			}
 			for(let i = 0; i < args.length; i++)
 				message.channel.fetchMessage(args[i])
 				.then(msg => {
-					msg.delete();
-					console.log('Se ha borrado un mensaje con m-borrarmsg.');
+					if(msg.author.id === '651250669390528561') {
+						msg.delete();
+						console.log('Se ha borrado un mensaje con m-borrarmsg.');
+					} else {
+						message.channel.send(':warning: solo se pueden borrar mis mensajes con este comando.');
+						console.log('Se ha rechazado la petición para borrar mensaje con m-borrarmsg porque el mensaje no era del bot.');
+					}
 				})
 				.catch(err => {
-					console.log('Ha ocurrido un error al borrar un mensaje.');
+					console.log(':warning: ha ocurrido un error al borrar un mensaje.');
 					console.error(err);
 				});
 		} else message.channel.send(':warning: necesitas tener el permiso ***ADMINISTRAR ROLES** (MANAGE ROLES)* para usar este comando.');
