ac05359d9af69940d2ee000f090441be05f1664a
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-10 03:44:37 -0300

    Update F2

--- commands/Pure/prueba.js
+++ commands/Pure/prueba.js
@@ -3,19 +3,26 @@ const Canvas = require('canvas');
 let global = require('../../config.json'); //Variables globales
 
 async function dibujarBienvenida(msg) {
-    const canvas = Canvas.createCanvas(1000, 750);
+    const canvas = Canvas.createCanvas(1200, 750);
     const ctx = canvas.getContext('2d');
 
     const fondo = await Canvas.loadImage('./fondo.png');
     ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
 
+    const Texto = msg.author.username/*member*/;
+    let fontSize = 72;
+	while(ctx.measureText(Texto).width > (canvas.width - 200)) fontSize -= 2;
+	ctx.font = `${fontSize}px sans-serif`;
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(member.displayName, canvas.width / 2, 125 - fontSize / 2);
+
 	ctx.beginPath();
-	ctx.arc(500, 350, 150, 0, Math.PI * 2, true);
+	ctx.arc(500, 300, 150, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.clip();
 
     const avatar = await Canvas.loadImage(msg.member.user.displayAvatarURL/*member.user.displayAvatarURL*/);
-	ctx.drawImage(avatar, 350, 200, 300, 300);
+	ctx.drawImage(avatar, canvas.width / 2 - 150, 150, 300, 300);
 
     const imagen = new Discord.Attachment(canvas.toBuffer(), 'bienvenida.png');
     msg.channel.send(`Welcome to the server, ${msg.author.username/*member*/}!`, imagen);
