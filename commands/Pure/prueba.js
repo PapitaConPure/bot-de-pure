1a5ab215a5692a7b0bc44c6f288ec43dfe8d0ba5
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-10 02:17:46 -0300

    Update F2

--- commands/Pure/prueba.js
+++ commands/Pure/prueba.js
@@ -5,7 +5,7 @@ let global = require('../../config.json'); //Variables globales
 async function dibujarBienvenida(msg) {
     const canvas = Canvas.createCanvas(700, 250);
     const ctx = canvas.getContext('2d');
-    const fondo = await Canvas.loadImage('../fondo.png');
+    const fondo = await Canvas.loadImage('./fondo.png');
     ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
     const imagen = new Discord.Attachment(canvas.toBuffer(), 'bienvenida.png');
 
