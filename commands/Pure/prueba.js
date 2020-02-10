d6e3a0e6fd3d75da1017677a1f92858d28e61126
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-10 04:02:31 -0300

    Update F2

--- commands/Pure/prueba.js
+++ commands/Pure/prueba.js
@@ -14,10 +14,10 @@ async function dibujarBienvenida(msg) {
 	while(ctx.measureText(Texto).width > (canvas.width - 200)) fontSize -= 2;
 	ctx.font = `${fontSize}px sans-serif`;
 	ctx.fillStyle = '#ffffff';
-	ctx.fillText(msg.member.displayName, canvas.width / 2 - ctx.measureText(Texto).width / 2, 20);
+	ctx.fillText(msg.member.displayName, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), 125 - fontSize / 2);
 
 	ctx.beginPath();
-	ctx.arc(500, 300, 150, 0, Math.PI * 2, true);
+	ctx.arc(canvas.width, 300, 150, 0, Math.PI * 2, true);
 	ctx.closePath();
 	ctx.clip();
 
