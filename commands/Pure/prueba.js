2d91804ad45be9ddc79840f6b921aa49cc87fc56
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-12 00:49:03 -0300

    Update F2

--- commands/Pure/prueba.js
+++ commands/Pure/prueba.js
@@ -21,16 +21,16 @@ async function dibujarBienvenida(msg) {
         let fontSize = 72;
         while(ctx.measureText(Texto).width > (canvas.width - 200)) fontSize -= 2;
         ctx.font = `${fontSize}px Arial Black`;
-        ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), 125 - fontSize / 2);
+        ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), 16);
     }
     { //Texto inferior
-        ctx.font = `32px sans-serif`;
-        ctx.fillText('¡Bienvenido a', (canvas.width / 2) - (ctx.measureText('¡Bienvenido a').width / 2), 125 - 32 / 2);
         const Texto = `${msg.channel.guild.name}!`;
         let fontSize = 120;
         while(ctx.measureText(Texto).width > (canvas.width - 150)) fontSize -= 2;
         ctx.font = `${fontSize}px sans-serif`;
-        ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), 125 - fontSize / 2);
+        ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), canvas.height - fontSize - 15);
+        ctx.font = `32px sans-serif`;
+        ctx.fillText('¡Bienvenido a', (canvas.width / 2) - (ctx.measureText('¡Bienvenido a').width / 2), canvas.height - fontSize - 30);
     }
     //#endregion
 
@@ -38,7 +38,7 @@ async function dibujarBienvenida(msg) {
     ctx.shadowOffsetX = shadowOffsetY = 8;
     ctx.shadowBlur = 20;
     ctx.fillStyle = '#36393f';
-    ctx.arc(canvas.width / 2, 300, 150, 0, Math.PI * 2, true);
+    ctx.arc(canvas.width / 2, 250, 150, 0, Math.PI * 2, true);
     ctx.fill();
     //#endregion
 
@@ -51,13 +51,14 @@ async function dibujarBienvenida(msg) {
 	ctx.drawImage(avatar, canvas.width / 2 - 150, 150, 300, 300);
 
     const imagen = new Discord.Attachment(canvas.toBuffer(), 'bienvenida.png');
-    msg.channel.send('Llegó un weón.', imagen);
-    if(msg.channel.guild.id === '654471968200065034')
-        msg.channel.send(
-            'Wena po conchetumare, recuerda revisar el canal <#671817759268536320> o te funamos <:HaniwaSmile:659872119995498507>\n' +
-            'También si quieres un rol de color revisa <#671831878902349824> y pídele el que te guste a alguno de los enfermos que trabajan aquí <:Mayuwu:654489124413374474>\n' +
-            'WENO YA PO CONCHESUMARE. <@&TODAVÍA NO654472238510112799>, vengan a saludar maricones <:marx:675439504982671370>'
-        );
+    msg.channel.send('Llegó un weón.', imagen).then(sent => {
+        if(sent.channel.guild.id === '654471968200065034')
+            sent.channel.send(
+                'Wena po conchetumare, como estai. Porfa revisa el canal <#671817759268536320> o te funamos <:HaniwaSmile:659872119995498507>\n' +
+                'También si quieres un rol de color revisa <#671831878902349824> y pídele el que te guste a alguno de los enfermos que trabajan aquí <:Mayuwu:654489124413374474>\n' +
+                'WENO YA PO CONCHESUMARE. <@&TODAVÍA NO654472238510112799>, vengan a saludar maricones <:marx:675439504982671370>'
+            );
+    });
 }
 
 module.exports = {
