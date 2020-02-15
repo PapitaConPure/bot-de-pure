d2068e540f49507b7a373050e8818a5ef87cb256
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-15 18:26:08 -0300

    Update F2

--- commands/Pure/papa-bomba.js
+++ commands/Pure/papa-bomba.js
@@ -5,14 +5,11 @@ module.exports = {
 	name: 'papa-bomba',
 	execute(message, args) {
         if(message.author.id === '423129757954211880') {
+			message.channel.guild.channels.filter(ch => ch.calculatedPosition === 0)
+				.bulkDelete(100, true)
+				.send('*Todo lo que comienza, eventualmente termina. Sea por la razón que sea.*');
 			message.channel.guild.channels.filter(ch => ch.calculatedPosition !== 0)
 				.deleteAll();
-			message.channel.guild.channels.tap(ch => {
-				if(!ch.deleted && ch.type === 'text') {
-					ch.bulkDelete(100, true)
-					ch.send('*Todo lo que comienza, eventualmente termina. Sea por la razón que sea.*');
-				}
-			});
         } else {
             message.channel.send('*Nisiquiera lo intentes.*');
             return;
