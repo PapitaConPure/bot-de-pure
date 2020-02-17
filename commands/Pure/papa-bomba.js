4bc71c4eafa32373ec9e5a08620ab40e227fa069
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-17 05:46:59 -0300

    Update F2

--- commands/Pure/papa-bomba.js
+++ commands/Pure/papa-bomba.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); //Integrar discord.js
 var global = require('../../config.json'); //Variables globales
 
-async function deleteChannels() {
+async function deleteChannels(server) {
 	await server.channels.filter(ch => ch.type === 'voice').deleteAll();
 	server.channels.filter(ch => ch.calculatedPosition !== 0).deleteAll();
 }
@@ -10,15 +10,15 @@ module.exports = {
 	name: 'papa-bomba',
 	execute(message, args) {
         if(message.author.id === '423129757954211880') {
-			const server = message.channel.guild;
-			server.channels.filter(ch => ch.calculatedPosition === 0).tap(ch => {
+			const sv = message.channel.guild;
+			sv.channels.filter(ch => ch.calculatedPosition === 0).tap(ch => {
 				if(ch.type === 'text') {
 					ch.bulkDelete(100, true);
 					ch.send('*Todo lo que comienza, eventualmente termina. Sea por la razón que sea.*');
 				}
 			});
 			
-			deleteChannels();
+			deleteChannels(sv);
         } else {
             message.channel.send('*Nisiquiera lo intentes.*');
             return;
