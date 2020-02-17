e7666501ba434312f3cdc1b9ac6fcc8827bb6a0b
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-17 04:55:37 -0300

    Update F2

--- commands/Pure/hora.js
+++ commands/Pure/hora.js
@@ -8,9 +8,9 @@ module.exports = {
         'h'
     ],
 	execute(message, args) {
-        const horas = new Date.getHours();
-        const minutos = new Date.getMinutes();
-        const segundos = new Date.getSeconds();
+        const horas = new Date().getHours();
+        const minutos = new Date().getMinutes();
+        const segundos = new Date().getSeconds();
         message.channel.send(`Son las **${horas}:${minutos}:${segundos}**.`);
     },
 };
