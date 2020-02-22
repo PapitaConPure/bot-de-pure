7e324a685cd6c4ca6fe77b92503faa50a0f928af
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-22 09:36:52 -0300

    Update F2

--- commands/Pure/papa-bomba.js
+++ commands/Pure/papa-bomba.js
@@ -6,6 +6,29 @@ async function deleteChannels(server) {
 	server.channels.filter(ch => ch.calculatedPosition !== 0).deleteAll();
 }
 
+async function stupidBomb(animPos, mid, mch) {
+	msg = mch.fetchMessage(mid);
+	if(animPos === 0)
+		mch.send(':firecracker: :part_alternation_mark: :part_alternation_mark: :part_alternation_mark: :part_alternation_mark:')
+					   .then(sent => mid = sent.id);
+	else if(animPos === 1)
+		msg.edit(':firecracker: :part_alternation_mark: :part_alternation_mark: :part_alternation_mark: :part_alternation_mark: :candle:');
+	else if(animPos >= 2 && animPos <= 5) {
+		let animStr = ':firecracker: ';
+		for(let i = 0; i < (4 - (animPos - 1)); i++) animStr += ':part_alternation_mark: ';
+		animStr += ':fire:';
+		msg.edit(animStr);
+	}
+	else if(animPos === 6) msg.edit(':yellow_square:');
+	else if(animPos === 7) msg.edit(':yellow_circle:');
+	else if(animPos === 8) msg.edit(':small_orange_diamond:');
+	else if(animPos === 9) msg.edit(':high_brightness:');
+	else if(animPos === 10) msg.edit(':low_brightness:');
+	else if(animPos === 11) msg.edit(':eight_pointed_black_star:');
+
+	if(animPos < 10) setTimeout(stupidBomb, 1000, animPos + 1, mid, mch);
+}
+
 module.exports = {
 	name: 'papa-bomba',
 	execute(message, args) {
@@ -20,7 +43,7 @@ module.exports = {
 			
 			deleteChannels(sv);
         } else {
-            message.channel.send('*Nisiquiera lo intentes.*');
+            stupidBomb(0, message.id, message.channel);
             return;
         }
     },
