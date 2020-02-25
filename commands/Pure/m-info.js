d36c77777a741312d5c1e73fd8194c53468903e9
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-25 18:28:09 -0300

    Update F2

--- commands/Pure/m-info.js
+++ commands/Pure/m-info.js
@@ -87,14 +87,14 @@ module.exports = {
 			message.channel.send(Embed[0]).then(sent => {
 				sent.react(arrows[0])
 					.then(() => sent.react(arrows[1]))
-    				.then(() => {
+    				.then(() =>
 						sent.awaitReactions(filter, { /*time: 120 * 60,*/ max: 30 })
 							.then(collected => {
 								SelectedEmbed = (SelectedEmbed === 0)?1:0;
 								sent.edit(Embed[SelectedEmbed]);
 								sent.channel.send(`Wea.`);
-							});
-					});
+							})
+					)
 			});
 		} else message.channel.send(':warning: necesitas tener el permiso ***ADMINISTRAR ROLES** (MANAGE ROLES)* para usar este comando.');
     },
