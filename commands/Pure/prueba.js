5d30b9e685f5fb53a2746ec7691641e01226b293
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-25 13:31:52 -0300

    Update F2

--- commands/Pure/prueba.js
+++ commands/Pure/prueba.js
@@ -2,89 +2,9 @@ const Discord = require('discord.js'); //Integrar discord.js
 var global = require('../../config.json'); //Variables globales
 const Canvas = require('canvas');
 
-async function dibujarMillion(msg) { //Dar bienvenida a un miembro nuevo de un servidor
-    const canal = msg.channel; //Canal de mensajes de sistema
-
-    //#region Creación de imagen
-    const canvas = Canvas.createCanvas(1500, 750);
-    const ctx = canvas.getContext('2d');
-
-    const fondo = await Canvas.loadImage('./fondo3.png');
-    ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
-    //#endregion
-
-    //#region Texto
-    //#region Propiedades de texto
-    ctx.textBaseline = 'bottom';
-    ctx.shadowOffsetX = shadowOffsetY = 2;
-    ctx.shadowBlur = 10;
-    ctx.shadowColor = 'black';
-    ctx.fillStyle = '#ffffff';
-    //#endregion
-
-    //#region Nombre del usuario
-    let Texto = `${msg.author.username}`;
-    let fontSize = 72;
-    while(ctx.measureText(Texto).width > (canvas.width - 200)) fontSize -= 2;
-    ctx.font = `bold ${fontSize}px sans-serif`;
-    ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), 80);
-    //#endregion
-    
-    //#region Texto inferior
-    Texto = 'Uno en Un Millón';
-    fontSize = 120;
-    while(ctx.measureText(Texto).width > (canvas.width - 150)) fontSize -= 2;
-    ctx.font = `bold ${fontSize}px sans-serif`;
-    ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), canvas.height - 15);
-    Texto = '¡Felicidades! Tu mensaje es el destacado de';
-    ctx.font = `bold 48px sans-serif`;
-    ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), canvas.height - fontSize - 30);
-    //#endregion
-    //#endregion
-
-    //#region Foto de Perfil
-    //#region Sombra
-    const ycenter = (80 + (canvas.height - fontSize - 48 - 30)) / 2;
-    ctx.shadowOffsetX = shadowOffsetY = 8;
-    ctx.shadowBlur = 20;
-    ctx.fillStyle = '#36393f';
-    ctx.arc(canvas.width / 2, ycenter, 150, 0, Math.PI * 2, true);
-    ctx.fill();
-    //#endregion
-
-    //#region Imagen circular
-	ctx.beginPath();
-	ctx.arc(canvas.width / 2, ycenter, 150, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-    const avatar = await Canvas.loadImage(msg.author.avatarURL);
-	ctx.drawImage(avatar, canvas.width / 2 - 150, ycenter - 150, 300, 300);
-    //#endregion
-    //#endregion
-
-    const imagen = new Discord.Attachment(canvas.toBuffer(), 'bienvenida.png');
-
-    //#region Imagen y Mensaje extra
-    canal.send('', imagen).then(sent => {
-        if(servidor.id === '654471968200065034') { //Hourai Doll
-            canal.send(
-                `*Wao, <@${msg.author.id}>, tu mensaje fue seleccionado de entre un millón de otros mensajes. No ganaste nada, pero felicidades <:marx:675439504982671370>*\n` +
-                '*Bueno, de hecho, te ganaste esta imagen personalizada para presumir a los demás tu __suerte de uno en un millón__ <:sakiGyate:659872130216755220>*'
-            );
-        } else { //Animal Realm
-            canal.send(
-                `***ES:** ¡WOAH, FELICIDADES <@${msg.author.id}>! ¡Este mensaje fue nominado como uno en un millón!*\n` +
-                '*Realmente no ganaste nada. Pero hey, ¡ahora tienes esta imagen personalizada para presumir tu __suerte de uno en un millón__!*\n\n' +
-                `***EN:** WOAH, CONGRATZ <@${msg.author.id}>! This message has been nominated as one in a million!*\n` +
-                `*You really didn't win anything. But hey, now you have this customized image to show off your __one in a million luck__!*`
-            );
-        }
-    });
-    //#endregion
-}
 module.exports = {
 	name: 'prueba',
 	execute(message, args){
-		dibujarMillion(message);
+		message.channel.send('Comando de prueba. Actualmente vacío.');
     },
 };
--- index.js
+++ index.js
@@ -36,6 +36,93 @@ client.on('ready', () => { //Confirmación de inicio y cambio de estado
     //func.saveState();//func.reloadState();
 });
 
+async function dibujarMillion(msg) { //Dar bienvenida a un miembro nuevo de un servidor
+    const canal = msg.channel; //Canal de mensajes de sistema
+
+    //#region Creación de imagen
+    const canvas = Canvas.createCanvas(1500, 750);
+    const ctx = canvas.getContext('2d');
+
+    const fondo = await Canvas.loadImage('./fondo3.png');
+    ctx.drawImage(fondo, 0, 0, canvas.width, canvas.height);
+    //#endregion
+
+    //#region Texto
+    //#region Propiedades de texto
+    ctx.textBaseline = 'bottom';
+    ctx.shadowOffsetX = shadowOffsetY = 2;
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = 'black';
+    ctx.fillStyle = '#ffffff';
+    //#endregion
+
+    //#region Nombre del usuario
+    let Texto = `${msg.author.username}`;
+    let fontSize = 72;
+    while(ctx.measureText(Texto).width > (canvas.width - 200)) fontSize -= 2;
+    ctx.font = `bold ${fontSize}px sans-serif`;
+    ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), 80);
+    //#endregion
+    
+    //#region Texto inferior
+    Texto = 'Uno en Un Millón';
+    fontSize = 120;
+    while(ctx.measureText(Texto).width > (canvas.width - 150)) fontSize -= 2;
+    ctx.font = `bold ${fontSize}px sans-serif`;
+    ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), canvas.height - 15);
+    Texto = '¡Felicidades! Tu mensaje es el destacado de';
+    ctx.font = `bold 48px sans-serif`;
+    ctx.fillText(Texto, (canvas.width / 2) - (ctx.measureText(Texto).width / 2), canvas.height - fontSize - 30);
+    //#endregion
+    //#endregion
+
+    //#region Foto de Perfil
+    //#region Sombra
+    const ycenter = (80 + (canvas.height - fontSize - 48 - 30)) / 2;
+    ctx.shadowOffsetX = shadowOffsetY = 8;
+    ctx.shadowBlur = 20;
+    ctx.fillStyle = '#36393f';
+    ctx.arc(canvas.width / 2, ycenter, 150, 0, Math.PI * 2, true);
+    ctx.fill();
+    //#endregion
+
+    //#region Imagen circular
+	ctx.beginPath();
+	ctx.arc(canvas.width / 2, ycenter, 150, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+    const avatar = await Canvas.loadImage(msg.author.avatarURL);
+	ctx.drawImage(avatar, canvas.width / 2 - 150, ycenter - 150, 300, 300);
+    //#endregion
+    //#endregion
+
+    const imagen = new Discord.Attachment(canvas.toBuffer(), 'bienvenida.png');
+
+    //#region Imagen y Mensaje extra
+    canal.send('', imagen).then(sent => {
+        if(servidor.id === '654471968200065034') { //Hourai Doll
+            canal.send(
+                `*Wao, <@${msg.author.id}>, tu mensaje fue seleccionado de entre un millón de otros mensajes. No ganaste nada, pero felicidades <:marx:675439504982671370>*\n` +
+                '*Bueno, de hecho, te ganaste esta imagen personalizada para presumir a los demás tu __suerte de uno en un millón__ <:sakiGyate:659872130216755220>*\n' +
+                '```\n' +
+                `${msg.content}` +
+                '```\n'
+            );
+        } else { //Animal Realm
+            canal.send(
+                `***ES:** ¡WOAH, FELICIDADES <@${msg.author.id}>! ¡Este mensaje fue nominado como uno en un millón!*\n` +
+                '*Realmente no ganaste nada. Pero hey, ¡ahora tienes esta imagen personalizada para presumir tu __suerte de uno en un millón__!*\n\n' +
+                `***EN:** WOAH, CONGRATZ <@${msg.author.id}>! This message has been nominated as one in a million!*\n` +
+                `*You really didn't win anything. But hey, now you have this customized image to show off your __one in a million luck__!*\n\n` +
+                '```\n' +
+                `${msg.content}` +
+                '```\n'
+            );
+        }
+    });
+    //#endregion
+}
+
 client.on('message', message => { //En caso de recibir un mensaje
     if(global.cansay === 0) { if(message.author.bot) return; } //Hacer que el bot no sea un pelotudo (ignorar mensajes de bots)
     console.log(`[${message.guild.name}→#${message.channel.name}] ${message.author.username}: "${message.content}"`); //Hacer que el bot de hecho sea inteligente (messages log)
@@ -78,7 +165,7 @@ client.on('message', message => { //En caso de recibir un mensaje
     //#region Uno en un millón
     const millionchance = Math.floor(Math.random() * 1000000);
     if(millionchance === 0) {
-        message.channel.send(`<@${message.author.id}> Probando. Si de por casualidad te sale este mensaje mientras hablas, ignóralo uwu.`);
+        dibujarMillion(message);
     }
     //#endregion
     //#endregion
