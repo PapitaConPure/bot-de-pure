a6e393782ee296fb5ee28f324287863f591df784
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-24 22:52:34 -0300

    Update F2

--- commands/Pure/papa-bomba.js
+++ commands/Pure/papa-bomba.js
@@ -7,30 +7,30 @@ async function deleteChannels(server) {
 }
 
 async function stupidBomb(animLen, animPos, mid, mch) {
-	const msg = await mch.fetchMessage(mid);
-	if(animPos === 0) {
-		let animStr = ':firecracker: ';
-		for(let i = 0; i < animLen; i++) animStr += ':part_alternation_mark: ';
-		mch.send(animStr).then(sent => mid = sent.id);
-	} else if(animPos === 1) {
-		let animStr = ':firecracker: ';
-		for(let i = 0; i < animLen; i++) animStr += ':part_alternation_mark: ';
-		animStr += ':candle:';
-		msg.edit(animStr);
-	} else if(animPos >= 2 && animPos <= (animLen + 2)) {
-		let animStr = ':firecracker: ';
-		for(let i = 0; i < (animLen - (animPos - 1)); i++) animStr += ':part_alternation_mark: ';
-		animStr += ':fire:';
-		msg.edit(animStr);
-	}
-	else if(animPos === (animLen + 2)) msg.edit(':yellow_square:');
-	else if(animPos === (animLen + 3)) msg.edit(':yellow_circle:');
-	else if(animPos === (animLen + 4)) msg.edit(':small_orange_diamond:');
-	else if(animPos === (animLen + 5)) msg.edit(':high_brightness:');
-	else if(animPos === (animLen + 6)) msg.edit(':low_brightness:');
-	else if(animPos === (animLen + 7)) msg.edit(':eight_pointed_black_star:');
-
-	if(animPos < (8 + animLen)) setTimeout(stupidBomb, 3000, animLen, animPos + 1, mid, mch);
+	mch.fetchMessage(mid).then(msg => {
+		if(animPos === 0) {
+			let animStr = ':firecracker: ';
+			for(let i = 0; i < animLen; i++) animStr += ':part_alternation_mark: ';
+			mch.send(animStr).then(sent => mid = sent.id);
+		} else if(animPos === 1) {
+			let animStr = ':firecracker: ';
+			for(let i = 0; i < animLen; i++) animStr += ':part_alternation_mark: ';
+			animStr += ':candle:';
+			msg.edit(animStr);
+		} else if(animPos >= 2 && animPos <= (animLen + 2)) {
+			let animStr = ':firecracker: ';
+			for(let i = 0; i < (animLen - (animPos - 1)); i++) animStr += ':part_alternation_mark: ';
+			animStr += ':fire:';
+			msg.edit(animStr);
+		}
+		else if(animPos === (animLen + 2)) msg.edit(':yellow_square:');
+		else if(animPos === (animLen + 3)) msg.edit(':yellow_circle:');
+		else if(animPos === (animLen + 4)) msg.edit(':small_orange_diamond:');
+		else if(animPos === (animLen + 5)) msg.edit(':high_brightness:');
+		else if(animPos === (animLen + 6)) msg.edit(':low_brightness:');
+		else if(animPos === (animLen + 7)) msg.edit(':eight_pointed_black_star:');
+	});
+	if(animPos < (8 + animLen)) setTimeout(stupidBomb, 1000, animLen, animPos + 1, mid, mch);
 }
 
 module.exports = {
@@ -50,9 +50,9 @@ module.exports = {
 				deleteChannels(sv);
 			}
         } else {
-			let len = 4;
-			if(args.length) len = parseInt(args[0]);
-            stupidBomb(len, 0, message.id, message.channel);
+			let bomblen = 4;
+			if(args.length) bomblen = parseInt(args[0]);
+            stupidBomb(bomblen, 0, message.id, message.channel);
         }
     },
 };
