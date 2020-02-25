87d5a5dccef633fd74965bd231a60d26a502cb41
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-25 15:58:15 -0300

    Update F2

--- commands/Pure/uwu.js
+++ commands/Pure/uwu.js
@@ -4,12 +4,12 @@ var global = require('../../config.json'); //Variables globales
 module.exports = {
 	name: 'uwu',
 	execute(message, args) {
-		let uwusopt = [
+		const uwusopt = [
 			'<:uwu:681935702308552730>',
 			'<:uwu2:681936445958914128>',
 			'<:uwu3:681937073401233537>',
 			'<:uwu4:681937074047549467>'
-		]
+		];
 		message.channel.send(uwusopt[Math.floor(Math.random()) * uwusopt.length]);
     },
 };
