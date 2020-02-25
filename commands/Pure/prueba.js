3540aa25537a55353c394eee3aafc1e0d70ff5d4
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-02-25 15:35:37 -0300

    Update F2

--- commands/Pure/prueba.js
+++ commands/Pure/prueba.js
@@ -6,10 +6,10 @@ module.exports = {
 	name: 'prueba',
 	execute(message, args){
 		message.channel.send(
-			':edgenw::lineh::lineh::edgene:\n' +
-			':linev:\t\t\t:linev2:\n' +
-			':linev:\t\t\t:linev2:\n' +
-			':edgesw::lineh2::lineh2::edgese:'
+			'<:edgenw:681929350798377007><:lineh:681929351083982914><:lineh:681929351083982914><:edgene:681929351029194857>\n' +
+			'<:linev:681929350823542888>\t\t\t<:linev2:681930015860195391>\n' +
+			'<:linev:681929350823542888>\t\t\t<:linev2:681930015860195391>\n' +
+			'<:edgesw:681929351029063856><:lineh2:681930015440371763><:lineh2:681930015440371763><:edgese:681929351029063728>'
 		);
     },
 };
