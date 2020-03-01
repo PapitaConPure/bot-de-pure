01b6624f455235cda6afb0ef1a96e2f0e1b2a4f9
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-03-01 06:49:09 -0300

    Update F2

--- commands/Pure/random.js
+++ commands/Pure/random.js
@@ -18,6 +18,18 @@ module.exports = {
         for(let i = 0; i < 11; i++)
             videostr += randchar[Math.floor(Math.random() * 64)];
 
-        message.channel.send(`https://www.youtube.com/watch?v=${videostr}`);
+        message.channel.send(`https://www.youtube.com/watch?v=${videostr}`).then(sent => {
+            const filter = rct => rct.emoji.id === '683610811008024609';
+            sent.react(message.client.emojis.get('683610811008024609'))
+                .then(() => {
+                    const collector = sent.createReactionCollector(filter, { time: 8 * 60 * 1000 });
+                    collector.on('collect', reaction => {
+                        videostr = '';
+                        for(let i = 0; i < 11; i++)
+                            videostr += randchar[Math.floor(Math.random() * 64)];
+                        sent.edit(`https://www.youtube.com/watch?v=${videostr}`);
+                    });
+                });
+        });
     },
 };
