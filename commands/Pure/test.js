aec564c49240db01412627565f5b1d50ebb98df0
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-04-03 20:35:12 -0300

    awefawef

---
+++ commands/Pure/test.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js'); //Integrar discord.js
+var global = require('../../config.json'); //Variables globales
+
+module.exports = {
+	name: 'test',
+	execute(message, args) {
+		message.channel.send(`\`${message.member.roles.cache.size}\``);
+    },
+};
