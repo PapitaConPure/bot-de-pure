ec01a3545c832e292039ac23d798ace34a6abb2d
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-04-26 02:50:42 -0300

    Wea

--- commands/Pure/touhou.js
+++ commands/Pure/touhou.js
@@ -215,7 +215,7 @@ const tmpfunc = async function(tmpch, arglist, tmpauth) {
 			let showtag = ':mag_right: ';
 			if(!isNaN(arglist[0])) showpg += `[1~**${arglist[0]}**] => Seleccionada: ***${srchpg + 1}***`;
 			else showpg += 'No ingresaste un rango de páginas. `p!2hu <¿rango?> <¿etiquetas?>`'
-			if(customtags.length) showtag += `*${customtags.trim()}*`;
+			if(customtags.length) showtag += `*${customtags.replace('*', '\\*').trim()}*`;
 			else showtag += 'No ingresaste etiquetas. `p!2hu <¿rango?> <¿etiquetas?>`';
 			data.data.forEach(image => {
 				if(image !== undefined && i === selectedpic) {
