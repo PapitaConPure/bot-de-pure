1c9796a61854b8429ec7b53cb755b72dc51c7364
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-05-03 23:43:36 -0300

    wasd

--- commands/Pure/pingear.js
+++ commands/Pure/pingear.js
@@ -10,12 +10,11 @@ module.exports = {
 	execute(message, args) {
 		if(args.length === 2) {
 			let cnt = -1;
-			if(!isNaN(args[0])) {
-				if(args[0] >= 2 && args[0] <= 10) cnt = args[0];
-				else message.channel.send(':warning: solo puedes pingear a alguien entre 2 y 10 veces.');
-			}
-			
-			if(args[1].startsWith('<@') && args[1].endsWith('>')) {
+			if(!isNaN(args[0])) cnt = args[0];
+
+			if(cnt < 2 || cnt > 10) 
+				message.channel.send(':warning: solo puedes pingear a alguien entre 2 y 10 veces.');
+			else if(args[1].startsWith('<@') && args[1].endsWith('>')) {
 				func.pingear(cnt, args[1], message.channel); 
 			} else message.channel.send(':warning: debes ingresar un valor numérico y una mención (`p!pingear <número*> <mención*>`).');
 		} else message.channel.send(':warning: debes ingresar 2 parámetros (`p!pingear <número*> <mención*>`).');
