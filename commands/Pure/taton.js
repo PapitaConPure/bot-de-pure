15a33bed0c329301280228ddccd3e93e4b76ee0d
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-06-11 18:07:22 -0300

    wea

--- commands/Pure/taton.js
+++ commands/Pure/taton.js
@@ -8,32 +8,50 @@ module.exports = {
 		'perritos'
 	],
 	execute(message, args) {
-		const uwusopt = [
-			'<:perrito:720736050305171456>',
-			'<:otirrep:720736059251753180>',
-			'<:od:720736065673232404>',
-			'<:do:720736087131291678>',
-			'<:cerca:720736097948401686>',
-			'<:muycerca:720736106768891924>',
-			'<:lejos:720736112909221950>',
-			'<:muylejos:720736124007481424>',
-			'<:invertido:720736131368485025>',
-			'<:dormido:720736140935692389>',
-			'<:pistola:720736152348262500>',
-			'<:sad:720736161441644654>',
-			'<:gorrito:720736171642060868>',
-			'<:gorra:720736179883999401>',
-			'<:almirante:720736189782556807>',
-			'<:detective:720736199727251536>',
-			'<:ban:720736213773975613>',
-			'<:helado:720736228710023279>',
-			'<:corona:720736247089332225>',
-			'<:payaso:720736264373928030>',
-			'<:enojado:720736264382447697>',
-			'<:policia:720736265070444615>',
-			'<:ladron:720736300449398794>',
-			'<:importado:720736309290860666>'
+		const perritosopt = [
+			'perrito',		'720736050305171456',
+			'otirrep',		'720736059251753180',
+			'od',			'720736065673232404',
+			'do',			'720736087131291678',
+			'cerca',		'720736097948401686',
+			'muycerca',		'736106768891924',
+			'lejos',		'736112909221950',
+			'muylejos',		'720736124007481424',
+			'invertido',	'736131368485025',
+			'dormido',		'720736140935692389',
+			'pistola',		'720736152348262500',
+			'sad',			'720736161441644654',
+			'gorrito',		'720736171642060868',
+			'gorra',		'720736179883999401',
+			'almirante',	'720736189782556807',
+			'detective',	'720736199727251536',
+			'ban',			'720736213773975613',
+			'helado',		'720736228710023279',
+			'corona',		'720736247089332225',
+			'payaso',		'720736264373928030',
+			'enojado',		'720736264382447697',
+			'policia',		'720736265070444615',
+			'ladron',		'720736300449398794',
+			'importado',	'720736309290860666'
 		];
-		message.channel.send(uwusopt[Math.floor(Math.random() * uwusopt.length)]);
+
+		if(!args.length) {
+			const randperrito = Math.floor(Math.random() * perritosopt.length) * 2;
+
+			message.channel.send(`<:${perritosopt[randperrito]}:${perritosopt[randperrito + 1]}>`);
+		} else {
+			if(args[0] === 'todo' || args[0] === 'everything' || args[0] === 'all')
+				message.channel.send(
+					'**Emote**\t**Nombre**\n' +
+					message.client.guilds.get('676251911850164255')
+					.emojis.map(emote => `<:${emote.name}:${emote.id}>\t\t${emote.name}`)
+					.join('\n')
+				);
+			else
+				message.client.guilds.get('676251911850164255').emojis.map(emote => {
+					if(emote.name.startsWith(args[0]))
+						message.channel.send(`<:${emote.name}:${emote.id}>`);
+				});
+		}
     },
 };
