d4fb4e058472dfc06d3f17b9fb2d22007ee9bdd7
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-06-16 03:30:54 -0300

    wasd

---
+++ commands/Pure/karl.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js'); //Integrar discord.js
+var global = require('../../config.json'); //Variables globales
+
+module.exports = {
+	name: 'bern',
+	aliases: [
+        'bewny', 'bernbailando',
+		'berndance'
+    ],
+	execute(message, args){
+		const emot = [
+			':musical_keyboard:', ':saxophone:', ':trumpet:', ':violin:', ':guitar:',' :banjo:', ':aquarius:'
+		];
+		message.channel.send(
+			`**Buenas, soy Karl. Combina estas weás, créeme soy licenciado** <:reibu:686220828773318663> :thumbs_up:\n` +
+			`<:arrowr:681963688411922460> ${emot[getRandomInt(emot.length)]} ${emot[getRandomInt(emot.length)]} ${emot[getRandomInt(emot.length)]} <:arrowl:681963688361590897>`
+		);
+    },
+};
