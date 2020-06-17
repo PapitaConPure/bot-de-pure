ea820dea525acbbd2dc4cc90f1312b205316624f
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-06-17 09:14:00 -0300

    w

--- commands/Pure/presentarse.js
+++ commands/Pure/presentarse.js
@@ -15,9 +15,9 @@ module.exports = {
             'Habiendo sido creado como un proyecto de prueba para un evento competitivo de una pequeña comunidad, actualmente me encuentro como un bot bastante decente y con varias funcionalidades extra que ya distorsionaron mi propósito original por completo.\n' +
             '¡No dudes en investigar lo que puedo hacer! Al menos una risa te vas a llevar.\n\n' +
             '```css\n' +
-            `Versión actual: ${global.bot_version}\n` +
-            `Estado: ${global.bot_status}\n` +
-            `Desarrollador: Papita con Puré#6932 (@423129757954211880)` +
+            `[Versión actual]: ${global.bot_version}\n` +
+            `[Estado]: ${global.bot_status}\n` +
+            `[Desarrollador]: Papita con Puré#6932 (@423129757954211880)` +
             '```\n' +
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n'
         );
