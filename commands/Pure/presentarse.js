4a2cd28ebcf087abb3a77c1e64af2c97cdf06ace
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-06-17 09:16:22 -0300

    wasd

--- commands/Pure/presentarse.js
+++ commands/Pure/presentarse.js
@@ -11,9 +11,9 @@ module.exports = {
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n' +
             '***PRESENTACIÓN***\n\n' +
             '*¡Permíteme presentarme!*\n' +
-            '¡Hola! Soy __Bot de Puré__, un bot dedicado al entretenimiento con tecnologías de complemento de chat y de búsqueda de imágenes. ' +
-            'Habiendo sido creado como un proyecto de prueba para un evento competitivo de una pequeña comunidad, actualmente me encuentro como un bot bastante decente y con varias funcionalidades extra que ya distorsionaron mi propósito original por completo.\n' +
-            '¡No dudes en investigar lo que puedo hacer! Al menos una risa te vas a llevar.\n\n' +
+            '¡Hola! Soy __Bot de Puré__, un bot dedicado al entretenimiento con tecnologías de complemento de chat y de búsqueda de imágenes.\n' +
+            'Habiendo sido creado como un proyecto de prueba para un evento competitivo de una pequeña comunidad, actualmente me encuentro como un bot bastante decente y con varias funcionalidades extra que ya distorsionaron mi propósito original por completo.' +
+            ' ¡No dudes en investigar lo que puedo hacer! Al menos una risa te vas a llevar.\n\n' +
             '```css\n' +
             `[Versión actual]: ${global.bot_version}\n` +
             `[Estado]: ${global.bot_status}\n` +
