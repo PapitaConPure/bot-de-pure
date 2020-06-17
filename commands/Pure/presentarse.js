9afe662ed359f0c4ccd7610dec89a5ac11ea3c89
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-06-17 09:18:58 -0300

    wasd

--- commands/Pure/presentarse.js
+++ commands/Pure/presentarse.js
@@ -4,20 +4,20 @@ var global = require('../../config.json'); //Variables globales
 module.exports = {
 	name: 'presentarse',
     aliases: [
-        'presentacion', 'presentación', 'hola', 'presentar'
+        'presentacion', 'presentación', 'hola', 'presentar', 'puré', 'pure'
     ],
 	execute(message, args) {
         message.channel.send(
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n' +
-            '***PRESENTACIÓN***\n\n' +
-            '*¡Permíteme presentarme!*\n' +
+            '***PRESENTACIÓN***\n' +
+            '*¡Permíteme presentarme!*\n\n' +
             '¡Hola! Soy __Bot de Puré__, un bot dedicado al entretenimiento con tecnologías de complemento de chat y de búsqueda de imágenes.\n' +
             'Habiendo sido creado como un proyecto de prueba para un evento competitivo de una pequeña comunidad, actualmente me encuentro como un bot bastante decente y con varias funcionalidades extra que ya distorsionaron mi propósito original por completo.' +
             ' ¡No dudes en investigar lo que puedo hacer! Al menos una risa te vas a llevar.\n\n' +
             '```css\n' +
             `[Versión actual]: ${global.bot_version}\n` +
             `[Estado]: ${global.bot_status}\n` +
-            `[Desarrollador]: Papita con Puré#6932 (@423129757954211880)` +
+            `[Desarrollador]: Papita con Puré#6932 (@423129757954211880)\n` +
             '```\n' +
             '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n'
         );
