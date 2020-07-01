4456949a87c16c2d9c21b3676b748cbb334ba42f
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-06-30 23:47:54 -0300

    w

--- commands/Pure/avatar.js
+++ commands/Pure/avatar.js
@@ -18,15 +18,14 @@ module.exports = {
             
             message.channel.send(embed);
         } else {
+            if(args[0].startsWith('<@') && args[0].endsWith('>')) {
+                args[0] = args[0].slice(2, -1);
+                if(args[0].startsWith('!')) args[0] = args[0].slice(1);
+            }
             if(isNaN(args[0])) {
-                if(args[0].startsWith('<@') && args[0].endsWith('>')) {
-                    args[0] = args[0].slice(2, -1);
-                    if(args[0].startsWith('!')) args[0] = args[0].slice(1);
-                }
-
                 const temp = args[0];
-                args[0] = message.channel.guild.members.filter(member => 
-                    member.nickname.toLowerCase().indexOf(temp) !== -1 || member.user.username.toLowerCase().indexOf(temp) !== -1
+                args[0] = message.channel.guild.members.filter(gmember => 
+                    (gmember.nickname.toLowerCase().indexOf(temp) !== -1 || gmember.user.username.toLowerCase().indexOf(temp) !== -1)
                 ).first().id;
             }
 
