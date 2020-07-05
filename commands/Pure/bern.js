40ee815aacf28d472b07a5b4bee43ab848b3b7f3
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-07-05 19:26:10 -0300

    w

--- commands/Pure/bern.js
+++ commands/Pure/bern.js
@@ -13,8 +13,9 @@ module.exports = {
     ],
 	execute(message, args){
 		const emot = [
-			'Mi polola.', 'Mi reina.', 'Mi princesa', 'Mi esposa', 'Mi mujer', 'Procrastinar', ':wine_glass:',
-			'No avancé en el manga de Kogasa', 'No avancé en el manga de Komachi', 'Mañana lo hago', 'Otro día'
+			'Mi polola', 'Mi reina', 'Mi princesa', 'Mi esposa', 'Mi mujer',
+			':wine_glass:', ':wine_glass::wine_glass:', ':wine_glass::wine_glass::wine_glass:',
+			'No avancé en el manga de Kogasa', 'No avancé en el manga de Komachi', 'Mañana lo hago', 'Otro día', 'Mañana sin falta', 'Esta semana lo termino', 'Procrastinar'
 		];
 		const lel = [
 			message.client.emojis.get('654504689873977347'), //Kogablush
@@ -25,9 +26,9 @@ module.exports = {
 		const selection = getRandomInt(emot.length);
 		
 		message.channel.send(`**${emot[selection]}** <:bewny:722334924845350973>`).then(sent => {
-			if(selection < 6) {
+			if(selection <= 4) {
 				sent.react(lel[0]);
-			} else if(selection === 6) {
+			} else if(selection > 4 && selection <= 7) {
 				sent.react(lel[1]);
 			} else {
 				sent.react(lel[2])
