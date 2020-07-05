b72749f2d4996a06b458d09ccf823a134665f0c3
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-07-05 19:03:10 -0300

    bwuh

--- commands/Pure/imagine.js
+++ commands/Pure/imagine.js
@@ -7,6 +7,21 @@ module.exports = {
         'tryhard', 'tryhardeo'
     ],
 	execute(message, args){
-		message.channel.send('**TRYHARD**\n**TRYHARD**');
+		const mayuwu = [
+			'<:mayuwu:654489124413374474>',
+			'<:keikuwu:725572179101614161>',
+			'<:koipwaise:657346542847524875>',
+			'<:pepe:697320983106945054>',
+			'<:kokocrong:697323104141049867>'
+		];
+		const lel = [
+			message.client.emojis.get('697320983106945054'),
+			message.client.emojis.get('697323104141049867')
+		];
+
+		message.channel.send(`${mayuwu[Math.floor(Math.random() * mayuwu.length)]} (?`).then(sent => {
+			sent.react(lel[0])
+				.then(() => sent.react(lel[1]));
+		});
     },
 };
