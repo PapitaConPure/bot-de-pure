ae128a0f5665d8d6198585015aa17248d759a210
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2020-07-17 18:30:11 -0300

    w

--- commands/Pure/papa-invitar.js
+++ commands/Pure/papa-invitar.js
@@ -10,7 +10,7 @@ module.exports = {
                 '***ENLACE DE INVITACIÓN DEL BOT***\n' +
                 '*Para invitar al bot a algún servidor, __Papita con Puré__ (<@423129757954211880>) debe formar parte del mismo*\n\n' +
                 'Clickea este enlace y selecciona el servidor al que quieres invitar al bot (solo __Papita con Puré__):\n' +
-                'https://discordapp.com/oauth2/authorize?client_id=651250669390528561&scope=bot\n' +
+                'https://discord.com/oauth2/authorize?client_id=651250669390528561&scope=bot\n' +
                 '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n'
             );
         } else {
