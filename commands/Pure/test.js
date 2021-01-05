e69fd84c1b51cb88ce132b0d27cb21f8228b640f
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2021-01-05 19:15:23 -0300

    Ok.

--- commands/Pure/test.js
+++ commands/Pure/test.js
@@ -6,8 +6,8 @@ const func = require('../../func.js'); //Funciones globales
 module.exports = {
 	name: 'test',
 	execute(message, args) {
-		//message.channel.send('No se están haciendo pruebas por el momento <:uwu:681935702308552730>');
-		//return;
+		message.channel.send('No se están haciendo pruebas por el momento <:uwu:681935702308552730>');
+		return;
 		message.channel.send(`\`${randInt(10, 1000)}\``);
     },
 };
