14a97d5a80f4f04fe42dc9b8206092652aa764c9
PapitaConPure <ojitosclaros_ivancho@hotmail.com>
2021-04-06 20:49:11 -0300

    p!imagine

--- commands/Pure/imagine.js
+++ commands/Pure/imagine.js
@@ -6,31 +6,31 @@ module.exports = {
 	aliases: [
         'tryhard', 'tryhardeo'
     ],
-    desc: 'Comando de coso de Imagine Breaker',
+    desc: 'Comando de x-uwus de Imagine Breaker',
     flags: [
-        'meme',
-		'outdated'
+        'meme'
     ],
     options: [
 
     ],
 	
 	execute(message, args) {
-		const mayuwu = [
+		const xuwu = [
 			'<:mayuwu:654489124413374474>',
+			'<:Staruwu:825224815865626645>',
+			'<:payasowo:828755669049278476>',
+			'<:monowo:757624423300726865>',
+			'<:mayuwu:654489124413374474>',
+			'<:mayunu:749727348030570728>',
+			'<:kyokuwu:810999921343594516>',
 			'<:keikuwu:725572179101614161>',
-			'<:koipwaise:657346542847524875>',
-			'<:pepe:697320983106945054>',
-			'<:kokocrong:697323104141049867>'
-		];
-		const lel = [
-			message.client.emojis.cache.get('697320983106945054'),
-			message.client.emojis.cache.get('697323104141049867')
+			'<:kageuwu:742506313258369056>',
+			'<:dormidowo:822701183340773417>',
+			'<:comodowo:824759668437811224>',
+			'<a:holareko:827304944620142593>',
+			'<a:sumiadios:827294808837062766>'
 		];
 
-		message.channel.send(`${mayuwu[Math.floor(Math.random() * mayuwu.length)]} (?`).then(sent => {
-			sent.react(lel[0])
-				.then(() => sent.react(lel[1]));
-		});
+		message.channel.send(xuwu[Math.floor(Math.random() * xuwu.length)]);
     },
 };
