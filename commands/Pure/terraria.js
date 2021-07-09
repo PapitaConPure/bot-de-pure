6a2f9065236bc245ddc9b0026af470b35b8ac4a4
Ivan zualet <ojitosclaros_ivancho@hotmail.com>
2021-07-09 03:22:53 -0300

    p!terraria sección tener en cuenta

--- commands/Pure/terraria.js
+++ commands/Pure/terraria.js
@@ -19,7 +19,8 @@ module.exports = {
 			.addField('Horario', ':earth_americas: 7PM~1AM UTC\n:flag_mx: 2PM~8PM GMT-5\n:flag_ar: 4PM~10PM GMT-3', true)
 			.addField('RadminVPN', ':link: [Descargar](https://www.radmin-vpn.com/es/)\n:globe_with_meridians: Hourai Doll\n:closed_lock_with_key: voodoo', true)
 			.addField('Terraria', ':moneybag: [Comprar](https://store.steampowered.com/app/105600/Terraria/)\n:pirate_flag: [Descargar](https://www.mediafire.com/file/m6hh8avyt58aqmo/T3rr4r1a-v1.4.2.3.rar/file)\n:hash: 26.93.83.58', true)
-			.addField('Guía Básica', 'Para jugar, necesitarás la versión más reciente de Terraria y RadminVPN encendido y conectado a nuestra red. Luego de eso abre Terraria, ve a Multiplayer, conéctate con la IP especificada y... ¡a jugar!');
+			.addField('Guía Básica', 'Para jugar, necesitarás la versión más reciente de Terraria y RadminVPN encendido y conectado a nuestra red. Luego de eso abre Terraria, ve a Multiplayer, conéctate con la IP especificada y... ¡a jugar!')
+			.addField('A tener en cuenta', 'Como preferencia, los mundos hosteados son en Modo Experto y los personajes usados son Modo Clásico (soltar dinero al morir).\nSi bien el servidor debería abrir todos los días, pueden haber excepciones sobre la marcha.\nSi tienes problemas para conectarte, contacta con el staff para intentar solucionarlo.');
 		message.channel.send(embed);
 	}
 };
