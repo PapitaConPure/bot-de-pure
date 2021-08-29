ab8621cc0f148a083adcaf200cfc949de287302c
Ivan zualet <ojitosclaros_ivancho@hotmail.com>
2021-08-29 19:42:57 -0300

    bugfix uwu

--- commands/Pure/inforol.js
+++ commands/Pure/inforol.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js'); //Integrar discord.js
-const { fetchArrows } = require('../../func');
+const { fetchFlag, fetchArrows } = require('../../func');
 const { p_pure } = require('../../localdata/config.json'); //Variables globales
 
 module.exports = {
@@ -10,26 +10,19 @@ module.exports = {
         'irol', 'ir', 'ri', 'rolei'
     ],
     desc: 'Realiza una búsqueda en el servidor para encontrar a todos los usuarios que cumplen con los búsqueda de roles solicitada. Devuelve el total de usuarios encontrados junto con una lista paginada de los mismos\n' +
-		'**[+]Inclusivo:** se consideran usuarios con __algunos de los roles__ buscados (dinámico)\n' +
-		'**[-]Exclusivo:** se consideran usuarios con __todos los roles__ buscados (específico)',
+		  'La búsqueda será de tipo flojo a menos que se especifique que sea `--estricta`. En dicho caso, solo se listarán los usuarios que tengan _todos_ los roles mencionados en lugar de _uno o más_.',
     flags: [
-        'mod',
-		//'outdated'
+        'mod'
     ],
     options: [
-		'`caso` _(signos +/-)_ para especificar si la búsqueda es de caso [+]Inclusivo o [-]Exclusivo',
-		'`búsqueda...` _(rol/roles...)_ para especificar los roles que quieres buscar'
+		'`-x` o `--estricto` para especificar si la búsqueda es estricta',
+		'`<búsqueda...>` _(rol/roles...)_ para especificar los roles que quieres buscar'
     ],
 	callx: '<caso> <búsqueda...>',
 	
 	execute(message, args) {
-		if(args.length < 2) {
-			message.channel.send({ content: `:x: ¡Debes ingresar al menos dos parámetros!\nUsa \`${p_pure}ayuda inforol\` para más información` });
-			return;
-		}
-
-		if(args[0] !== '-' && args[0] !== '+') {
-			message.channel.send({ content: `:warning: El primer parámetro solo puede ser \`+\` o \`-\`\nUsa \`${p_pure}ayuda inforol\` para más información` });
+		if(args.length < 1) {
+			message.channel.send({ content: `:x: ¡Debes ingresar al menos un parámetro!\nUsa \`${p_pure}ayuda inforol\` para más información` });
 			return;
 		}
 
@@ -37,7 +30,8 @@ module.exports = {
 		const servidor = message.channel.guild; //Variable que almacena un objeto del servidor a analizar
 
 		//Adquirir ID de los roles
-		for(let roleget = 1; roleget < args.length; roleget++) {
+        const strict = fetchFlag(args, { short: [ 'x', 'e' ], long: [ 'estricto', 'estricta' ], callback: true, fallback: false });
+		for(let roleget = 0; roleget < args.length; roleget++) {
 			if(args[roleget].startsWith('<@&') && args[roleget].endsWith('>')) {
 				args[roleget] = args[roleget].slice(3, -1);
 			}
@@ -58,7 +52,7 @@ module.exports = {
 		if(!args.every(argfetched => argfetched === '-1')) {
 			//Contadores de usuarios
 			const rolemembers = servidor.members.cache.filter(member => { //Usuarios con rol
-				if(args[0] === '+')
+				if(strict)
 					return args.some(argrole => {
 						if(argrole !== args[0] && argrole !== '-1')
 							return member.roles.cache.has(argrole);
@@ -121,11 +115,12 @@ module.exports = {
 						.then(() => sent.react(arrows[1]))
 						.then(() => {
 							const collector = sent.createReactionCollector({ filter: filter, time: 8 * 60 * 1000 });
-							collector.on('collect', reaction => {
+							collector.on('collect', (reaction, ruser) => {
 								const maxpage = Math.ceil(totalcnt / 10);
 								if(reaction.emoji.id === arrows[0].id) SelectedEmbed = (SelectedEmbed > 0)?(SelectedEmbed - 1):maxpage;
 								else SelectedEmbed = (SelectedEmbed < maxpage)?(SelectedEmbed + 1):0;
 								sent.edit({ embeds: [Embed[SelectedEmbed]] });
+								reaction.users.remove(ruser);
 							});
 						})
 				});
