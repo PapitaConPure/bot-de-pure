5b381296e84945ce5ab7125710b8b5b597e6f702
Ivan zualet <ojitosclaros_ivancho@hotmail.com>
2021-09-05 18:58:38 -0300

    p!celeste

---
+++ commands/Pure/celeste.js
@@ -0,0 +1,19 @@
+const celesteurl = 'https://i.imgur.com/h9L6qy9.png';
+
+module.exports = {
+	name: 'celeste',
+	desc: 'La versión que nunca te contaron del comando de Sassa',
+	flags: [
+		'meme'
+	],
+
+	async execute({ channel }, _) {
+		//Acción de comando
+		channel.send({ files: [celesteurl] });
+	},
+
+	async interact(interaction) {
+		//Acción de comando
+		interaction.reply({ files: [celesteurl] });
+	}
+};
