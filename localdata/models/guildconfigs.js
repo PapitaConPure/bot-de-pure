c496ad0ac966a89ce73340272101dc354ddb4253
Ivan zualet <ojitosclaros_ivancho@hotmail.com>
2021-11-19 00:53:14 -0300

    Nuevas configuraciones de servidor

--- localdata/models/guildconfigs.js
+++ localdata/models/guildconfigs.js
@@ -1,8 +1,13 @@
 const Mongoose = require('mongoose');
 
+/** Describe la configuración de un servidor. */
 const GuildConfigSchema = new Mongoose.Schema({
     guildId: { type: String },
+    /** Habilitar modo caótico del servidor. */
     chaos: { type: Boolean, default: false },
+    /** Habilitar mejora de vista previa de pixiv del servidor. */
+    pixify: { type: Boolean, default: false },
+    /** Papas del servidor. */
     potatoes: { type: Object, default: {} },
 });
 
