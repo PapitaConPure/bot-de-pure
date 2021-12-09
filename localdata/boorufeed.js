87e792220ddd94ba790f15d3ba0205ebf3dfa5b9
Ivan zualet <ojitosclaros_ivancho@hotmail.com>
2021-12-09 16:46:11 -0300

    Botones de Feed

--- localdata/boorufeed.js
+++ localdata/boorufeed.js
@@ -1,13 +1,13 @@
 const GuildConfig = require('./models/guildconfigs.js');
 const booru = require('booru');
-const { Client, MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
 module.exports = {
     /**@param {Client} client*/
     async updateBooruFeeds(client) {
         /**@type {Array<Object>} */
         const maxDocuments = 16;
-        const maxTags = 30;
+        const maxTags = 20;
         const guilds = client.guilds.cache;
         guilds.forEach(async guild => {
             const gcfg = await GuildConfig.findOne({ guildId: guild.id });
@@ -24,6 +24,22 @@ module.exports = {
                         if(!feed.ids.includes(image.id)) {
                             gcfg.feeds[chid].ids = [ image.id, ...gcfg.feeds[chid].ids ].slice(0, maxDocuments);
                             gcfg.markModified('feeds');
+                            const row = new MessageActionRow().addComponents(
+                                new MessageButton()
+                                    .setLabel('Gelbooru')
+                                    .setStyle('LINK')
+                                    .setURL(`https://gelbooru.com/index.php?page=post&s=view&id=${image.id}`),
+                            );
+                            const source = Array.isArray(image.source) ? image.source[0] : (image.source || undefined);
+                            console.log(source, source && source.match(/((http:\/\/|https:\/\/)?(www.)?(([a-zA-Z0-9-]){2,}\.){1,4}([a-zA-Z]){2,6}(\/([a-zA-Z-_\/\.0-9#:?=&;,]*)?)?)/));
+                            if(source && source.match(/((http:\/\/|https:\/\/)?(www.)?(([a-zA-Z0-9-]){2,}\.){1,4}([a-zA-Z]){2,6}(\/([a-zA-Z-_\/\.0-9#:?=&;,]*)?)?)/))
+                                row.addComponents(
+                                    new MessageButton()
+                                        .setLabel('Original')
+                                        .setStyle('LINK')
+                                        .setURL(source),
+                                );
+                            
                             channel.send({
                                 embeds: [
                                     new MessageEmbed()
@@ -31,12 +47,13 @@ module.exports = {
                                         .setAuthor('Desde Gelbooru', 'https://i.imgur.com/outZ5Hm.png')
                                         //.setTitle(feed.tags.split(/ +/g).slice(0, 3).join(' ').replace(/rating:/g, ''))
                                         .addField(`Tags (${Math.min(image.tags.length, maxTags)}/${image.tags.length})`, `*${image.tags.slice(0,maxTags).join(', ').replace(/\\*\*/g,'\\*').replace(/\\*_/g,'\\_')}*`)
-                                        .addField('Salsa', [
-                                            `[Gelbooru](https://gelbooru.com/index.php?page=post&s=view&id=${image.id})`,
-                                            image.source ? `[Original](${image.source})` : null
-                                        ].join('\n'))
+                                        //.addField('Salsa', [
+                                        //    `[Gelbooru](https://gelbooru.com/index.php?page=post&s=view&id=${image.id})`,
+                                        //    image.source ? `[Original](${image.source})` : null
+                                        //].join('\n'))
                                         .setImage(image.fileUrl)
-                                ]
+                                ],
+                                components: [row],
                             });
                         }
                     });
