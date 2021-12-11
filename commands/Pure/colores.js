448a58a3bed81be6a96d2e32271a78b458292431
Ivan zualet <ojitosclaros_ivancho@hotmail.com>
2021-12-11 08:43:00 -0300

    Claridad para daltónicos p!colores

--- commands/Pure/colores.js
+++ commands/Pure/colores.js
@@ -2,13 +2,13 @@ const { hourai } = require('../../localdata/config.json'); //Variables globales
 const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
 
 const colorsList = [
-	{ emoteId: '819772377814532116', roleId: '671851233870479375', roleName: 'French Doll'  },
-	{ emoteId: '819772377624870973', roleId: '671852132328275979', roleName: 'Holland Doll' },
-	{ emoteId: '819772377482526741', roleId: '671851228954755102', roleName: 'Tibetan Doll' },
-	{ emoteId: '819772377440583691', roleId: '671851235267182625', roleName: 'Kyoto Doll'   },
-	{ emoteId: '819772377856606228', roleId: '671851236538187790', roleName: 'London Doll'  },
-	{ emoteId: '819772377894354944', roleId: '671851234541699092', roleName: 'Russian Doll' },
-	{ emoteId: '819772377642041345', roleId: '671851228308963348', roleName: 'Orléans Doll' },
+	{ emoteId: '819772377814532116', roleId: '671851233870479375', roleName: 'French Doll',  roleDesc: 'Rojo'     },
+	{ emoteId: '819772377624870973', roleId: '671852132328275979', roleName: 'Holland Doll', roleDesc: 'Azul'     },
+	{ emoteId: '819772377482526741', roleId: '671851228954755102', roleName: 'Tibetan Doll', roleDesc: 'Verde '   },
+	{ emoteId: '819772377440583691', roleId: '671851235267182625', roleName: 'Kyoto Doll',   roleDesc: 'Púrpura'  },
+	{ emoteId: '819772377856606228', roleId: '671851236538187790', roleName: 'London Doll',  roleDesc: 'Naranja'  },
+	{ emoteId: '819772377894354944', roleId: '671851234541699092', roleName: 'Russian Doll', roleDesc: 'Amarillo' },
+	{ emoteId: '819772377642041345', roleId: '671851228308963348', roleName: 'Orléans Doll', roleDesc: 'Celeste'  },
 ];
 
 const roleList = (() => {
