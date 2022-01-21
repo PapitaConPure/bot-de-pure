e0c2aa006f401cea6ad2c5f3620a3dd67d0a9e23
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-01-20 21:45:37 -0300

    p!caramelos reworked

--- commands/Pure/caramelos.js
+++ commands/Pure/caramelos.js
@@ -1,4 +1,24 @@
-const { askCandy } = require('../../func.js'); //Funciones globales
+const { serverid: sid } = require('../../localdata/config.json');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+
+const embed = new MessageEmbed()
+	.setColor('DARK_PURPLE')
+	.addField('¡Se están repartiendo caramelos mágicos!', 'Antiguos relatos cuentan que permiten ver trazos de lujuria grabados en el aire.\nSe están aceptando devoluciones para aplicantes previos, solo vomítenlos con cuidado de dañarlos.');
+
+const row = new MessageActionRow().addComponents([
+	new MessageButton()
+		.setCustomId('caramelos_addCandy')
+		.setEmoji('778180421304188939')
+		.setLabel('Recibir')
+		.setStyle('SUCCESS'),
+	new MessageButton()
+		.setCustomId('caramelos_removeCandy')
+		.setEmoji('704612795072774164')
+		.setLabel('Devolver')
+		.setStyle('DANGER'),
+]);
+
+let candyRole = '683084373717024869';
 
 module.exports = {
 	name: 'caramelos',
@@ -10,15 +30,37 @@ module.exports = {
     flags: [
         'hourai',
     ],
+	experimental: true,
 	
-	async execute(message, _) {
-		askCandy(message.member, message.channel);
+	/**
+	 * @param {import("../Commons/typings").CommandRequest} request
+	 * @param {import('../Commons/typings').CommandOptions} _
+	 * @param {Boolean} isSlash
+	 */
+	async execute(request, _) {
+		request.reply({
+			embeds: [ embed.setAuthor('Punto de Reparto desplegado', (request.author ?? request.user).avatarURL()) ],
+			components: [row],
+		});
     },
-	
-	async interact(interaction, _) {
-		await interaction.reply({ content: 'Procesando...' });
-		const message = await interaction.fetchReply();
-		askCandy(message.member, message.channel);
-		message.delete();
+
+	async ['addCandy'](interaction) {
+		const { member } = interaction;
+		if(member.roles.cache.has(candyRole))
+			return await interaction.reply({ content: 'Oe tranqui po, que ya tení tus caramelos <:kageuwu:850196617495707678>', ephemeral: true });
+		return await Promise.all([
+			member.roles.add(candyRole),
+			interaction.reply({ content: 'Caramelos entregados <:miyoi:674823039086624808>:pinching_hand: :candy:', ephemeral: true }),
+		]);
+    },
+
+	async ['removeCandy'](interaction) {
+		const { member } = interaction;
+		if(!member.roles.cache.has(candyRole))
+			return await interaction.reply({ content: 'No tenei caramelos encima, si querei plata trabaja po\' <:lechita:931409943448420433>', ephemeral: true });
+		return await Promise.all([
+			member.roles.remove(candyRole),
+			interaction.reply({ content: 'Caramelos devueltos <:miyoi:674823039086624808>:pray:', ephemeral: true }),
+		]);
     },
 };
