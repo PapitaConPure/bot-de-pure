80c7c08fcd8fa0cac855537677597dfd138b3e36
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-01-25 10:30:52 -0300

    Dear Satan, I've come to

--- commands/Pure/tarjeta.js
+++ commands/Pure/tarjeta.js
@@ -114,7 +114,7 @@ module.exports = {
 		if(!highlights.survival[survivalname]) return await request.reply(`⚠ Debes ingresar una calidad de survival válida.\n${helpstr}`);
 		
 		const score = improveNumber(isSlash ? args.getNumber('survival') : args[3], false, 10);
-		if(!score || score < 0) return await request.reply(`⚠ Debes ingresar un puntaje final válido.\n${helpstr}`);
+		if(isNaN(score) || score <= 0) return await request.reply(`⚠ Debes ingresar un puntaje final válido.\n${helpstr}`);
 
 		let dateStr;
 		if(args.length > 4) {
