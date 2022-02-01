d8ea17e08dfcf5c9293ce261bdd7870f92633ccd
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-02-01 10:51:31 -0300

    Magia pero mejor

--- index.js
+++ index.js
@@ -610,7 +610,7 @@ client.on('voiceStateUpdate', async (oldState, state) => {
                     sessionTextChannel.permissionOverwrites.edit(guild.roles.everyone, { SEND_MESSAGES: false }, { reason: 'Restricción de envío de mensajes en sesión PuréVoice' }).catch(prematureError),
                     sessionTextChannel.permissionOverwrites.edit(guild.me, { SEND_MESSAGES: true }, { reason: 'Envío de mensajes propios en sesión PuréVoice' }).catch(prematureError),
                     sessionTextChannel.permissionOverwrites.edit(member, { SEND_MESSAGES: true }, { reason: 'Inclusión de miembro en sesión PuréVoice' }).catch(prematureError),
-                    sessionTextChannel.setTopic(`${guild.name}» PuréVoice » ${member.user.tag}\nCanal de texto de Sesión\n ¡Conéctate a <#${channel.id}> para conversar aquí!`).catch(prematureError),
+                    sessionTextChannel.setTopic(`#️⃣ ${guild.name} » PuréVoice » ${member.user.tag}\nCanal de texto de Sesión \n👥 ¡Conéctate a <#${channel.id}> para conversar aquí!`).catch(prematureError),
                 ]);
                 await channel.setName(`💠「${member.user.username.slice(24)}」`).catch(prematureError);
                 await channel.setUserLimit(0).catch(prematureError);
