ed227b15e9368de9256dd25de4ab8c4e11cc1041
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-03-23 00:14:17 -0300

    Pelotudo.

--- commands/Pure/uwus.js
+++ commands/Pure/uwus.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js'); //Integrar discord.js
+const { MessageEmbed } = require('discord.js');
 const { CommandOptionsManager } = require('../Commons/cmdOpts');
 
 const options = new CommandOptionsManager()
@@ -16,50 +16,61 @@ module.exports = {
     ],
     options,
     callx: '<tiempo?>',
+    experimental: true,
 	
-	async execute(message, args) {
-        let secs;
-        if(args.length) {
-            if(!isNaN(args[0])) secs = Math.max(0.1, Math.min(args[0], 3600 * 2));
-            else secs = 30;
-        } else secs = 30;
-        const Embed = new Discord.MessageEmbed()
+    /**
+     * 
+     * @param {import('../Commons/typings').CommandRequest} request 
+     * @param {import('../Commons/typings').CommandOptions} args 
+     * @param {Boolean} isSlash 
+     * @returns 
+     */
+	async execute(request, args, isSlash = false) {
+        const secs = isNaN(args[0]) ? 30 : Math.max(0.1, Math.min(args[0], 3600 * 2));
+        
+        let uwuUsers = {};
+        let lastUwu;
+        const filter = m => (m.content.toLowerCase().indexOf('uwu') !== -1 && !m.author.bot) || (m.content.toLowerCase() === 'antiuwu' && m.author.id === request.author.id);
+        coll = request.channel.createMessageCollector({ filter: filter, time: (secs * 1000) });
+
+        coll.on('collect', m => {
+            const userId = (m.author ?? m.user).id;
+            if(m.content === 'antiuwu') 
+                return coll.stop();
+            
+            uwuUsers[userId] = (uwuUsers[userId] ?? 0) + 1;
+            lastUwu = m.author ?? m.user;
+        });
+
+        coll.on('end', collected => {
+            let bestId;
+            let max = 0;
+            for(const [ uid, count ] of Object.entries(uwuUsers)) {
+                if(count >= max) {
+                    bestId = uid;
+                    max = count;
+                }
+            }
+            
+            const embed = new MessageEmbed()
+                .setColor('ffbbbb')
+                .setTitle('Evento UWU finalizado')
+                .addField('Estadísticas', `**UWUs totales:** ${collected.size}\n**UWUs por segundo:** ${collected.size / secs}`, true)
+                .addField('Persona que envió...', `**Más UWUs:** <@${bestId ?? 'nadie umu'}>\n**Último UWU:** ${lastUwu ?? 'nadie umu'}`, true);
+
+            return Promise.all([
+                request.channel.bulkDelete(collected),
+                request.channel.send({ embeds: [embed] }),
+            ]);
+        });
+
+        const embed = new MessageEmbed()
             .setColor('ffbbbb')
             .setTitle('Evento UWU')
             .addField('UWU', 'Envía **uwu** para sumar un **uwu**.')
             .addField('Duración del evento', `**${secs}** segundos.`)
-            .setAuthor({ name: `Evento iniciado por ${message.author.username}`, iconURL: message.author.avatarURL() });
-        message.channel.send({ embeds: [Embed] }).then(sent => {
-            const filter = m => (m.content.toLowerCase().indexOf('uwu') !== -1 && !m.author.bot) || (m.content.toLowerCase() === 'antiuwu' && m.author.id === message.author.id);
-            let uwusers = {}, ultimuwu;
-			coll = sent.channel.createMessageCollector({ filter: filter, time: (secs * 1000) });
-            coll.on('collect', m => {
-                if(m.content !== 'antiuwu') {
-                    if(!uwusers.hasOwnProperty(`${m.author.id}`)) { uwusers[m.author.id] = 1; }
-                    else uwusers[m.author.id]++;
-                    ultimuwu = m.author.id;
-                } else coll.stop();
-            });
-			coll.on('end', collected => {
-                let mvp;
-                let auwus = Object.entries(uwusers);
-                let max = 0, maxid = -1;
-                for(let uwuser of auwus) {
-                    if(uwuser[1] >= max) {
-                        maxid = uwuser[0];
-                        max = uwuser[1];
-                    }
-                }
-                collected.map(uwumsg => uwumsg.delete());
-                mvp = maxid;
-                message.channel.send({
-                    content:
-                        `**UWUs totales:** ${collected.size}\n` +
-                        `**UWUs por segundo:** ${collected.size / secs}\n` +
-                        `**Persona que envió más UWUs:** ${(mvp !== -1)?`<@${mvp}>`:'nadie umu'}\n` +
-                        `**Último UWU enviado por:** ${(mvp !== -1)?`<@${ultimuwu}>`:'nadie umu'}`
-                });
-            });
-        });
+            .setAuthor({ name: `Evento iniciado por ${request.author.username}`, iconURL: request.author.avatarURL() });
+        
+        return await request.reply({ embeds: [embed] });
     },
 };
