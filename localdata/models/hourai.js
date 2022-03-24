ea7e9fe4fc84eb72448288d624f93e55094ef44c
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-03-23 22:36:11 -0300

    Quitar comentarios innecesarios

--- localdata/models/hourai.js
+++ localdata/models/hourai.js
@@ -1,9 +1,8 @@
 const Mongoose = require('mongoose');
 
-/** Describe la configuración de un servidor. */
 const HouraiSchema = new Mongoose.Schema({
-    /** Habilitar modo caótico del servidor. */
     userInfractions: { type: Object, default: {} },
+    customRoles: { type: Object, default: {} },
 });
 
 module.exports = Mongoose.model('Hourai', HouraiSchema);
