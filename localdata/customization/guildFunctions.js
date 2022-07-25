083f8a2fd249c2dbc5ae226eb8ae1589611e5152
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-07-25 20:40:35 -0300

    Actualizar funciones de Guild

--- localdata/customization/guildFunctions.js
+++ localdata/customization/guildFunctions.js
@@ -1,9 +1,13 @@
+const { auditError } = require('../../systems/auditor');
 const global = require('../config.json');
 const Hourai = require('../models/hourai.js');
 const guildIds = global.serverid;
 
-function fuckEveryOtherBot({ content, channel, author }) {
-    content = content.toLowerCase();
+/**@param {import('discord.js').Message} message*/
+function fuckEveryOtherBot(message) {
+    const { channel, author } = message;
+    if(author.id === message.client.user.id) return;
+    const content = message.content.toLowerCase();
     const hrai = content.indexOf('hourai');
     const reps = global.hourai.replies;
     const { prefix: hraipf, suffix: hraisf } = reps.ignore;
@@ -19,52 +23,77 @@ function fuckEveryOtherBot({ content, channel, author }) {
         });
 };
 
+/**@param {import('discord.js').Message} message*/
+async function findBotInfraction(message) {
+    const { content, client, channel, author, guild } = message;
+    const whiteListed = global.hourai.infr.channels;
+    if(whiteListed[channel.id] || whiteListed[channel.parent?.id]) return;
+
+    //Detectar infracción
+    let user, member, infractionMessage;
+
+    const msg = content.toLowerCase();
+    // const blacklisted = [ /^->\w/, /^\$\w/, /^\+\w/, /^,(?!confession)\w/, /^~\w/, /^%\w/, /^[Nn][Ee][Kk][Oo] +\w/, /^[Gg]\. *\w/ ];
+    const blacklisted = [ /^->\w/, /^\$\w/, /^\+\w/, /^,(?!confession)\w/, /^%\w/, /^neko +\w/, /^g\. *\w/ ];
+    if(blacklisted.some(bp => msg.match(bp))) {
+        //Método de generalización de comando
+        infractionMessage = message;
+        user = author;
+        member = message.member;
+    } else {
+        //Método de detección de respuesta
+        if(!author.bot || !message.deletable) return;
+        infractionMessage = await channel.messages.fetch(message.reference?.messageId).catch(_ => null);
+        if(!infractionMessage?.deletable) return;
+
+        //No amonestar dos veces
+        const ifrmsg = infractionMessage.content.toLowerCase();
+        if(blacklisted.some(bp => ifrmsg.match(bp))) return;
+
+        user = infractionMessage.author;
+        if(!user || user.bot) return;
+        member = await guild.members.fetch(user);
+    }
+
+    const hourai = (await Hourai.findOne({})) || new Hourai({});
+    const now = Date.now();
+    const infractionUser = hourai.userInfractions?.[user.id]?.filter(inf => (now - inf) < (1000 * 60 * 60 * 4)) ?? [];
+    
+    //Sancionar según total de infracciones cometidas en las últimas 4 horas
+    const infractionCount = infractionUser.push(now); //Añade el momento de la infracción actual y retorna el largo del arreglo
+    hourai.userInfractions ??= {};
+    hourai.userInfractions[user.id] = infractionUser;
+    hourai.markModified('userInfractions');
+    hourai.save().catch(console.error);
+    switch(infractionCount) {
+        case 1:
+            return infractionMessage.react(client.emojis.cache.get('920020596526551072')).catch(auditError);
+
+        case 2:
+            return infractionMessage.react(client.emojis.cache.get('796930821554044928')).catch(auditError);
+
+        case 3: {
+            await infractionMessage.react(client.emojis.cache.get('859874631795736606')).catch(auditError);
+            const roleId = '682629889702363143'; //Hanged Doll
+            const reason = 'Colgado automáticamente por spam de bots';
+            if(!member.roles.cache.has(roleId))
+                return member.roles.add(roleId, reason).catch(auditError);
+        }
+
+        default: {
+            await infractionMessage.react(client.emojis.cache.get('852764014840905738')).catch(auditError);
+            const roleId = '925599922370256906'; //Crucified Doll
+            const reason = 'Colgado automáticamente por spam de bots. Debido a la evasión del castigo previo, se requiere más poder para revocar la sanción';
+            if(!member.roles.cache.has(roleId))
+                return member.roles.add(roleId, reason).catch(auditError);
+        }
+    }
+};
+
+//Funciones de Respuesta Rápida personalizadas por servidor
 module.exports = {
     [guildIds.hourai]: {
-        async findBotInfraction(message) {
-            const { client, content, channel, author, member } = message;
-            const whiteListed = global.hourai.infr.channels;
-            if(whiteListed[channel.id] || whiteListed[channel.parent?.id]) return;
-
-            const msg = content.toLowerCase();
-            const blacklisted = [ /^[Pp] *![\n ]*\w/, /^!\w/, /^->\w/, /^\$\w/, /^\.\w/, /^,(?!confession)\w/, /^,,\w/, /^~\w/, /^\/\w/, /^%\w/, /^[Nn][Ee][Kk][Oo] +\w/, /^[Gg]\. *\w/ ];
-            if(!blacklisted.some(bp => msg.match(bp))) return;
-
-            const hourai = (await Hourai.findOne({})) || new Hourai({});
-            const now = Date.now();
-            const infractionUser = hourai.userInfractions?.[author.id]?.filter(inf => (now - inf) < (1000 * 60 * 60 * 4)) ?? [];
-            
-            //Sancionar según total de infracciones cometidas en las últimas 4 horas
-            const infractionCount = infractionUser.push(now); //Añade el momento de la infracción actual y retorna el largo del arreglo
-            hourai.userInfractions = hourai.userInfractions ?? {};
-            hourai.userInfractions[author.id] = infractionUser;
-            hourai.markModified('userInfractions');
-            await hourai.save().catch(console.error);
-            switch(infractionCount) {
-                case 1:
-                    return message.react(client.emojis.cache.get('920020596526551072')).catch(console.error);
-
-                case 2:
-                    return message.react(client.emojis.cache.get('796930821554044928')).catch(console.error);
-
-                case 3: {
-                    await message.react(client.emojis.cache.get('859874631795736606')).catch(console.error);
-                    const roleId = '682629889702363143'; //Hanged Doll
-                    const reason = 'Colgado automáticamente por spam de bots';
-                    if(!member.roles.cache.has(roleId))
-                        return member.roles.add(roleId, reason).catch(console.error);
-                }
-
-                default: {
-                    await message.react(client.emojis.cache.get('852764014840905738')).catch(console.error);
-                    const roleId = '925599922370256906'; //Crucified Doll
-                    const reason = 'Colgado automáticamente por spam de bots. Debido a la evasión del castigo previo, se requiere más poder para revocar la sanción';
-                    if(!member.roles.cache.has(roleId))
-                        return member.roles.add(roleId, reason).catch(console.error);
-                }
-            }
-        },
-
+        findBotInfraction,
         fuckEveryOtherBot,
     },
 
