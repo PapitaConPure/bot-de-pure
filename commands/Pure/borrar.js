87ab7647f81f85d4a39eb2bd8f0af33e78685925
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-07-27 19:58:02 -0300

    Volver dinámico y usar nuevo modelo de comandos

--- commands/Pure/borrar.js
+++ commands/Pure/borrar.js
@@ -1,47 +1,55 @@
 const { fetchFlag, fetchUser } = require('../../func.js');
 const { p_pure } = require('../../localdata/customization/prefixes.js');
-const { CommandOptionsManager, CommandMetaFlagsManager } = require("../Commons/commands");
+const { CommandOptionsManager, CommandMetaFlagsManager, CommandManager } = require("../Commons/commands");
 
 const options = new CommandOptionsManager()
-	.addParam('cantidad', 'NUMBER', 'para especificar la cantidad de mensajes a borrar (sin contar el mensaje del comando)')
+	.addParam('cantidad', 'NUMBER', 'para especificar la cantidad de mensajes a borrar (sin contar el mensaje del comando)', { optional: true })
 	.addFlag('um', ['usuario', 'miembro'], 			'para especificar de qué usuario borrar mensajes', { name: 'user', type: 'USER' });
-
-module.exports = {
-	name: 'borrar',
-	aliases: [
+const flags = new CommandMetaFlagsManager().add('MOD');
+const command = new CommandManager('borrar', flags)
+	.setAliases(
 		'borrarmsg',
         'deletemsg', 'delete',
-        'del', 'd', 
-    ],
-    desc: 'Elimina una cierta cantidad de mensajes entre 2 y 100',
-    flags: new CommandMetaFlagsManager().add('MOD'),
-    options,
-	callx: '<cantidad>',
-	
-	async execute(message, args) {
-		await message.delete();
-		if(!args.length) {
-			const sent = await message.reply({ content: ':warning: debes especificar la cantidad o el autor de los mensajes a borrar.' });
-			setTimeout(() => sent.delete(), 1000 * 5);
+        'del', 'd',
+	)
+	.setLongDescription('Elimina una cierta cantidad de mensajes entre 2 y 100')
+	.setOptions(options)
+	.setExecution(async (request, args, isSlash) => {
+		if(!isSlash) await request.delete().catch(_ => undefined);
+
+		let user = options.fetchFlag(args, 'usuario', { callback: (f) => fetchUser(f, request) });
+		let amount = isSlash ? args.getNumber('cantidad') : (args.length ? parseInt(args[0]) : 100);
+
+		if(!user && !amount) {
+			const sent = await request.reply({ content: '⚠ Debes especificar la cantidad o el autor de los mensajes a borrar', ephemeral: true });
+			if(!isSlash)
+				setTimeout(() => sent.deletable && sent.delete().catch(_ => undefined), 1000 * 5);
 			return;
 		}
-		const user = fetchFlag(args, { property: true, short: ['u'], long: ['usuario', 'miembro'], callback: (x, i) => fetchUser(x[i], message), fallback: undefined });
-		let amt = (args.length) ? parseInt(args[0]) : 100;
-		if(isNaN(amt)) {
-			return message.reply({
+
+		if(isNaN(amount)) {
+			const sent = request.reply({
 				content:
-					':warning: Debes especificar la cantidad de mensajes a borrar\n' +
-					`Revisa \`${p_pure(message.guildId).raw}ayuda borrar\` para más información`
+					'⚠ Debes especificar la cantidad de mensajes a borrar\n' +
+					`Revisa \`${p_pure(request.guildId).raw}ayuda borrar\` para más información`,
+				ephemeral: true,
 			});
+			if(!isSlash)
+				setTimeout(() => sent.deletable && sent.delete().catch(_ => undefined), 1000 * 5);
+			return;
 		}
-		amt = Math.max(2, Math.min(amt, 100));
+
+		amount = Math.max(2, Math.min(amount, 100));
 		
 		if(user === undefined)
-			message.channel.bulkDelete(amt);
+			request.channel.bulkDelete(amount);
 		else {
-			const messages = await message.channel.messages.fetch({ limit: 100 });
+			const messages = await request.channel.messages.fetch({ limit: 100 });
 			let i = 0;
-			message.channel.bulkDelete(messages.filter(msg => msg.author.id === user.id && i++ < amt));
+			request.channel.bulkDelete(messages.filter(msg => msg.author.id === user.id && i++ < amount));
 		}
-    },
-};
+		if(isSlash)
+			return request.reply({ content: '✅ Mensajes eliminados', ephemeral: true });
+	});
+
+module.exports = command;
