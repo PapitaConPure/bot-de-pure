1a5d8ee515047e4e119d0efe849f2c37efb280f3
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-07-27 18:47:03 -0300

    Volver dinámico y usar nuevo modelo de comandos

--- commands/Pure/bern.js
+++ commands/Pure/bern.js
@@ -1,5 +1,6 @@
 const { randRange } = require("../../func");
-const { CommandMetaFlagsManager } = require("../Commons/commands");
+const { auditError } = require("../../systems/auditor");
+const { CommandMetaFlagsManager, CommandManager } = require("../Commons/commands");
 
 const emot = [
 	'Mi polola', 'Mi reina', 'Mi princesa', 'Mi esposa', 'Mi mujer',
@@ -7,25 +8,14 @@ const emot = [
 	'No avancé en el manga de Komachi', 'Mañana lo hago', 'Otro día', 'Mañana sin falta', 'Esta semana lo termino', 'Procrastinar'
 ];
 
-module.exports = {
-	name: 'bern',
-	aliases: [
-        'bewny', 'polola',
-		'procrastinar'
-    ],
-    desc: 'Comando de procrastinación de GoddamnBernkastel',
-    flags: new CommandMetaFlagsManager().add(
-		'MEME',
-		'OUTDATED',
-	),
-	experimental: true,
-
-	/**
-	 * @param {import("../Commons/typings").CommandRequest} request
-	 * @param {import('../Commons/typings').CommandOptions} args
-	 * @param {Boolean} isSlash
-	 */
-	async execute(request, _, isSlash = false) {
+const flags = new CommandMetaFlagsManager().add(
+	'MEME',
+	'OUTDATED',
+);
+const command = new CommandManager('bern', flags)
+	.setAliases('bewny', 'polola', 'procrastinar')
+	.setLongDescription('Comando de procrastinación de GoddamnBernkastel')
+	.setExecution(async request => {
 		const lel = [
 			'654504689873977347',
 			'722334924845350973',
@@ -33,21 +23,17 @@ module.exports = {
 			'697323104141049867',
 		].map(eid => request.client.emojis.cache.get(eid));
 		const selection = randRange(0, emot.length);
+		const sent = await request.reply({ content: `**${emot[selection]}** <:bewny:722334924845350973>`, fetchReply: true })
+		.catch(error => auditError(error, { request, ping: true }));
 
-		const sentqueue = (await Promise.all([
-			request.reply({ content: `**${emot[selection]}** <:bewny:722334924845350973>` }),
-			isSlash ? request.fetchReply() : null,
-		])).filter(sq => sq);
-		const sent = sentqueue.pop();
 		if(selection <= 4)
-			await sent.react(lel[0]);
-		else if(selection > 4 && selection <= 7)
-			await sent.react(lel[1]);
-		else
-			await Promise.all([
-				sent.react(lel[2]),
-				sent.react(lel[3]),
-			]);
-		return sent;
-    },
-};
+			return sent.react(lel[0]).catch(auditError);
+		if(selection > 4 && selection <= 7)
+			return sent.react(lel[1]).catch(auditError);
+		return Promise.all([
+			sent.react(lel[2]),
+			sent.react(lel[3]),
+		]).catch(auditError);
+	});
+
+module.exports = command;
