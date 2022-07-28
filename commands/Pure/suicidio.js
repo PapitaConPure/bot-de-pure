cf92a48013c3b99a9b050f33b4c82f2e01132381
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-07-28 00:33:41 -0300

    Usar nuevo modelo de comandos

--- commands/Pure/suicidio.js
+++ commands/Pure/suicidio.js
@@ -1,27 +1,25 @@
-const { CommandMetaFlagsManager } = require('../Commons/commands');
+const { CommandMetaFlagsManager, CommandManager } = require('../Commons/commands');
 
 const hd = '682629889702363143'; //Hanged Doll
 
-module.exports = {
-	name: 'suicidio',
-	aliases: [
+const flags = new CommandMetaFlagsManager().add(
+	'HOURAI',
+	'MEME',
+);
+const command = new CommandManager('suicidio', flags)
+	.setAliases(
 		'suicidar', 'suicidarse',
-		'suicide'
-	],
-	brief: 'Te otorga el rol "Hanged Doll" (solo Hourai Doll)',
-	desc: 'Te otorga el rol __Hanged Doll__ en Hourai Doll.',
-	flags: new CommandMetaFlagsManager().add(
-		'HOURAI',
-		'MEME',
-	),
-	experimental: true,
-
-	async execute(request, _) {
+		'suicide',
+	)
+	.setBriefDescription('Te otorga el rol "Hanged Doll" (solo Hourai Doll)')
+	.setLongDescription('Te otorga el rol __Hanged Doll__ en Hourai Doll.')
+	.setExecution(async request => {
 		const { member } = request;
 		if(member.roles.cache.has(hd))
 			return request.reply({ content: '\'pérate, cómo weá estai hablando <:junkNo:697321858407727224>' });
 
 		await member.roles.add(hd, 'Por pelotudo');
 		return request.reply({ content: '<:houraidoll:853402616208949279> Shanghai Shanghai Shanghai Shanghai\nHourai Hourai Hourai Hourai' });
-	},
-};
+	});
+
+module.exports = command;
