f9a9709b004dda54cec99cb5ba08010ee9171cb6
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-08-01 20:01:43 -0300

    Quitar tipo innecesario

--- commands/Commons/typings.js
+++ commands/Commons/typings.js
@@ -1,7 +1,6 @@
 const { Message, CommandInteraction, CommandInteractionOptionResolver } = require("discord.js");
 
 /**
- * @typedef {} CommandBuilderOptions
  * @typedef {Message | CommandInteraction} CommandRequest
  * @typedef {Array<*> | CommandInteractionOptionResolver} CommandOptions
  */
