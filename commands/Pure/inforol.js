f45d0880f700d913cde798ee2baab1efb209859d
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-11-02 18:57:09 -0300

    Actualizar comando; pendiente agregar versión Slash

--- commands/Pure/inforol.js
+++ commands/Pure/inforol.js
@@ -4,7 +4,7 @@ const { p_pure } = require('../../localdata/customization/prefixes.js');
 const { CommandOptionsManager, CommandMetaFlagsManager, CommandManager } = require('../Commons/commands');
 
 const options = new CommandOptionsManager()
-	.addParam('búsqueda', 'ROLE', 'para especificar los roles que quieres buscar', { poly: 'MULTIPLE', polymax: 8 })
+	.addParam('términos', 'ROLE', 'para especificar los roles que quieres buscar', { poly: 'MULTIPLE', polymax: 8 })
 	.addFlag('xe', ['estricta', 'estricto', 'exclusivo'], 'para especificar si la búsqueda es estricta');
 const flags = new CommandMetaFlagsManager().add('MOD');
 const command = new CommandManager('inforol', flags)
@@ -19,52 +19,61 @@ const command = new CommandManager('inforol', flags)
 		'Si la búsqueda es `--estricta`, solo se listarán los usuarios que tengan _todos_ los roles mencionados, en lugar de _uno o más_.',
 	)
 	.setOptions(options)
+	.setExperimental(false)
 	.setExecution(async (request, args, isSlash) => {
-		if(!isSlash) {
-			if(args.length < 1)
-				return request.reply({ content: `:x: ¡Debes ingresar al menos un parámetro!\nUsa \`${p_pure(request.guildId).raw}ayuda inforol\` para más información` });
-			
-			args = regroupText(args);
-		}
+		if(!isSlash && args.length < 1)
+			return request.reply({ content: `:x: ¡Debes ingresar al menos un parámetro!\nUsa \`${p_pure(request.guildId).raw}ayuda inforol\` para más información` });
 
 		//Parámetros
 		const servidor = request.channel.guild;
         const strict = options.fetchFlag(args, 'estricta');
+		let roleIds;
 
-		//Adquirir ID de roles
-		request.channel.sendTyping();
-		args = args.map(arg => {
-			if(arg.startsWith('<@&') && arg.endsWith('>'))
-				arg = arg.slice(3, -1);
-			if(isNaN(arg)) {
-				arg = arg.toLowerCase();
-				arg = servidor.roles.cache.find(r => r.name.toLowerCase().indexOf(arg) >= 0);
-				if(typeof arg === 'undefined')
-					arg = null;
-				else
-					arg = arg.id;
-			}
-			return arg;
-		}).filter(arg => arg);
+		if(!isSlash) {
+			args = args.map(arg => {
+				if(arg.startsWith('<@&') && arg.endsWith('>'))
+					return `,${arg},`;
+				return arg;
+			})
+			args = regroupText(args);
 
-		if(!args.length)
+			//Adquirir ID de roles
+			request.channel.sendTyping();
+			roleIds = args.map(arg => {
+				if(arg.startsWith('<@&') && arg.endsWith('>'))
+					arg = arg.slice(3, -1);
+				if(isNaN(arg)) {
+					arg = arg.toLowerCase();
+					arg = servidor.roles.cache.find(r => r.name.toLowerCase().indexOf(arg) >= 0);
+					if(arg == undefined)
+						arg = null;
+					else
+						arg = arg.id;
+				}
+				return arg;
+			}).filter(arg => arg);
+		} else
+			roleIds = options.fetchParamPoly(args, 'términos', args.getRole, null).filter(arg => arg);
+
+		if(!roleIds.length)
 			return request.reply({ content: '⚠ No se encontró ningún rol...' });
 
 		//Contadores de usuarios
 		const rolemembers = servidor.members.cache.filter(member => //Usuarios con rol
 			strict
-				? args.every(arg => member.roles.cache.has(arg))
-				: args.some(arg => member.roles.cache.has(arg))
+				? roleIds.every(arg => member.roles.cache.has(arg))
+				: roleIds.some(arg => member.roles.cache.has(arg))
 		);
 		const totalcnt = rolemembers.size;
 		const peoplecnt = rolemembers.filter(member => !member.user.bot).size;
 		const botcnt = totalcnt - peoplecnt;
 
 		//Crear y usar embed
+		const user = request.author ?? request.user;
 		let SelectedEmbed = 0;
 		let Embed = [];
 		let peoplelist = [...rolemembers.values()];
-		const anaroles = args.map(ar => `<@&${ar}>`).join(', ');
+		const anaroles = roleIds.map(ar => `<@&${ar}>`).join(', ');
 
 		if(anaroles.length === 0)
 			return request.reply({ content: '⚠ Entrada inválida' });
@@ -78,7 +87,7 @@ const command = new CommandManager('inforol', flags)
 				{ name: 'Cuenta total', value: `:wrestlers: x ${peoplecnt}\n:robot: x ${botcnt}`, inline: true },
 			)
 			.setThumbnail(servidor.iconURL)
-			.setAuthor({ name: `Comando invocado por ${request.author.username}`, iconURL: request.author.avatarURL() })
+			.setAuthor({ name: `Comando invocado por ${user.username}`, iconURL: user.avatarURL() })
 			.setFooter({ text: `Página principal` });
 
 		for(let i = 0; i < (totalcnt / 10); i++) {
@@ -93,9 +102,9 @@ const command = new CommandManager('inforol', flags)
 				.setColor('#ff00ff')
 				.setTitle('Análisis del roles (Detalle)')
 
-				.addField('Lista de usuarios', plrange)
+				.setFields({ name: 'Lista de usuarios', value: plrange })
 
-				.setAuthor({ name: `Comando invocado por ${request.author.username}`, iconURL: request.author.avatarURL() })
+				.setAuthor({ name: `Comando invocado por ${user.username}`, iconURL: user.avatarURL() })
 				.setFooter({ text: `Página de lista ${i + 1}/${Math.ceil(totalcnt / 10)}` });
 		}
 		
