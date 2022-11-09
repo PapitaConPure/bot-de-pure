cfbc5b0a4d4ca86b4083887585621771d0c5edfe
I. Z <ojitosclaros_ivancho@hotmail.com>
2022-11-09 19:14:46 -0300

    Agentes de Webhook más fáciles de usar

---
+++ systems/discordagent.js
@@ -0,0 +1,57 @@
+const { TextChannel, User, MessagePayload } = require('discord.js');
+
+class DiscordAgent {
+    constructor() {
+        this.webhook = null;
+        this.user = null;
+    };
+
+    /**
+     * @param {TextChannel} channel 
+     * @param {String} name 
+     * @returns 
+     */
+    async setup(channel, name = 'Agente Puré') {
+        const channelWebhooks = await channel.fetchWebhooks();
+        this.webhook = channelWebhooks.find(wh => wh.token && wh.channelId === channel.id);
+        
+        if(!this.webhook)
+            this.webhook = await channel.createWebhook(name);
+
+        return this;
+    };
+
+    /**
+     * @param {User} user
+     */
+    setUser(user) {
+        this.user = user;
+        return this;
+    };
+
+    /**
+     * 
+     * @param {import('discord.js').WebhookMessageOptions } messageOptions 
+     * @returns 
+     */
+    async sendAsUser(messageOptions) {
+        if(!this.user)
+            throw new ReferenceError('No se ha definido un usuario');
+
+        try {
+            console.log(`Usuario asociado: ${this.user}`)
+            console.log(`Webhook asociado: ${this.webhook}`)
+            return await this.webhook.send({
+                ...messageOptions,
+                username: this.user.username,
+                avatarURL: this.user.avatarURL({ dynamic: true }),
+            })
+        } catch(e) {
+            return console.error(e);
+        }
+    }
+}
+
+module.exports = {
+    DiscordAgent,
+};
