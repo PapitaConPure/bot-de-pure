4d463e0b0d976dd7d22c393bc2f6621a1a80d83b
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-01-08 09:54:17 -0300

    Nueva estructura de database para Hourai

--- localdata/models/hourai.js
+++ localdata/models/hourai.js
@@ -3,6 +3,26 @@ const Mongoose = require('mongoose');
 const HouraiSchema = new Mongoose.Schema({
     userInfractions: { type: Object, default: {} },
     customRoles: { type: Object, default: {} },
+    mentionRoles: {
+        type: Object,
+        default: {
+            GAMES: {
+                functionName: 'selectGame',
+                rolePool: [],
+                exclusive: false,
+            },
+            DRINKS: {
+                functionName: 'selectDrink',
+                rolePool: [],
+                exclusive: false,
+            },
+            FAITH: {
+                functionName: 'selectReligion',
+                rolePool: [],
+                exclusive: true,
+            },
+        },
+    },
 });
 
 module.exports = Mongoose.model('Hourai', HouraiSchema);
