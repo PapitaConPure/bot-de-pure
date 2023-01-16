401fa7af90b62031712891fe9b13ac684f8d1fbd
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-01-16 07:38:14 -0300

    Actualizar comandos

--- commands/Pure/encuesta.js
+++ commands/Pure/encuesta.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js'); //Integrar discord.js
-const { fetchFlag, fetchSentence } = require('../../func.js');
+const { fetchSentence } = require('../../func.js');
 const { CommandManager } = require('../Commons/cmdBuilder.js');
 const { CommandMetaFlagsManager } = require('../Commons/cmdFlags.js');
 const { CommandOptionsManager } = require('../Commons/cmdOpts.js');
@@ -10,7 +10,6 @@ const options = new CommandOptionsManager()
 	.addFlag('h',  ['hora', 'horas'], 	     'para añadir tiempo en horas',    { name: 'n', type: 'NUMBER' })
 	.addFlag('m',  ['minuto', 'minutos'],    'para añadir tiempo en minutos',  { name: 'n', type: 'NUMBER' })
 	.addFlag('s',  ['segundo', 'segundos'],  'para añadir tiempo en segundos', { name: 'n', type: 'NUMBER' });
-
 const flags = new CommandMetaFlagsManager().add('MOD');
 const command = new CommandManager('encuesta', flags)
 	.setAliases(
@@ -30,30 +29,28 @@ const command = new CommandManager('encuesta', flags)
 			return request.reply({ content: ':warning: Necesitas ingresar al menos dos opciones' });
 
 		//Parámetros de comando
-		let channel = fetchFlag(args, {
-			property: true,
-			short: [ 'c' ],
-			long: [ 'canal', 'channel' ],
-			callback: (x,i)=> {
-				let cs = x[i];
+		let channel = options.fetchFlag(args, 'canal', {
+			callback: (cs, isSlash) => {
+				if(isSlash) return cs;
+
 				if(cs.startsWith('<#') && cs.endsWith('>'))
 					cs = cs.slice(2, -1);
 				return request.guild.channels.cache.find(c => c.name.toLowerCase().indexOf(cs) !== -1 || c.id === cs);
 			},
+			fallback: request.channel,
 		});
-		if(channel === undefined) channel = request.channel;
-		const question = fetchFlag(args, { property: true, short: ['p', 'q'], long: ['pregunta', 'question'], callback: fetchSentence });
+		const question = options.fetchFlag(args, 'pregunta');
 		const intOrZero = (n) => {
 			const parsed = parseInt(n);
 			if(isNaN(parsed)) return 0;
-			else return parsed;
+			return parsed;
 		};
 		const time = {
-			h: fetchFlag(args, { property: true, short: ['h'], long: ['hora', 'horas'], callback: (x,i) => intOrZero(x[i]), fallback: 0 }),
-			m: fetchFlag(args, { property: true, short: ['m'], long: ['minuto', 'minutos'], callback: (x,i) => intOrZero(x[i]), fallback: 0 }),
-			s: fetchFlag(args, { property: true, short: ['s'], long: ['segundo', 'segundos'], callback: (x,i) => intOrZero(x[i]), fallback: 0 }),
+			h: options.fetchFlag(args, 'hora',    { callback: intOrZero, fallback: 0 }),
+			m: options.fetchFlag(args, 'minuto',  { callback: intOrZero, fallback: 0 }),
+			s: options.fetchFlag(args, 'segundo', { callback: intOrZero, fallback: 0 }),
 			t: 0
-		}
+		};
 		if(!time.h && !time.m && !time.s)
 			time.m = 1;
 		time.t = Object.values(time).slice(0, -1).reduce((a, b) => a * 60 + b);
@@ -65,7 +62,7 @@ const command = new CommandManager('encuesta', flags)
 			return request.reply(':warning: Para crear una encuesta, debes ingresar al menos 2 opciones');
 		
 		const eregexp = /^<a*:\w+:[0-9]+>\B/;
-		const options = [];
+		const answers = [];
 		const resolveEmote = (uemt) => request.guild.emojis.resolve(uemt.slice(uemt.lastIndexOf(':') + 1, -1));
 		args.reduce((a, b, i) => {
 			const last = (i === (args.length - 1));
@@ -85,17 +82,17 @@ const command = new CommandManager('encuesta', flags)
 					if(last) {
 						if(!be) {
 							option.text = `${option.text} ${b}`;
-							options.push(option);
+							answers.push(option);
 						} else {
-							options.push(option);
-							options.push({ emote: resolveEmote(be), text: '' });
+							answers.push(option);
+							answers.push({ emote: resolveEmote(be), text: '' });
 						}
-					} else options.push(option);
+					} else answers.push(option);
 					return b;
 				}
 			} else return b;
 		});
-		if(options.length < 2)
+		if(answers.length < 2)
 			return request.reply({ content: ':warning: Necesitas ingresar al menos dos opciones' });
 
 		const embed = new Discord.MessageEmbed()
@@ -104,12 +101,12 @@ const command = new CommandManager('encuesta', flags)
 			.setFooter({ text: `Tiempo para votar: ${time.h}h, ${time.m}m, ${time.s}s` })
 			.addFields({
 				name: question || 'Opciones',
-				value: options.map(o => `${o.emote} ${o.text}`).join('\n'),
+				value: answers.map(o => `${o.emote} ${o.text}`).join('\n'),
 			});
 		
 		const sent = await channel.send({ embeds: [embed] });
-		Promise.all([ options.map(o => sent.react(o.emote)) ]);
-		const filter = (rc, u) => !u.bot && options.some(option => rc.emoji.id === option.emote.id);
+		Promise.all([ answers.map(o => sent.react(o.emote)) ]);
+		const filter = (rc, u) => !u.bot && answers.some(option => rc.emoji.id === option.emote.id);
 		const collector = sent.createReactionCollector({ filter: filter, time: time.t * 1000 });
 		collector.on('collect', () => { return; });
 		collector.on('end', rcs => {
@@ -127,7 +124,7 @@ const command = new CommandManager('encuesta', flags)
 				.setAuthor({ name: 'Encuesta finalizada', iconURL: request.guild.iconURL({ dynamic: false, size: 256 }) })
 				.addFields({
 					name: question || 'Resultados de la votación',
-					value: options
+					value: answers
 						.sort((a,b) => (counts[b.emote.id] || 0) - (counts[a.emote.id] || 0))
 						.map(o => `${o.emote} **x ${(counts[o.emote.id] || 1) - 1}** ${o.text}`)
 						.join('\n'),
