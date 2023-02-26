4dbed2e96a0d99dd9fded779ff8415a5888f5b7d
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-02-26 14:29:52 -0300

    Mejorar botón de documentación

--- commands/Pure/tubérculo.js
+++ commands/Pure/tubérculo.js
@@ -55,7 +55,7 @@ const paginationRows = (page, lastPage, navigationEnabled = true) => {
 			new MessageButton()
 				.setURL('https://drive.google.com/drive/folders/1wv2-n4J5SSZNH9oQ5gNEPpptm7rNFEnV?usp=share_link')
 				.setLabel('Aprende PuréScript (v1.0)')
-				.setEmoji('📕')
+				.setEmoji('📖')
 				.setStyle('LINK'),
 		),
 	);
@@ -236,7 +236,7 @@ const command = new CommandManager('tubérculo', flags)
 		'En caso de estar creando un Tubérculo, se requerirá un `<mensaje>` y/o `<archivos>`, junto a la `<id>` que quieras darle al mismo. Si la ID ya está registrada, será *editada*',
 		'En cualquier parte del contenido del mensaje, coloca "#FIN#" para bajar un renglón (no es necesario con PuréScript)',
 		'En caso de estar editando o borrando un Tubérculo existente, se requerirá su TuberID',
-		'Puedes leer o descargar la documentación de la versión 1.0 de PuréScript desde [aquí](https://drive.google.com/file/d/1KebeyvsjJUqpInvCiy8wlOJ-M_ewrTch/view?usp=share_link) (3MB)',
+		'Puedes leer o descargar la documentación de la versión 1.0 de PuréScript desde [aquí](https://drive.google.com/drive/folders/1wv2-n4J5SSZNH9oQ5gNEPpptm7rNFEnV?usp=share_link) (~3MiB)',
 		'Nótese que el lenguaje se encuentra en una etapa prematura y puede tener bugs o cambiar considerablemente',
 	)
 	.setOptions(options)
