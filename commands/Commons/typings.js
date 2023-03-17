dba5e19fa22330e52867c8545b6a15b6ff9f77a2
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-03-17 00:37:59 -0300

    Mejorar tipado de commandos

--- commands/Commons/typings.js
+++ commands/Commons/typings.js
@@ -1,6 +1,8 @@
-const { Message, CommandInteraction, CommandInteractionOptionResolver } = require("discord.js");
+const { Message, User, CommandInteraction, CommandInteractionOptionResolver } = require("discord.js");
 
 /**
  * @typedef {Message | CommandInteraction} CommandRequest
+ * @typedef {{user: User, userId: String}} ExtendedCommandRequest
+ * @typedef {CommandRequest & ExtendedCommandRequest} ComplexCommandRequest
  * @typedef {Array<*> | CommandInteractionOptionResolver} CommandOptions
  */
