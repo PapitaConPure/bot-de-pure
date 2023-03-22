282a939f8f7fdc42746fbe275ee82392e303cdd9
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-03-22 13:36:31 -0300

    Remover prefijo de Drawmaku

--- func.js
+++ func.js
@@ -89,10 +89,10 @@ module.exports = {
             content: 
                 '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬\n' + 
                 `:art: *Le toca dibujar a <@${global.jugadores[global.ndibujante]}> (jugador ${global.numeros[global.ndibujante]}).* :art:\n` +
-                `_Para enviar el nombre de tu danmaku, ingresa \`${global.p_drmk.raw}draw ||<danmaku>||\`. **No olvides las \\|\\|barras verticales\\|\\|**._\n` +
-                `_Para enviar el dibujo de tu danmaku, ingresa \`${global.p_drmk.raw}draw <dibujo de danmaku>\`. **El dibujo se envía adjunto al comando**._\n` +
-                `_Para consultar la lista de usuarios envía \`${global.p_drmk.raw}lista\`_\n` +
-                `_Para consultar todos los comandos ingresa \`${global.p_drmk.raw}ayuda\`_\n` +
+                `_Para enviar el nombre de tu danmaku, ingresa \`${p_pure().raw}draw ||<danmaku>||\`. **No olvides las \\|\\|barras verticales\\|\\|**._\n` +
+                `_Para enviar el dibujo de tu danmaku, ingresa \`${p_pure().raw}draw <dibujo de danmaku>\`. **El dibujo se envía adjunto al comando**._\n` +
+                `_Para consultar la lista de usuarios envía \`${p_pure().raw}lista\`_\n` +
+                `_Para consultar todos los comandos ingresa \`${p_pure().raw}ayuda\`_\n` +
                 '▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬'
         });
     },
--- localdata/customization/prefixes.js
+++ localdata/customization/prefixes.js
@@ -1,16 +1,13 @@
-const { p_pure, p_drmk } = require('../config.json');
+const { p_pure } = require('../config.json');
+
+/**
+ * @typedef {{raw: String, regex: RegExp}} PrefixPair
+ */
 
 module.exports = {
     /**
      * @param {String} gid La id del servidor
-     * @typedef {{raw: String, regex: RegExp}} PrefixPair
      * @returns {PrefixPair} Un objeto conteniendo el texto crudo y RegExp del prefijo
      */
     p_pure: (gid = '0') => p_pure[gid] || p_pure['0'],
-    /**
-     * @param {String} gid La id del servidor
-     * @typedef {{raw: String, regex: RegExp}} PrefixPair
-     * @returns {PrefixPair} Un objeto conteniendo el texto crudo y RegExp del prefijo
-     */
-    p_drmk: (gid = '0') => p_drmk[gid] || p_drmk['0']
 }
