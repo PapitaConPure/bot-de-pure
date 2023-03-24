af81b7bb5fb5f00092bc2435592e750e5735f33c
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-03-24 12:37:02 -0300

    bugfix + mejorar tipado

--- events/onRateLimit.js
+++ events/onRateLimit.js
@@ -1,9 +1,10 @@
 const chalk = require('chalk');
 
+/**@param {import('discord.js').RateLimitData} rateLimit*/
 function onRateLimit(rateLimit) {
     console.log(
         chalk.redBright('RateLimit'),
-        chalk.yellowBright(`(${rateLimit.timeout}ms / global ${rateLimit.global}):`),
+        chalk.yellowBright(`(${rateLimit.timeToReset}ms / global ${rateLimit.global}):`),
         chalk.bgCyanBright.black.bold(`${rateLimit.method} →`), chalk.greenBright(rateLimit.route),
     );
 }
