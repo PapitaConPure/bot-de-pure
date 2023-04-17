69243ffb8cc4b0292e919743af88383c742a2c38
I. Z <ojitosclaros_ivancho@hotmail.com>
2023-04-17 02:05:42 -0300

    Mejorar tipado

--- commands/Commons/typings.js
+++ commands/Commons/typings.js
@@ -1,7 +1,7 @@
 const { Message, User, CommandInteraction, CommandInteractionOptionResolver } = require("discord.js");
 
 /**
- * @typedef {Message | CommandInteraction} CommandRequest
+ * @typedef {Message<true> | CommandInteraction<true>} CommandRequest
  * @typedef {{user: User, userId: String}} ExtendedCommandRequest
  * @typedef {CommandRequest & ExtendedCommandRequest} ComplexCommandRequest
  * @typedef {Array<*> | CommandInteractionOptionResolver} CommandOptions
