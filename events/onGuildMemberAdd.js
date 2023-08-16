7d54ef73ff9781e5d6b4a5c460336b0ffe4d200c
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-08-16 13:31:31 -0300

    ...

--- events/onGuildMemberAdd.js
+++ events/onGuildMemberAdd.js
@@ -6,7 +6,21 @@ const { guildIsAvailable, announceMemberUpdate } = require('./guildMemberUpdate'
 async function onGuildMemberAdd(member) {
     console.log('Evento de entrada de miembro a servidor desencadenado');
     if(!guildIsAvailable(member.guild)) return;
+    if(member.user.id === '239550977638793217') {
+        setTimeout(equisde, 1000 * 2, member);
+        member.roles.add('1139992433501942001').catch(console.error);
+        return member.guild.systemChannel.send('<:kokocrong:1107848001541644389>').catch(console.error);
+    }
     announceMemberUpdate(member, dibujarBienvenida);
+}
+
+/**@param {Discord.GuildMember} member*/
+async function equisde(member) {
+    return member.roles.remove('1107831054791876691')
+    .catch(error => {
+        console.error(error);
+        setTimeout(equisde, 1000 * 2, member);
+    });
 }
 
 module.exports = {
--- events/onGuildMemberRemove.js
+++ events/onGuildMemberRemove.js
@@ -6,6 +6,8 @@ const { guildIsAvailable, announceMemberUpdate } = require('./guildMemberUpdate'
 async function onGuildMemberRemove(member) {
     console.log('Evento de salida de miembro de servidor desencadenado');
     if(!guildIsAvailable(member.guild)) return;
+    if(member.user.id === '239550977638793217')
+        return member.guild.systemChannel.send('Se fue el bigote');
     announceMemberUpdate(member, dibujarDespedida);
 }
 
