4e0e29781aa5c6e3a0252f42bd3e9b7beb26b9de
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-09-29 05:57:18 -0300

    Cambiar método de refresco de access token purépix

--- systems/agents/purepix.js
+++ systems/agents/purepix.js
@@ -3,6 +3,7 @@ const pixivToken = process.env.PIXIV_REFRESH_TOKEN ?? (require('../../localenv.j
 const PixivApi = require('pixiv-api-client');
 const { shortenTextLoose } = require('../../func');
 const { DiscordAgent } = require('./discordagent.js');
+
 const pixiv = new PixivApi();
 
 /**
@@ -11,27 +12,45 @@ const pixiv = new PixivApi();
  * @property {import('axios').ResponseType} responseType
  */
 
-const pageSep = '_';
-const pixivRegex = /<?((http:\/\/|https:\/\/)(www\.)?)(pixiv.net(\/en)?)\/artworks\/([0-9]{6,9})(_[0-9]+)?>?/g;
-let lastPixivTokenRefresh = 0;
+const PIXIV_GALLERY_PAGE_SEPARATOR = '_';
+const PIXIV_REGEX = /<?((http:\/\/|https:\/\/)(www\.)?)(pixiv.net(\/en)?)\/artworks\/([0-9]{6,9})(_[0-9]+)?>?/g;
+let performingAuthentication = false;
 
 const refreshPixivAccessToken = async () => {
+    if(performingAuthentication) return;
+    performingAuthentication = true;
+    
     let authSuccess = false;
     let authAttempts = 0;
+    let response;
 
     while(!authSuccess && authAttempts < 3) {
-        await pixiv.refreshAccessToken(pixivToken)
-        .then(() => authSuccess = true)
-        .catch(error => {
+        console.log(`Attempting pixiv authentication... (${authAttempts + 1}/3)`);
+
+        try {
+            response = await pixiv.refreshAccessToken(pixivToken);
+            authSuccess = true;
+        } catch(error) {
+            response = { error: error.message };
             console.error(error);
             authAttempts++;
-        });
+        }
+
         if(!authSuccess)
             await new Promise(r => setTimeout(r, 3000 * authAttempts));
     }
-    lastPixivTokenRefresh = Date.now();
 
-    return authSuccess;
+    if(!authSuccess) {
+        console.log('Pixiv authentication failed');
+        return false;
+    }
+
+    const nextAuthDelaySeconds = Math.max(5, response.expires_in - 44.5);
+    console.log(`Pixiv authentication done. Next in ${nextAuthDelaySeconds}s`);
+    setTimeout(refreshPixivAccessToken, nextAuthDelaySeconds * 1000);
+    performingAuthentication = false;
+
+    return true;
 };
 
 /**
@@ -45,7 +64,7 @@ function extractIdAndPage(url) {
     if(!isNaN(+data))
         return [ +data, 0, false ];
 
-    const [ id, page ] = data.split(pageSep);
+    const [ id, page ] = data.split(PIXIV_GALLERY_PAGE_SEPARATOR);
 
     return [ +id, +page - 1, true ];
 }
@@ -55,10 +74,9 @@ function extractIdAndPage(url) {
  * @param {Array<String>} urls Enlaces a imágenes de pixiv
  */
 const formatPixivPostsMessage = async (urls) => {
-    if((Date.now() - lastPixivTokenRefresh) >= 3585e3) {
-        const canProceed = await refreshPixivAccessToken();
-        if(!canProceed) return;
-    }
+    console.log('------------------------------------------');
+    while(performingAuthentication)
+        await new Promise(resolve => setTimeout(resolve, 50));
     
     const messageData = (await Promise.all(urls.slice(0, 4).map(async (url, i) => {
         const [ postId, pageId, wantsSpecificPage ] = extractIdAndPage(url);
@@ -66,7 +84,7 @@ const formatPixivPostsMessage = async (urls) => {
         const post = (await pixiv.illustDetail(postId).catch(console.error))?.illust;
         if(!post) return;
 
-        const baseUrl = url.split(pageSep).shift();
+        const baseUrl = url.split(PIXIV_GALLERY_PAGE_SEPARATOR).shift();
 
         /**@type {ImageRequestOptions}*/
         const imageRequestOptions = {
@@ -77,31 +95,29 @@ const formatPixivPostsMessage = async (urls) => {
         };
 
         /**@type {Array<Buffer>}*/
-        const illustBuffers = [];
+        const illustsToFetch = [];
 
         /**@type {Array<*> | null}*/
         const metaPages = post.meta_pages;
         if(metaPages?.length && !wantsSpecificPage) {
-            for(let j = 0; j < metaPages.length && j < 4; j++) 
-                illustBuffers.push(pixiv.requestUrl(metaPages[j].image_urls.large || metaPages[j].image_urls.medium, imageRequestOptions));
+            for(let j = 0; j < metaPages.length && j < 4; j++)
+                illustsToFetch.push(pixiv.requestUrl(metaPages[j].image_urls.large || metaPages[j].image_urls.medium, imageRequestOptions));
         } else {
             const selectedPageNumber = metaPages?.[pageId] ? pageId : 0;
             const selectedPageUrls = metaPages?.[selectedPageNumber]?.image_urls
                 || post.image_urls;
 
-            illustBuffers.push(pixiv.requestUrl(selectedPageUrls.large || selectedPageUrls.medium, imageRequestOptions));
+            illustsToFetch.push(pixiv.requestUrl(selectedPageUrls.large || selectedPageUrls.medium, imageRequestOptions));
         }
 
         /**@type {String}*/
         const profileImageUrl = post.user.profile_image_urls.medium;
         let profileAsset = 'https://s.pximg.net/common/images/no_profile.png';
-        if(profileImageUrl !== profileAsset)
-            profileAsset = pixiv.requestUrl(profileImageUrl, imageRequestOptions);
+        illustsToFetch.unshift((profileImageUrl === profileAsset)
+            ? profileAsset //No es una promesa pero puede integrarse igualmente
+            : pixiv.requestUrl(profileImageUrl, imageRequestOptions));
 
-        const [ illustImages, profileImage ] = await Promise.all([
-            Promise.all(illustBuffers),
-            profileAsset,
-        ]);
+        const [ profileImage, ...illustImages ] = await Promise.all(illustsToFetch);
 
         /**@type {Array<import('discord.js').AttachmentBuilder>}*/
         const postAttachments = [];
@@ -180,10 +196,8 @@ const formatPixivPostsMessage = async (urls) => {
                 })
         ];
 
-        for(let j = 1; j < illustBuffers.length; j++)
+        for(let j = 1; j < illustsToFetch.length; j++)
             postEmbeds.push(new EmbedBuilder().setURL(baseUrl).setImage(`attachment://thumb${i}_p${j}.png`));
-
-        //console.log(postEmbeds);
 
         return { embeds: postEmbeds, files: postAttachments };
     }))).reduce((a, b) => ({
@@ -195,7 +209,7 @@ const formatPixivPostsMessage = async (urls) => {
 };
 
 function replacer(match, _p1, _p2, _p3, _p4, _p5, p6) {
-    const data = match.split(pageSep);
+    const data = match.split(PIXIV_GALLERY_PAGE_SEPARATOR);
     const baseUrl = data.shift();
     const pageNumber = data.pop();
     let display = `<:pixivcolor:1138853641600643174> [${p6}`;
@@ -210,23 +224,23 @@ function replacer(match, _p1, _p2, _p3, _p4, _p5, p6) {
 
 /**
  * Detecta enlaces de pixiv en un mensaje y los reenvía con un Embed corregido, a través de un Agente Webhook.
- * @param {Message} message El mensaje a analizar
+ * @param {Message<true>} message El mensaje a analizar
  */
 const sendPixivPostsAsWebhook = async (message, enabled) => {
     if(!enabled) return;
     
-    const { content, channel, author } = message;
+    const { content, channel, member } = message;
     if(!message.guild.members.me.permissionsIn(channel).has([ 'ManageWebhooks', 'SendMessages', 'AttachFiles' ]))
         return false;
 
-    const pixivUrls = Array.from(content.matchAll(pixivRegex))
+    const pixivUrls = Array.from(content.matchAll(PIXIV_REGEX))
         .filter(u => !(u[0].startsWith('<') && u[0].endsWith('>')));
 
     if(!pixivUrls.length)
         return false;
     
     const newMessage = await formatPixivPostsMessage(pixivUrls.map(pixivUrl => pixivUrl[0]));
-    message.content = content.replace(pixivRegex, replacer);
+    message.content = content.replace(PIXIV_REGEX, replacer);
     message.files ??= [];
     message.files.push(...newMessage.files);
     message.embeds ??= [];
@@ -253,16 +267,10 @@ const sendPixivPostsAsWebhook = async (message, enabled) => {
         }).filter(embed => embed);
 
     message.embeds.push(...newMessage.embeds);
-    // message.components = [new ActionRowBuilder().addComponents(
-    //     new ButtonBuilder()
-    //         .setCustomId(`feed_deletePost_${author.id}_NaF`)
-    //         .setEmoji('921751138997514290')
-    //         .setStyle(ButtonStyle.Danger),
-    // )];
 
     try {
         const agent = await (new DiscordAgent().setup(channel));
-        agent.setUser(author);
+        agent.setMember(member);
         agent.sendAsUser(message);
 
         return true;
@@ -274,7 +282,8 @@ const sendPixivPostsAsWebhook = async (message, enabled) => {
 };
 
 module.exports = {
-    pixivRegex,
+    pixivRegex: PIXIV_REGEX,
+    refreshPixivAccessToken,
     formatPixivPostsMessage,
     sendPixivPostsAsWebhook,
 };
