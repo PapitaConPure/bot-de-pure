0739bf4a7b4b447916d441a2c3270f1c4cdccc2b
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-10-24 22:50:26 -0300

    Permitir múltiples queues de concurrencia

--- concurrency.js
+++ concurrency.js
@@ -1,28 +1,97 @@
-/**@param {() => Promise<*>} callback*/
-async function scheduleTask(callback) {
-	scheduleTask['queue'] ??= [];
-	const queue = scheduleTask['queue'];
+/**@template T*/
+class QueueNode {
+	/**@type {T}*/
+	value;
+	/**@type {QueueNode<T>?}*/
+	next;
 
-	let resolveResult;
-	const result = new Promise(resolve => {
-		resolveResult = resolve;
-	});
+	/**@param {T} value*/
+	constructor(value) {
+		this.value = value;
+	}
+}
 
-	const performQueueStep = async () => {
-		const result = await callback();
-		queue.shift();
-		queue[0]?.();
-		resolveResult(result);
+/**@template T*/
+class Queue {
+	/**@type {number}*/
+	count;
+	/**@type {QueueNode<T>?}*/
+	#head;
+	/**@type {QueueNode<T>?}*/
+	#tail;
+
+	constructor() {
+		this.count = 0;
+		this.#head = null;
+		this.#tail = null;
+	}
+
+	/**@param {T} value*/
+	enqueue(value) {
+		const node = new QueueNode(value);
+
+		if(this.#head == null) {
+			this.#head = node;
+			this.#tail = node;
+		} else {
+			this.#head.next = node;
+			this.#head = node;
+		}
+
+		this.count++;
+	}
+
+	dequeue() {
+		if(this.#tail == null) return null;
+
+		const node = this.#tail;
+		this.#tail = this.#tail.next;
+
+		if(this.#tail == null)
+			this.#head = null;
+
+		this.count--;
+
+		return node.value;
+	}
+
+	peek() {
+		return this.#tail?.value;
+	}
+}
+
+function createTaskScheduler() {
+	/**@type {Queue<() => Promise<*>>}*/
+	const queue = new Queue();
+	
+	/**@param {() => Promise<*>} callback*/
+	const scheduleTask = async function(callback) {
+		let resolveResult;
+		const result = new Promise(resolve => {
+			resolveResult = resolve;
+		});
+	
+		const performQueueStep = async () => {
+			const result = await callback();
+			queue.dequeue();
+			queue.peek()?.();
+			resolveResult(result);
+		};
+	
+		queue.enqueue(performQueueStep);
+	
+		if(queue.count === 1)
+			await performQueueStep();
+	
+		return result;
+	}
+
+	return {
+		queue,
+		scheduleTask,
 	};
-
-	queue.push(performQueueStep);
-
-	if(queue.length === 1)
-		await performQueueStep();
-
-	return result;
 }
 
 module.exports = {
-	scheduleTask,
+	createTaskScheduler,
 };
