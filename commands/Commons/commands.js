8fcd7c0e0055aeb2a32110ada8da6d5d08b66163
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-03-28 07:20:54 -0300

    Facilitar el acceso a nombres de archivos de comando

--- commands/Commons/commands.js
+++ commands/Commons/commands.js
@@ -1,9 +1,13 @@
+const { readdirSync } = require('fs');
 const commandOptions = require('./cmdOpts');
 const commandFlags = require('./cmdTags');
 const commandBuilder = require('./cmdBuilder');
+
+const commandFilenames = readdirSync('./commands/Pure').filter(file => file.endsWith('.js'));
 
 module.exports = {
     ...commandOptions,
     ...commandFlags,
     ...commandBuilder,
+    commandFilenames,
 };
