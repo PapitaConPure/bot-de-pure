0594bbc1b65e294de1ce0ed00826c7b5601642a5
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-03-29 18:06:31 -0300

    Acción "encontrar fuente"

---
+++ actions/Instances/findSource.js
@@ -0,0 +1,60 @@
+const { Attachment } = require('discord.js');
+const { Translator } = require('../../internationalization.js');
+const { ContextMenuActionManager } = require('../Commons/actionBuilder.js');
+const { injectSauceNAOEmbeds } = require('../../systems/others/saucenao.js');
+const SauceNAOUser = require('../../localdata/models/saucenaoUsers');
+
+const action = new ContextMenuActionManager('actionFindSource', 'Message')
+    .setMessageResponse(async interaction => {
+        const message = interaction.targetMessage;
+        const uid = interaction.user.id;
+        const translator = await Translator.from(uid);
+
+        if((message.webhookId == undefined || message.webhookId.length == 0) && message.author.id !== interaction.client.user.id)
+            return interaction.reply({
+                content: translator.getText('invalidMessage'),
+                ephemeral: true,
+            });
+            
+        const sauceNAOUser = (await SauceNAOUser.findOne({ userId: interaction.user.id }));
+        if(!sauceNAOUser)
+            return interaction.reply({
+                content: translator.getText('saucenaoUnregisteredNotice'),
+                ephemeral: true,
+            });
+
+        const messageAttachments = message.attachments
+            ? [ ...message.attachments.values() ]
+            : /**@type {Array<Attachment>}*/([]);
+		
+		const attachmentUrls = messageAttachments.map(att => att.url);
+		const otherMessageUrls = message.embeds
+			?.flatMap(e => [ e.image?.url, e.thumbnail?.url ])
+			.filter(u => u)
+			|| [];
+
+		const queries = [
+			...attachmentUrls,
+			...otherMessageUrls,
+		].slice(0, 5);
+
+		if(!queries.length)
+			return interaction.reply({
+                content: translator.getText('saucenaoInvalidImage'),
+                ephemeral: true,
+            });
+        
+		await interaction.deferReply({ ephemeral: true });
+
+		const successes = [];
+		const failures = [];
+		
+		await injectSauceNAOEmbeds(sauceNAOUser.clientId, queries, translator, { successes, failures });
+
+		if(!successes.length && !failures.length)
+			return interaction.editReply({ content: translator.getText('saucenaoInvalidImage') });
+
+		return interaction.editReply({ embeds: [ ...successes, ...failures ] });
+    });
+
+module.exports = action;
