67d25114fb804220f81dd6097d33b9d91bbdacf6
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-04-07 03:37:35 -0300

    Correcciones de diseño y optimización

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,19 +7,20 @@ import {
   faCode,
   faCogs,
   faCouch,
-  faFaceGrin,
+  faGrinTongueWink,
   faHeadphonesAlt,
   faImage,
   faList,
+  faMusic,
   faRobot,
   faScrewdriver,
   faScrewdriverWrench,
   faServer,
   faShieldAlt,
-  faStar,
   faUser,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
@@ -28,11 +29,12 @@ export default function Home() {
       <section className="grid max-h-[80vh] min-h-[80vh] grid-rows-[1fr_max-content_1fr] items-center px-8 sm:items-start sm:px-10 md:px-8 lg:px-10 xl:px-12">
         <div className="row-start-2 flex flex-col-reverse items-center justify-center gap-6 sm:gap-10 md:flex-row md:gap-20 lg:gap-20 xl:gap-26">
           <div className="flex flex-col items-center gap-2 md:items-start md:gap-[32px]">
-            <h1 className="-mb-1 max-w-74 text-center text-2xl font-extrabold tracking-[-.01em] sm:mb-0 sm:max-w-84 sm:text-[27px] md:max-w-94 md:text-left md:text-3xl lg:max-w-150 lg:text-5xl xl:max-w-188 xl:text-6xl">
+            <h1 className="mb-1 max-w-74 text-center text-2xl font-extrabold tracking-[-.01em] sm:mb-2 sm:max-w-84 sm:text-[27px] md:mb-0 md:max-w-94 md:text-left md:text-3xl lg:max-w-150 lg:text-5xl xl:max-w-188 xl:text-6xl">
               Dale a tu servidor el gusto que se merece
             </h1>
-            <p className="text-center text-xs sm:text-left sm:text-sm md:text-base lg:text-lg">
+            <p className="text-center text-xs sm:text-left sm:text-sm md:text-base lg:text-lg md:max-w-94 lg:max-w-150 xl:max-w-188">
               Utilidad. Administración. Personalización. Entretenimiento.
+              Potencia.
             </p>
 
             <div className="mt-6 flex w-3/4 flex-col items-center gap-4 sm:w-5/6 md:mt-0 md:w-full md:flex-row">
@@ -50,19 +52,19 @@ export default function Home() {
                 className="border-foreground/8 bg-foreground/2 hover:bg-foreground/5 flex h-10 w-full items-center justify-center rounded-md border border-solid px-4 text-sm font-medium transition-colors hover:border-transparent sm:h-12 sm:text-base md:w-[117px] md:px-5 lg:w-auto"
                 href="/learn"
               >
-                <span className="inline sm:hidden lg:inline">
-                  Aprender Más
-                </span>
+                <span className="inline sm:hidden lg:inline">Aprender Más</span>
                 <span className="hidden sm:inline lg:hidden">Ayuda</span>
               </Link>
             </div>
           </div>
-          <div className="relative min-w-max">
+          <div className="relative h-48 min-w-48 sm:h-64 sm:min-w-64 md:h-72 md:min-w-72 lg:h-88 lg:min-w-88 xl:h-112 xl:min-w-112">
             <div className="shadow-primary/60 bg-primary/60 absolute inset-0 h-48 scale-120 animate-[pulse_16s_cubic-bezier(0.4,0,0.6,1)_infinite] rounded-full mix-blend-lighten shadow-[0_0_600px] blur-2xl transition-all sm:h-64 md:h-72 lg:h-88 xl:h-112" />
             <div className="border-primary/30 absolute inset-0 h-48 animate-[ping_3s_cubic-bezier(0,0,0.3,1)_infinite] rounded-full border mix-blend-lighten transition-transform motion-reduce:hidden sm:h-64 md:h-72 lg:h-88 xl:h-112" />
-            <img
+            <Image
               src="./boticon.webp"
               alt="Logo de Bot de Puré"
+              fill
+              style={{ objectFit: 'cover' }}
               className="relative h-48 rounded-full transition-transform duration-300 ease-in-out hover:scale-105 sm:h-64 md:h-72 lg:h-88 xl:h-112"
             />
           </div>
@@ -70,11 +72,11 @@ export default function Home() {
       </section>
       <section className="border-t-foreground/6 flex w-full flex-col gap-y-6 border-t px-8 py-4 sm:px-10 sm:py-6 lg:px-14 lg:py-10 xl:px-16 xl:py-12">
         <div className="flex flex-col flex-nowrap gap-x-6 gap-y-6 md:flex-row md:justify-center">
-          <div className="bg-secondary/20 flex flex-col gap-y-6 self-start md:self-auto rounded-md px-6 pt-4 pb-6 w-2/3 md:w-1/2 md:grow-0 lg:px-8 lg:py-6">
+          <div className="bg-secondary/20 flex min-w-5/6 flex-col gap-y-6 self-start rounded-md px-6 pt-4 pb-6 sm:w-2/3 md:w-1/2 md:min-w-0 md:self-auto lg:px-8 lg:py-6">
             <h1 className="border-b-foreground/8 border-b pb-2 text-center text-lg font-bold md:text-xl">
               Utilidad Ante Todo
             </h1>
-            <ul className="mx-auto flex flex-col gap-y-4 text-xs sm:text-left sm:text-sm lg:text-lg">
+            <ul className="mx-auto flex flex-col gap-y-6 text-xs sm:text-left sm:text-sm md:gap-y-5 lg:text-lg">
               <li className="flex flex-row items-center gap-x-4">
                 <FontAwesomeIcon
                   className="text-accent w-4"
@@ -92,17 +94,20 @@ export default function Home() {
               </li>
             </ul>
           </div>
-          <div className="bg-secondary/20 flex flex-col gap-y-6 self-end md:self-auto rounded-md px-6 pt-4 pb-6 w-2/3 md:w-1/2 md:grow-0 lg:px-8 lg:py-6">
+          <div className="bg-secondary/20 flex min-w-5/6 flex-col gap-y-6 self-end rounded-md px-6 pt-4 pb-6 sm:w-2/3 md:w-1/2 md:min-w-0 md:self-auto lg:px-8 lg:py-6">
             <h1 className="border-b-foreground/8 border-b pb-2 text-center text-lg font-bold md:text-xl">
               Moderación
             </h1>
-            <ul className="mx-auto flex flex-col gap-y-4 text-xs sm:text-left sm:text-sm lg:text-lg">
+            <ul className="mx-auto flex flex-col gap-y-6 text-xs sm:text-left sm:text-sm md:gap-y-5 lg:text-lg">
               <li className="flex flex-row items-center gap-x-4">
                 <FontAwesomeIcon className="text-accent w-4" icon={faList} />
                 <span>Especifica reglas flojas o estrictas.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faScrewdriverWrench} />
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faScrewdriverWrench}
+                />
                 <span>Define acceso y permisos con precisión.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
@@ -113,13 +118,13 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col flex-nowrap gap-x-6 gap-y-6 md:flex-row md:justify-center">
-          <div className="bg-secondary/20 flex flex-col gap-y-6 self-start md:self-auto rounded-md px-6 pt-4 pb-6 w-2/3 md:w-1/2 md:grow-0 lg:px-8 lg:py-6">
+          <div className="bg-secondary/20 flex min-w-5/6 flex-col gap-y-6 self-start rounded-md px-6 pt-4 pb-6 sm:w-2/3 md:w-1/2 md:min-w-0 md:self-auto lg:px-8 lg:py-6">
             <h1 className="border-b-foreground/8 border-b pb-2 text-center text-lg font-bold md:text-xl">
               Entretenimiento
             </h1>
-            <ul className="mx-auto flex flex-col gap-y-4 text-xs sm:text-left sm:text-sm lg:text-lg">
+            <ul className="mx-auto flex flex-col gap-y-6 text-xs sm:text-left sm:text-sm md:gap-y-5 lg:text-lg">
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faStar} />
+                <FontAwesomeIcon className="text-accent w-4" icon={faGrinTongueWink} />
                 <span>Comandos de memes, respuestas y emotes.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
@@ -127,16 +132,19 @@ export default function Home() {
                 <span>Búsqueda de multimedia de anime y manga.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faFaceGrin} />
-                <span>Genéticamente modificada para ser tonta.</span>
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faMusic}
+                />
+                <span>Reproducción de música fácil y sin rodeos.</span>
               </li>
             </ul>
           </div>
-          <div className="bg-secondary/20 flex flex-col gap-y-6 self-end md:self-auto rounded-md px-6 pt-4 pb-6 w-2/3 md:w-1/2 md:grow-0 lg:px-8 lg:py-6">
+          <div className="bg-secondary/20 flex min-w-5/6 flex-col gap-y-6 self-end rounded-md px-6 pt-4 pb-6 sm:w-2/3 md:w-1/2 md:min-w-0 md:self-auto lg:px-8 lg:py-6">
             <h1 className="border-b-foreground/8 border-b pb-2 text-center text-lg font-bold md:text-xl">
               Personalización de Usuario
             </h1>
-            <ul className="mx-auto flex flex-col gap-y-4 text-xs sm:text-left sm:text-sm lg:text-lg">
+            <ul className="mx-auto flex flex-col gap-y-6 text-xs sm:text-left sm:text-sm md:gap-y-5 lg:text-lg">
               <li className="flex flex-row items-center gap-x-4">
                 <FontAwesomeIcon className="text-accent w-4" icon={faUser} />
                 <span>Todas tus preferencias en un solo lugar.</span>
@@ -146,20 +154,26 @@ export default function Home() {
                 <span>Interoperabilidad con sistemas de servidor.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faScrewdriver} />
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faScrewdriver}
+                />
                 <span>Configuración directa, intuitiva y fácil.</span>
               </li>
             </ul>
           </div>
         </div>
         <div className="flex flex-col flex-nowrap gap-x-6 gap-y-6 md:flex-row md:place-content-evenly md:content-evenly md:justify-center">
-          <div className="bg-secondary/20 flex flex-col gap-y-6 self-start md:self-auto rounded-md px-6 pt-4 pb-6 w-2/3 md:w-1/2 md:grow-0 lg:px-8 lg:py-6">
+          <div className="bg-secondary/20 flex min-w-5/6 flex-col gap-y-6 self-start rounded-md px-6 pt-4 pb-6 sm:w-2/3 md:w-1/2 md:min-w-0 md:self-auto lg:px-8 lg:py-6">
             <h1 className="border-b-foreground/8 border-b pb-2 text-center text-lg font-bold md:text-xl">
               Personalización de Servidor
             </h1>
-            <ul className="mx-auto flex flex-col gap-y-4 text-xs sm:text-left sm:text-sm lg:text-lg">
+            <ul className="mx-auto flex flex-col gap-y-6 text-xs sm:text-left sm:text-sm md:gap-y-5 lg:text-lg">
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faChartDiagram} />
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faChartDiagram}
+                />
                 <span>Comandos personalizados de servidor.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
@@ -167,26 +181,35 @@ export default function Home() {
                 <span>Mecanismos absurdamente configurables.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faShieldAlt} />
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faShieldAlt}
+                />
                 <span>Medidas de seguridad cuando las necesites.</span>
               </li>
             </ul>
           </div>
-          <div className="bg-secondary/20 flex flex-col gap-y-6 self-end md:self-auto rounded-md px-6 pt-4 pb-6 w-2/3 md:w-1/2 md:grow-0 lg:px-8 lg:py-6">
+          <div className="bg-secondary/20 flex min-w-5/6 flex-col gap-y-6 self-end rounded-md px-6 pt-4 pb-6 sm:w-2/3 md:w-1/2 md:min-w-0 md:self-auto lg:px-8 lg:py-6">
             <h1 className="border-b-foreground/8 border-b pb-2 text-center text-lg font-bold md:text-xl">
               Funcionalidad Avanzada
             </h1>
-            <ul className="mx-auto flex flex-col gap-y-4 text-xs sm:text-left sm:text-sm lg:text-lg">
+            <ul className="mx-auto flex flex-col gap-y-6 text-xs sm:text-left sm:text-sm md:gap-y-5 lg:text-lg">
               <li className="flex flex-row items-center gap-x-4">
                 <FontAwesomeIcon className="text-accent w-4" icon={faCode} />
                 <span>Crea comandos avanzados con PuréScript.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faHeadphonesAlt} />
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faHeadphonesAlt}
+                />
                 <span>Despliega sesiones de voz dinámicamente.</span>
               </li>
               <li className="flex flex-row items-center gap-x-4">
-                <FontAwesomeIcon className="text-accent w-4" icon={faBookBookmark} />
+                <FontAwesomeIcon
+                  className="text-accent w-4"
+                  icon={faBookBookmark}
+                />
                 <span>Documentación extensiva y detallada.</span>
               </li>
             </ul>
