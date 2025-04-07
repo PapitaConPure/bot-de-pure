aa47643e719d2bd5eb1cfd502d71524b496deca5
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-04-07 02:12:05 -0300

    a ver

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,16 @@ const withMDX = createMDX({
   extension: /\.mdx?$/,
 });
 
+const repoName = 'bot-de-pure';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repoName}` : '',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withMDX(nextConfig);
