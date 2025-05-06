8d44738924a1c12b52a243033f716640c104aa86
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2025-05-06 17:27:59 -0300

    Actualizar tipado

--- types.js
+++ types.js
@@ -13,4 +13,11 @@
  * @typedef {{[K in keyof T]: T[K]} & {}} Flatten
  */
 
+/**
+ * @typedef {import('discord.js').JSONEncodable<import('discord.js').APIMessageTopLevelComponent>
+ *           | import('discord.js').TopLevelComponentData
+ *           | import('discord.js').ActionRowData<import('discord.js').MessageActionRowComponentData | import('discord.js').MessageActionRowComponentBuilder>
+ *           | import('discord.js').APIMessageTopLevelComponent} MessageComponentDataResolvable
+ */
+
 module.exports = {};
