f96514ffd2e8448dcb3b55840e4150ee75f8ef44
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-13 04:28:57 -0300

    TS ESM

--- commands/Instances/número.ts
+++ commands/Instances/número.ts
@@ -1,26 +1,30 @@
-const { improveNumber } = require('../../func'); //Funciones globales
-const { CommandOptions, CommandTags, Command, CommandOptionSolver } = require('../Commons/');
+import { improveNumber } from '../../func';
+import { Translator } from '../../i18n';
+import { CommandOptions, CommandTags, Command, CommandOptionSolver } from '../Commons';
 
-const flags = new CommandTags().add('COMMON');
+const tags = new CommandTags().add('COMMON');
+
 const options = new CommandOptions()
 	.addParam('num', 'NUMBER', 'para designar el número a operar')
 	.addFlag(['a','s'], 'acortar', 'para acortar el número')
 	.addFlag(['m','d'], ['mínimo','minimo','digitos'], 'para designar el mínimo de dígitos', { name: 'minimo', type: 'NUMBER' })
 	.addFlag(['n','e'], 'exponente', 'para exponenciar el número', { name: 'exp', type: 'NUMBER' });
-const command = new Command('número', flags)
+
+const command = new Command('número', tags)
 	.setAliases('numero', 'núm', 'num')
 	.setDescription('Para operar un número. Sí, solo eso, tenía ganas de jugar con algo')
 	.setOptions(options)
 	.setExecution(async (request, args) => {
-		//Acción de comando
+		const translator = await Translator.from(request);
+		
 		const shorten = args.hasFlag('acortar');
 		const minDigits = args.flagExprIf('mínimo', x => CommandOptionSolver.asNumber(x), 1);
 		const exp = args.flagExprIf('exponente', x => CommandOptionSolver.asNumber(x), 1);
 		
 		let num = args.getNumber('num');
-		if(!num) return request.reply({ content: '⚠️ Debes ingresar un número' });
+		if(!num) return request.reply({ content: translator.getText('invalidNumber') });
 		num = Math.pow(num, exp);
 		return request.reply({ content: improveNumber(num, { shorten, minDigits }) });
 	});
 
-module.exports = command;
+export default command;
