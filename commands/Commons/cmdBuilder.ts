4c3ab5a578064aea0da116e4e57817e38836cfbc
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-14 04:33:45 -0300

    Corregir tipado

--- commands/Commons/cmdBuilder.ts
+++ commands/Commons/cmdBuilder.ts
@@ -1,53 +1,10 @@
 import { Message, CommandInteraction, Collection, ChatInputCommandInteraction, Snowflake, Attachment, PermissionsBitField, MessagePayload, MessageEditOptions, InteractionReplyOptions, InteractionDeferReplyOptions, InteractionEditReplyOptions, User, MessageComponentInteraction, MessageActionRowComponentBuilder, Interaction, ModalMessageModalSubmitInteraction, ButtonInteraction, SelectMenuInteraction, MessageReplyOptions, MessageFlags, InteractionCallbackResponse, BooleanCache, InteractionResponse, Client, InteractionCallback, InteractionCallbackResource } from 'discord.js';
 import { CommandTags } from './cmdTags';
-import { CommandArguments, CommandRequest, ComplexCommandRequest, ComponentInteraction } from './typings';
+import { CommandArguments, CommandRequest, ComplexCommandRequest, ComponentInteraction, ExtendedCommandRequest } from './typings';
 import { CommandOptions, CommandOptionSolver } from './cmdOpts';
 import { CommandPermissions } from './cmdPerms';
 
-export interface ExtendedCommandRequestPrototype {
-	/**The command's initial reply.*/
-	initialReply: Message<boolean>;
-	/**Whether the command is a message command (true) or not (false).*/
-	isMessage: boolean;
-	/**Whether the command is an interaction or Slash command (true) or not (false).*/
-	isInteraction: boolean;
-	/**Infers the command as a guild Message Command. Throws an error if the command isn't really a Message Command.*/
-	inferAsMessage: () => Message<true>;
-	/**Infers the command as a cached Slash Command Interaction. Throws an error if the command isn't really a Slash Command.*/
-	inferAsSlash: () => ChatInputCommandInteraction<'cached'>;
-	/**If the command is a message command, returns the message's activity (if any).*/
-	activity?: import ('discord.js').MessageActivity;
-	/**A collection of the command's attachments, if it's a message command and it has attachments.*/
-	attachments: Collection<Snowflake, Attachment>;
-	/**The permissions of the application or bot in the current channel.*/
-	appPermisions: Readonly<PermissionsBitField>;
-	deferred: boolean;
-	replied: boolean;
-	/**The permissions within the current channel of the member who started the command.*/
-	memberPermissions: Readonly<PermissionsBitField>;
-	/**The user who started the command.*/
-	user: User;
-	/**The id of the user who started the command.*/
-	userId: string;
-	/**If a Slash command, defers the initial reply. Otherwise, sends a message and remembers it as the initial reply.*/
-	reply: (options?: (string|MessagePayload)&(MessageEditOptions|InteractionReplyOptions)|{}|undefined) => Promise<Message<boolean>>;
-	/**If a Slash command, defers the initial reply. Otherwise, sends a message and remembers it as the initial reply*/
-	replyFirst: (options?: (string|MessagePayload)&(MessageEditOptions|InteractionReplyOptions)|{}|undefined) => Promise<Message<boolean>>;
-	/**If a Slash command, defers the initial reply. Otherwise, sends a message and remembers it as the initial reply.*/
-	deferReply(options?: InteractionDeferReplyOptions & { fetchReply: true }): Promise<Message<boolean>>;
-	/**Deletes the original message if the command is a message command.*/
-	delete: () => Promise<Message<boolean>>;
-	/**Deletes the initial reply.*/
-	deleteReply: () => Promise<Message<boolean>>;
-	/**Edits the initial reply.*/
-	editReply: (options: (string|MessagePayload)&(MessageEditOptions|InteractionEditReplyOptions)|{}|undefined) => Promise<Message<boolean>>;
-	/**Determines whether the initial reply was deferred (true) or not (false).*/
-	wasDeferred: () => boolean;
-	/**Determines whether the initial reply was sent (true) or not (false).*/
-	wasReplied: () => boolean;
-}
-
-const extendedCommandRequestPrototype: ExtendedCommandRequestPrototype = {
+const extendedCommandRequestBlank: ExtendedCommandRequest = {
 	initialReply: undefined,
 	isInteraction: false,
 	isMessage: false,
@@ -74,8 +31,7 @@ const extendedCommandRequestPrototype: ExtendedCommandRequestPrototype = {
 };
 
 function extendRequest(request: CommandRequest | ComponentInteraction): ComplexCommandRequest {
-	/**@type {ExtendedCommandRequestPrototype}*/
-	const extension: ExtendedCommandRequestPrototype = Object.assign({}, extendedCommandRequestPrototype);
+	const extension: ExtendedCommandRequest = Object.assign({}, extendedCommandRequestBlank);
 
 	if(Command.requestIsMessage(request)) {
 		extension.isMessage = true;
--- commands/Commons/typings.ts
+++ commands/Commons/typings.ts
@@ -1,13 +1,54 @@
-import { AutocompleteInteraction, ButtonInteraction, ChatInputCommandInteraction, CommandInteractionOptionResolver, Message, MessageContextMenuCommandInteraction, StringSelectMenuInteraction } from 'discord.js';
-import { ExtendedCommandRequestPrototype } from './cmdBuilder';
+import { Attachment, AutocompleteInteraction, ButtonInteraction, ChatInputCommandInteraction, Collection, CommandInteractionOptionResolver, InteractionDeferReplyOptions, InteractionEditReplyOptions, InteractionReplyOptions, Message, MessageContextMenuCommandInteraction, MessageEditOptions, MessagePayload, PermissionsBitField, Snowflake, StringSelectMenuInteraction, User } from 'discord.js';
 
 /**@description Representa los tipos de petición de comando.*/
 export type CommandRequest =
 	| Message<true>
-	| Omit<ChatInputCommandInteraction<'cached'>, 'deferReply'>;
+	| Omit<ChatInputCommandInteraction<'cached'>, 'reply' | 'deferReply'>;
 
-/**@description Interfaz con extensiones para {@link CommandRequest}.*/
-export type ExtendedCommandRequest = ExtendedCommandRequestPrototype;
+/**@description Extensiones para {@link CommandRequest}.*/
+
+export interface ExtendedCommandRequest {
+	/**The command's initial reply.*/
+	initialReply: Message<boolean>;
+	/**Whether the command is a message command (true) or not (false).*/
+	isMessage: boolean;
+	/**Whether the command is an interaction or Slash command (true) or not (false).*/
+	isInteraction: boolean;
+	/**Infers the command as a guild Message Command. Throws an error if the command isn't really a Message Command.*/
+	inferAsMessage: () => Message<true>;
+	/**Infers the command as a cached Slash Command Interaction. Throws an error if the command isn't really a Slash Command.*/
+	inferAsSlash: () => ChatInputCommandInteraction<'cached'>;
+	/**If the command is a message command, returns the message's activity (if any).*/
+	activity?: import ('discord.js').MessageActivity;
+	/**A collection of the command's attachments, if it's a message command and it has attachments.*/
+	attachments: Collection<Snowflake, Attachment>;
+	/**The permissions of the application or bot in the current channel.*/
+	appPermisions: Readonly<PermissionsBitField>;
+	deferred: boolean;
+	replied: boolean;
+	/**The permissions within the current channel of the member who started the command.*/
+	memberPermissions: Readonly<PermissionsBitField>;
+	/**The user who started the command.*/
+	user: User;
+	/**The id of the user who started the command.*/
+	userId: string;
+	/**If a Slash command, defers the initial reply. Otherwise, sends a message and remembers it as the initial reply.*/
+	reply: (options?: (string|MessagePayload)&(MessageEditOptions|InteractionReplyOptions)|{}|undefined) => Promise<Message<boolean>>;
+	/**If a Slash command, defers the initial reply. Otherwise, sends a message and remembers it as the initial reply*/
+	replyFirst: (options?: (string|MessagePayload)&(MessageEditOptions|InteractionReplyOptions)|{}|undefined) => Promise<Message<boolean>>;
+	/**If a Slash command, defers the initial reply. Otherwise, sends a message and remembers it as the initial reply.*/
+	deferReply(options?: InteractionDeferReplyOptions & { fetchReply: true }): Promise<Message<boolean>>;
+	/**Deletes the original message if the command is a message command.*/
+	delete: () => Promise<Message<boolean>>;
+	/**Deletes the initial reply.*/
+	deleteReply: () => Promise<Message<boolean>>;
+	/**Edits the initial reply.*/
+	editReply: (options: (string|MessagePayload)&(MessageEditOptions|InteractionEditReplyOptions)|{}|undefined) => Promise<Message<boolean>>;
+	/**Determines whether the initial reply was deferred (true) or not (false).*/
+	wasDeferred: () => boolean;
+	/**Determines whether the initial reply was sent (true) or not (false).*/
+	wasReplied: () => boolean;
+}
 
 /**@description Interfaz con extensiones para {@link CommandRequest}.*/
 export type ComplexCommandRequest = CommandRequest & ExtendedCommandRequest;
