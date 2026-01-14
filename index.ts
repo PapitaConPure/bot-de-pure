1aef217f6fe87aa961a53be42e65e50d476445ab
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-14 10:11:48 -0300

    * as events

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 console.time('Carga de inicio');
 import { initializeClient } from './core/client';
-import { registerCommandFiles } from './core/commandInit.js';
-import { onCriticalError, onGuildMemberAdd, onGuildMemberRemove, onGuildMemberUpdate, onInteraction, onMessage, onMessageDelete, onRateLimit, onReactionAdd, onReactionRemove, onStartup, onUncaughtException, onUnhandledRejection, onVoiceUpdate } from './events/';
+import * as events from './events';
 import { discordToken } from './data/globalProps';
 console.timeEnd('Carga de inicio');
 
@@ -9,27 +8,23 @@ console.time('Creación de cliente de Discord');
 const client = initializeClient();
 console.timeEnd('Creación de cliente de Discord');
 
-console.time('Detección de archivos de comando');
-registerCommandFiles(false);
-console.timeEnd('Detección de archivos de comando');
-
 console.time('Registro de eventos de proceso');
-process.on('uncaughtException', onUncaughtException);
-process.on('unhandledRejection', onUnhandledRejection);
+process.on('uncaughtException', events.onUncaughtException);
+process.on('unhandledRejection', events.onUnhandledRejection);
 console.timeEnd('Registro de eventos de proceso');
 
 console.time('Registro de eventos del cliente');
-client.on('clientReady', onStartup);
-client.on('messageCreate', message => onMessage(message).catch(onCriticalError));
-client.on('messageReactionAdd', (reaction, user) => onReactionAdd(reaction, user).catch(onCriticalError));
-client.on('messageReactionRemove', (reaction, user) => onReactionRemove(reaction, user).catch(onCriticalError));
-client.on('messageDelete', message => onMessageDelete(message).catch(onCriticalError));
-client.on('interactionCreate', interaction => onInteraction(interaction, client).catch(onCriticalError));
-client.on('voiceStateUpdate', (oldState, newState) => onVoiceUpdate(oldState, newState).catch(onCriticalError));
-client.on('guildMemberAdd', member => { onGuildMemberAdd(member).catch(onCriticalError) });
-client.on('guildMemberRemove', member => { onGuildMemberRemove(member).catch(onCriticalError) });
-client.on('guildMemberUpdate', (oldMember, newMember) => { onGuildMemberUpdate(oldMember, newMember).catch(onCriticalError) });
-client.rest.on('rateLimited', onRateLimit);
+client.on('clientReady', events.onStartup);
+client.on('messageCreate', message => events.onMessage(message).catch(events.onCriticalError));
+client.on('messageReactionAdd', (reaction, user) => events.onReactionAdd(reaction, user).catch(events.onCriticalError));
+client.on('messageReactionRemove', (reaction, user) => events.onReactionRemove(reaction, user).catch(events.onCriticalError));
+client.on('messageDelete', message => events.onMessageDelete(message).catch(events.onCriticalError));
+client.on('interactionCreate', interaction => events.onInteraction(interaction).catch(events.onCriticalError));
+client.on('voiceStateUpdate', (oldState, newState) => events.onVoiceUpdate(oldState, newState).catch(events.onCriticalError));
+client.on('guildMemberAdd', member => { events.onGuildMemberAdd(member).catch(events.onCriticalError) });
+client.on('guildMemberRemove', member => { events.onGuildMemberRemove(member).catch(events.onCriticalError) });
+client.on('guildMemberUpdate', (oldMember, newMember) => { events.onGuildMemberUpdate(oldMember, newMember).catch(events.onCriticalError) });
+client.rest.on('rateLimited', events.onRateLimit);
 
 client.login(discordToken);
 console.timeEnd('Registro de eventos del cliente');
