b3175c8a149c04f64b563fb47b3221f2b07daec3
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-15 10:25:42 -0300

    Por algún motivo eso no estaba andando

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,21 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
+import ts from 'typescript';
 
 export default defineConfig([
-  { files: ['**/*.ts'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.ts'],
+    plugins: { js, ts },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
   tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
 ]);
