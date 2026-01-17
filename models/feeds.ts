ad69390026661bff3ad2bed9ecc159b2529e512c
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-01-17 03:22:36 -0300

    Nuevos schemas

---
+++ models/feeds.ts
@@ -0,0 +1,48 @@
+import Mongoose from 'mongoose';
+
+const FeedSchema = new Mongoose.Schema({
+	ids: {
+		type: [String],
+		default: [],
+	},
+	tags: {
+		type: String,
+		required: true,
+	},
+    faults: {
+		type: Number,
+		default: 0,
+	},
+    lastFetchedAt: {
+		type: Date,
+		default: Date.UTC(1970, 0, 1, 0, 0, 0, 0),
+	},
+    title: {
+		type: String,
+		required: false,
+	},
+    subtitle: {
+		type: String,
+		required: false,
+	},
+    maxTags: {
+		type: Number,
+		required: false,
+	},
+    cornerIcon: {
+		type: String,
+		required: false,
+	},
+    footer: {
+		type: String,
+		required: false,
+	},
+});
+
+const Feed = Mongoose.model('Feed', FeedSchema);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function m() { return new Feed({}); }
+export type FeedDocument = ReturnType<(typeof m)>;
+
+export default Feed;
---
+++ models/tubers.ts
@@ -0,0 +1,99 @@
+import Mongoose from 'mongoose';
+import { CURRENT_PS_VERSION } from '../systems/ps/common/executeTuber';
+
+const TuberInputSchema = new Mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	desc: {
+		type: String,
+		default: null,
+	},
+	kind: {
+		type: String,
+		enum: [ 'Number', 'Text', 'Boolean' ],
+		required: true,
+	},
+	optional: {
+		type: Boolean,
+		default: false,
+	},
+	spread: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const TuberSchema = new Mongoose.Schema({
+	id: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: String,
+		required: true,
+	},
+	desc: {
+		type: String,
+		default: null,
+	},
+	inputs: {
+		type: [[TuberInputSchema]],
+		default: [],
+	},
+	advanced: {
+		type: Boolean,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: false,
+	},
+	files: {
+		type: [String],
+	},
+	script: {
+		type: String,
+		default: null,
+		required: false,
+	},
+	psVersion: {
+		type: Number,
+		default: () => CURRENT_PS_VERSION,
+	},
+	saved: {
+		type: Map,
+		of: {
+			name: {
+				type: String,
+				required: true,
+			},
+			desc: {
+				type: String,
+				default: null,
+			},
+			kind: {
+				type: String,
+				required: true,
+			},
+			optional: {
+				type: Boolean,
+				required: true,
+			},
+			spread: {
+				type: Boolean,
+				required: true,
+			},
+		},
+		required: false,
+	},
+});
+
+const Tuber = Mongoose.model('Tuber', TuberSchema);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function m() { return new Tuber({}); }
+export type TuberDocument = ReturnType<(typeof m)>;
+
+export default Tuber;
