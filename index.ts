43311a08dd65243d441ea5f977703286ae572c77
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-10 02:25:06 -0300

    Extraer lógica de inicio a core/bootstrap.ts

--- index.ts
+++ index.ts
@@ -1,37 +1,3 @@
-console.time('Carga de inicio');
-import { getRuntimeEnvHint } from './utils/runtime';
+import { bootstrap } from './core/bootstrap';
 
-if(!process.env?.MONGODB_URI) {
-	const hint = getRuntimeEnvHint();
-	throw new Error(`Missing MongoDB database environment variable.\n\n${hint}`);
-}
-
-import { initializeClient } from './core/client';
-import * as events from './events';
-import { discordToken } from './data/globalProps';
-console.timeEnd('Carga de inicio');
-
-console.time('Creación de cliente de Discord');
-const client = initializeClient();
-console.timeEnd('Creación de cliente de Discord');
-
-console.time('Registro de eventos de proceso');
-process.on('uncaughtException', events.onUncaughtException);
-process.on('unhandledRejection', events.onUnhandledRejection);
-console.timeEnd('Registro de eventos de proceso');
-
-console.time('Registro de eventos del cliente');
-client.on('clientReady', events.onStartup);
-client.on('messageCreate', message => events.onMessage(message).catch(events.onCriticalError));
-client.on('messageReactionAdd', (reaction, user) => events.onReactionAdd(reaction, user).catch(events.onCriticalError));
-client.on('messageReactionRemove', (reaction, user) => events.onReactionRemove(reaction, user).catch(events.onCriticalError));
-client.on('messageDelete', message => events.onMessageDelete(message).catch(events.onCriticalError));
-client.on('interactionCreate', interaction => events.onInteraction(interaction).catch(events.onCriticalError));
-client.on('voiceStateUpdate', (oldState, newState) => events.onVoiceUpdate(oldState, newState).catch(events.onCriticalError));
-client.on('guildMemberAdd', member => { events.onGuildMemberAdd(member).catch(events.onCriticalError); });
-client.on('guildMemberRemove', member => { events.onGuildMemberRemove(member).catch(events.onCriticalError); });
-client.on('guildMemberUpdate', (oldMember, newMember) => { events.onGuildMemberUpdate(oldMember, newMember).catch(events.onCriticalError); });
-client.rest.on('rateLimited', events.onRateLimit);
-
-client.login(discordToken);
-console.timeEnd('Registro de eventos del cliente');
+bootstrap();
