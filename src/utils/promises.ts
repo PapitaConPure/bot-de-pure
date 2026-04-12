e268a48dc80e0c0d294afafb0af2d2511f0738bf
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-12 15:05:49 -0300

    Fución de utilidad para promesas

---
+++ src/utils/promises.ts
@@ -0,0 +1,6 @@
+export function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
+	return Promise.race([
+		promise,
+		new Promise<T>((_, reject) => setTimeout(() => reject(new Error('Timeout')), ms)),
+	]);
+}
