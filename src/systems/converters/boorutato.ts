bec660b27f6a7aedacc79e6b4c5b50c7bfe40af9
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-15 21:38:35 -0300

    Remove generic tags from BoorutatoConvert output

--- src/systems/converters/boorutato.ts
+++ src/systems/converters/boorutato.ts
@@ -71,7 +71,7 @@ export async function sendConvertedBooruPosts(
 		const formattedGelbooruUrl = `${spoiler}<:gelbooru:919398540172750878>[\`${id}\`](${original})${spoiler}`;
 
 		const container = await formatBooruPostMessage(booru, post, {
-			maxTags: 20,
+			maxTags: 0,
 			manageableBy: message.author.id,
 			allowNSFW: isNSFWChannel(message.channel),
 			isNotFeed: true,
