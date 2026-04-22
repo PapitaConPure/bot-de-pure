120a8830b17b7125f08ccd10c843a80decb12d68
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-22 17:18:00 -0300

    Remove unnecessary silliness

--- src/events/onGuildMemberAdd.ts
+++ src/events/onGuildMemberAdd.ts
@@ -8,19 +8,5 @@ const { debug } = Logger('DEBUG', 'GMemberAdd');
 export async function onGuildMemberAdd(member: GuildMember) {
 	debug(`Evento disparado - ${member.user.username} → ${member.guild.name} (${member.guild.id})`);
 	if (!guildIsAvailable(member.guild)) return;
-	if (member.user.id === '239550977638793217') {
-		setTimeout(equisde, 1000 * 2, member);
-		member.roles.add('1139992433501942001').catch(console.error);
-		return member.guild.systemChannel
-			?.send('<:kokocrong:1107848001541644389>')
-			.catch(console.error);
-	}
 	announceMemberUpdate(member, 'welcome', sendWelcomeMessage);
-}
-
-async function equisde(member: GuildMember) {
-	return member.roles.remove('1107831054791876691').catch((error) => {
-		console.error(error);
-		setTimeout(equisde, 1000 * 2, member);
-	});
 }
