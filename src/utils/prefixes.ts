5994eed91aed4af6eb3ac8f714f682eb6d422cd6
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-21 21:00:19 -0300

    Allow any interaction for prefix formulation

--- src/utils/prefixes.ts
+++ src/utils/prefixes.ts
@@ -1,15 +1,21 @@
+import type { AnySelectMenuInteraction } from 'discord.js';
 import type { PrefixPair } from '../data/globalProps';
 import { prefixes } from '../data/globalProps';
 import type { ComplexCommandRequest } from '../types/commands';
 
-export const slashPrefixPair = { raw: '/', regex: /\//i } as const;
+export const slashPrefixPair = { raw: '/', regex: /\//i } as const satisfies PrefixPair;
 
 /**
  * @param context Contexto de servidor o request
  * @returns Un objeto conteniendo el texto crudo y RegExp del prefijo
  */
-export function p_pure(context: ComplexCommandRequest | string = '0'): PrefixPair {
+export function p_pure(
+	context: ComplexCommandRequest | AnySelectMenuInteraction<'cached'> | string = '0',
+): PrefixPair {
 	if (typeof context === 'string') return prefixes[context] || prefixes['0'];
 
-	return context.isInteraction ? slashPrefixPair : p_pure(context.guildId);
+	if ('isInteraction' in context)
+		return context.isInteraction ? slashPrefixPair : p_pure(context.guildId);
+
+	return slashPrefixPair;
 }
