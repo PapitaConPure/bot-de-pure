d0ce35417d4899b65f6fa259ea44513b17ce21df
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-07-25 14:28:17 -0300

    Add event log

--- src/events/onGuildMemberRemove.ts
+++ src/events/onGuildMemberRemove.ts
@@ -1,11 +1,12 @@
 import type { GuildMember, PartialGuildMember } from 'discord.js';
 import { sendFarewellMessage } from '@/utils/announcements';
+import Logger from '@/utils/logs.js';
 import { announceMemberUpdate, guildIsAvailable } from './guildMemberUpdate';
 
+const { debug } = Logger('DEBUG', 'GMemberRemove');
+
 export async function onGuildMemberRemove(member: GuildMember | PartialGuildMember) {
-	console.log('Evento de salida de miembro de servidor desencadenado');
-
+	debug(`Evento disparado - ${member.user.username} ← ${member.guild.name} (${member.guild.id})`);
 	if (!guildIsAvailable(member.guild)) return;
-
 	announceMemberUpdate(member, 'farewell', sendFarewellMessage);
 }
